2ba49e5ef04f45357141dae0a95f01aea98b0d2f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-07 12:39:41 -0600

    Create SearchActors.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/App/Bsky/Actor/SearchActors.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\App\Bsky\Actor;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * Find actors (profiles) matching search criteria.
+ *
+ * @see \Ghostwriter\AtProtocol\Tests\Unit\App\Bsky\Actor\SearchActorsTest
+ */
+final readonly class SearchActors
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+    ) {}
+
+    public function __invoke(
+        UriInterface $uri,
+        ?string $q = null,
+        ?int $limit = null,
+        ?string $cursor = null,
+    ): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'GET',
+                $uri->withPath('xrpc/app.bsky.actor.searchActors')
+                    ->withQuery(http_build_query(array_filter([
+                    'q' => $q,
+                    'limit' => $limit,
+                    'cursor' => $cursor,
+                ])))
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request;
+    }
+}
