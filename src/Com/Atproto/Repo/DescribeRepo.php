5f7b9e3033e7e291d3c9da7f697037f57c44be11
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-07 13:05:40 -0600

    Create DescribeRepo.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Com/Atproto/Repo/DescribeRepo.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Com\Atproto\Repo;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * Get information about the repo, including the list of collections.
+ *
+ * @see \Ghostwriter\AtProtocol\Tests\Unit\Com\Atproto\Repo\DescribeRepoTest
+ */
+final readonly class DescribeRepo
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+    ) {}
+
+    public function __invoke(
+        UriInterface $uri,
+        string $repo = null,
+    ): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'GET',
+                $uri->withPath('xrpc/com.atproto.repo.describeRepo')
+                    ->withQuery(http_build_query(array_filter([
+                    'repo' => $repo,
+                ])))
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request;
+    }
+}
