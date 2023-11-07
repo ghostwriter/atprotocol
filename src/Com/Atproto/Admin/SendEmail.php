bfe6f67c024bf1b4ce9936e9f60687b23c6f7a53
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-07 13:04:18 -0600

    Create SendEmail.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Com/Atproto/Admin/SendEmail.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Com\Atproto\Admin;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * Send email to a user's primary email address
+ *
+ * @see \Ghostwriter\AtProtocol\Tests\Unit\Com\Atproto\Admin\SendEmailTest
+ */
+final readonly class SendEmail
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+        private StreamFactoryInterface $streamFactory,
+    ) {}
+
+    public function __invoke(
+        UriInterface $uri,
+        string $recipientDid = null,
+        string $content = null,
+        ?string $subject = null,
+    ): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'POST',
+                $uri->withPath('xrpc/com.atproto.admin.sendEmail')
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        $jsonBody = json_encode(array_filter([
+            'recipientDid' => $recipientDid,
+            'content' => $content,
+            'subject' => $subject,
+        ]));
+
+        if (false === $jsonBody){
+            throw new \RuntimeException('Failed to encode JSON');
+        }
+
+        return $request->withBody(
+            $this->streamFactory->createStream(
+                $jsonBody
+            )
+        );
+    }
+}
