6edd97a4bf7b41d0089ce8cc65589592d57aa825
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-07 13:04:55 -0600

    Create QueryLabels.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Com/Atproto/Label/QueryLabels.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Com\Atproto\Label;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * Find labels relevant to the provided URI patterns.
+ *
+ * @see \Ghostwriter\AtProtocol\Tests\Unit\Com\Atproto\Label\QueryLabelsTest
+ */
+final readonly class QueryLabels
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+    ) {}
+
+    public function __invoke(
+        UriInterface $uri,
+        array $uriPatterns = null,
+        ?array $sources = null,
+        ?int $limit = null,
+        ?string $cursor = null,
+    ): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'GET',
+                $uri->withPath('xrpc/com.atproto.label.queryLabels')
+                    ->withQuery(http_build_query(array_filter([
+                    'uriPatterns' => $uriPatterns,
+                    'sources' => $sources,
+                    'limit' => $limit,
+                    'cursor' => $cursor,
+                ])))
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request;
+    }
+}
