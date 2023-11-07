cc2526801fffadfb6a8355f1cac45c2b119a5b05
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-07 12:38:26 -0600

    Create GetPreferences.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/App/Bsky/Actor/GetPreferences.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\App\Bsky\Actor;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * Get private preferences attached to the account.
+ *
+ * @see \Ghostwriter\AtProtocol\Tests\Unit\App\Bsky\Actor\GetPreferencesTest
+ */
+final readonly class GetPreferences
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+    ) {}
+
+    public function __invoke(
+        UriInterface $uri,
+    ): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'GET',
+                $uri->withPath('xrpc/app.bsky.actor.getPreferences')
+                    ->withQuery(http_build_query(array_filter([])))
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request;
+    }
+}
