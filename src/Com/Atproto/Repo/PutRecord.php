46ebec08be6adb7aecf96b114e9b6aaca55bf1b4
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-07 13:06:16 -0600

    Create PutRecord.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Com/Atproto/Repo/PutRecord.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Com\Atproto\Repo;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * Write a record, creating or updating it as needed.
+ *
+ * @see \Ghostwriter\AtProtocol\Tests\Unit\Com\Atproto\Repo\PutRecordTest
+ */
+final readonly class PutRecord
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+        private StreamFactoryInterface $streamFactory,
+    ) {}
+
+    public function __invoke(
+        UriInterface $uri,
+        string $repo = null,
+        string $collection = null,
+        string $rkey = null,
+        string $record = null,
+        ?bool $validate = null,
+        ?string $swapRecord = null,
+        ?string $swapCommit = null,
+    ): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'POST',
+                $uri->withPath('xrpc/com.atproto.repo.putRecord')
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        $jsonBody = json_encode(array_filter([
+            'repo' => $repo,
+            'collection' => $collection,
+            'rkey' => $rkey,
+            'validate' => $validate,
+            'record' => $record,
+            'swapRecord' => $swapRecord,
+            'swapCommit' => $swapCommit,
+        ]));
+
+        if (false === $jsonBody){
+            throw new \RuntimeException('Failed to encode JSON');
+        }
+
+        return $request->withBody(
+            $this->streamFactory->createStream(
+                $jsonBody
+            )
+        );
+    }
+}
