89b178ec36a91c832ac1705bfced3e9bbb7712a0
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-07 12:55:59 -0600

    Create GetPosts.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/App/Bsky/Feed/GetPosts.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\App\Bsky\Feed;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * A view of an actor's feed.
+ *
+ * @see \Ghostwriter\AtProtocol\Tests\Unit\App\Bsky\Feed\GetPostsTest
+ */
+final readonly class GetPosts
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+    ) {}
+
+    public function __invoke(
+        UriInterface $uri,
+        array $uris = null,
+    ): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'GET',
+                $uri->withPath('xrpc/app.bsky.feed.getPosts')
+                    ->withQuery(http_build_query(array_filter([
+                    'uris' => $uris,
+                ])))
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request;
+    }
+}
