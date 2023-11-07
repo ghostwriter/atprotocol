23539365006916a8a32733de9a3863451fa3312a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-07 13:07:41 -0600

    Create ListAppPasswords.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Com/Atproto/Server/ListAppPasswords.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Com\Atproto\Server;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * List all app-specific passwords.
+ *
+ * @see \Ghostwriter\AtProtocol\Tests\Unit\Com\Atproto\Server\ListAppPasswordsTest
+ */
+final readonly class ListAppPasswords
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+    ) {}
+
+    public function __invoke(
+        UriInterface $uri,
+    ): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'GET',
+                $uri->withPath('xrpc/com.atproto.server.listAppPasswords')
+                    ->withQuery(http_build_query(array_filter([])))
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request;
+    }
+}
