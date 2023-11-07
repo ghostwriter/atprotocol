30d4f64ed531139628e3a3919c95f4088a18b392
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-07 13:05:12 -0600

    Create ApplyWrites.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Com/Atproto/Repo/ApplyWrites.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Com\Atproto\Repo;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * Apply a batch transaction of creates, updates, and deletes.
+ *
+ * @see \Ghostwriter\AtProtocol\Tests\Unit\Com\Atproto\Repo\ApplyWritesTest
+ */
+final readonly class ApplyWrites
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+        private StreamFactoryInterface $streamFactory,
+    ) {}
+
+    public function __invoke(
+        UriInterface $uri,
+        string $repo = null,
+        array $writes = null,
+        ?bool $validate = null,
+        ?string $swapCommit = null,
+    ): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'POST',
+                $uri->withPath('xrpc/com.atproto.repo.applyWrites')
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        $jsonBody = json_encode(array_filter([
+            'repo' => $repo,
+            'validate' => $validate,
+            'writes' => $writes,
+            'swapCommit' => $swapCommit,
+        ]));
+
+        if (false === $jsonBody){
+            throw new \RuntimeException('Failed to encode JSON');
+        }
+
+        return $request->withBody(
+            $this->streamFactory->createStream(
+                $jsonBody
+            )
+        );
+    }
+}
