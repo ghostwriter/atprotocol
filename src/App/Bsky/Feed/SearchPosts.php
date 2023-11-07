74c7082484211b61e95be6a411a1ddc6292244d6
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-07 12:58:17 -0600

    Create SearchPosts.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/App/Bsky/Feed/SearchPosts.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\App\Bsky\Feed;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * Find posts matching search criteria
+ *
+ * @see \Ghostwriter\AtProtocol\Tests\Unit\App\Bsky\Feed\SearchPostsTest
+ */
+final readonly class SearchPosts
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+    ) {}
+
+    public function __invoke(
+        UriInterface $uri,
+        string $q = null,
+        ?int $limit = null,
+        ?string $cursor = null,
+    ): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'GET',
+                $uri->withPath('xrpc/app.bsky.feed.searchPosts')
+                    ->withQuery(http_build_query(array_filter([
+                    'q' => $q,
+                    'limit' => $limit,
+                    'cursor' => $cursor,
+                ])))
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request;
+    }
+}
