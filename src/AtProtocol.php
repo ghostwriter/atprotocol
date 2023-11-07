bddae6f74aa4419f8c779462a6c8a3467194bc90
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-07 13:02:04 -0600

    Create AtProtocol.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/AtProtocol.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol;
+
+use Ghostwriter\AtProtocol\Trait\HttpTrait;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\UploadedFileInterface;
+use Psr\Http\Message\RequestInterface;
+
+/** @see AtProtocolTest */
+final class AtProtocol
+{
+    use HttpTrait;
+    public function createSession(
+        string $username,
+        string $password
+    ): ResponseInterface {
+        // replace this block of code with a separate class
+        return $this->httpClient->sendRequest(
+            ($this->createSession)($username, $password)
+        );
+    }
+}
