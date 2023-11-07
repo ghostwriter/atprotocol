ffb72282db4bdb3f0ebc55f63f7f523c657dc495
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-07 12:55:13 -0600

    Create GetFeedSkeleton.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/App/Bsky/Feed/GetFeedSkeleton.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\App\Bsky\Feed;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * A skeleton of a feed provided by a feed generator
+ *
+ * @see \Ghostwriter\AtProtocol\Tests\Unit\App\Bsky\Feed\GetFeedSkeletonTest
+ */
+final readonly class GetFeedSkeleton
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+    ) {}
+
+    public function __invoke(
+        UriInterface $uri,
+        string $feed = null,
+        ?int $limit = null,
+        ?string $cursor = null,
+    ): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'GET',
+                $uri->withPath('xrpc/app.bsky.feed.getFeedSkeleton')
+                    ->withQuery(http_build_query(array_filter([
+                    'feed' => $feed,
+                    'limit' => $limit,
+                    'cursor' => $cursor,
+                ])))
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request;
+    }
+}
