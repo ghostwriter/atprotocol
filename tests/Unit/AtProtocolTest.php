d8481cbfa9951c169a3f4707109ceeb569e4aec1
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-07 13:11:52 -0600

    Create AtProtocolTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/AtProtocolTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Tests\Unit;
+
+use Ghostwriter\AtProtocol\AtProtocol;
+use Ghostwriter\AtProtocol\Com\Atproto\Server\CreateSession;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\UsesClass;
+use PHPUnit\Framework\TestCase;
+use Laminas\Diactoros\Uri;
+
+#[CoversClass(AtProtocol::class)]
+#[UsesClass(CreateSession::class)]
+final class AtProtocolTest extends TestCase
+{
+    public function testAtProtocol(): void
+    {
+        self::assertInstanceOf(
+            AtProtocol::class,
+            AtProtocol::new('https://bsky.social')
+        );
+    }
+}
