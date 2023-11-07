576173187de434077f026efaab3833f181575628
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-07 13:06:09 -0600

    Create ListRecords.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Com/Atproto/Repo/ListRecords.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Com\Atproto\Repo;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * List a range of records in a collection.
+ *
+ * @see \Ghostwriter\AtProtocol\Tests\Unit\Com\Atproto\Repo\ListRecordsTest
+ */
+final readonly class ListRecords
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+    ) {}
+
+    public function __invoke(
+        UriInterface $uri,
+        string $repo = null,
+        string $collection = null,
+        ?int $limit = null,
+        ?string $cursor = null,
+        ?bool $reverse = null,
+    ): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'GET',
+                $uri->withPath('xrpc/com.atproto.repo.listRecords')
+                    ->withQuery(http_build_query(array_filter([
+                    'repo' => $repo,
+                    'collection' => $collection,
+                    'limit' => $limit,
+                    'cursor' => $cursor,
+                    'reverse' => $reverse,
+                ])))
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request;
+    }
+}
