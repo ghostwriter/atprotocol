70b4d482a12ee5447c1cfb137dcdbc379629bf63
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-07 13:08:53 -0600

    Create GetBlob.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Com/Atproto/Sync/GetBlob.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Com\Atproto\Sync;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * Get a blob associated with a given repo.
+ *
+ * @see \Ghostwriter\AtProtocol\Tests\Unit\Com\Atproto\Sync\GetBlobTest
+ */
+final readonly class GetBlob
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+    ) {}
+
+    public function __invoke(
+        UriInterface $uri,
+        string $did = null,
+        string $cid = null,
+    ): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'GET',
+                $uri->withPath('xrpc/com.atproto.sync.getBlob')
+                    ->withQuery(http_build_query(array_filter([
+                    'did' => $did,
+                    'cid' => $cid,
+                ])))
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => '*/*; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request;
+    }
+}
