8c49390d3184715c14952c7a5379f4681430bd65
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-07 12:38:35 -0600

    Create GetProfile.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/App/Bsky/Actor/GetProfile.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\App\Bsky\Actor;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * GetProfile
+ *
+ * @see \Ghostwriter\AtProtocol\Tests\Unit\App\Bsky\Actor\GetProfileTest
+ */
+final readonly class GetProfile
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+    ) {}
+
+    public function __invoke(
+        UriInterface $uri,
+        string $actor = null,
+    ): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'GET',
+                $uri->withPath('xrpc/app.bsky.actor.getProfile')
+                    ->withQuery(http_build_query(array_filter([
+                    'actor' => $actor,
+                ])))
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request;
+    }
+}
