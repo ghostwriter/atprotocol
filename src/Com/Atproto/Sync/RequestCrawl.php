0bc6f1b5b8844545880dfbd12021840bcf88c10e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-07 13:09:51 -0600

    Create RequestCrawl.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Com/Atproto/Sync/RequestCrawl.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Com\Atproto\Sync;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * Request a service to persistently crawl hosted repos.
+ *
+ * @see \Ghostwriter\AtProtocol\Tests\Unit\Com\Atproto\Sync\RequestCrawlTest
+ */
+final readonly class RequestCrawl
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+        private StreamFactoryInterface $streamFactory,
+    ) {}
+
+    public function __invoke(
+        UriInterface $uri,
+        string $hostname = null,
+    ): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'POST',
+                $uri->withPath('xrpc/com.atproto.sync.requestCrawl')
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        $jsonBody = json_encode(array_filter([
+            'hostname' => $hostname,
+        ]));
+
+        if (false === $jsonBody){
+            throw new \RuntimeException('Failed to encode JSON');
+        }
+
+        return $request->withBody(
+            $this->streamFactory->createStream(
+                $jsonBody
+            )
+        );
+    }
+}
