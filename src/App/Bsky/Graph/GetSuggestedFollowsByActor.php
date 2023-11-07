58f797d3bb3519a79d76a1587a0859a9028733be
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-07 12:59:40 -0600

    Create GetSuggestedFollowsByActor.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/App/Bsky/Graph/GetSuggestedFollowsByActor.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\App\Bsky\Graph;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * Get suggested follows related to a given actor.
+ *
+ * @see \Ghostwriter\AtProtocol\Tests\Unit\App\Bsky\Graph\GetSuggestedFollowsByActorTest
+ */
+final readonly class GetSuggestedFollowsByActor
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+    ) {}
+
+    public function __invoke(
+        UriInterface $uri,
+        string $actor = null,
+    ): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'GET',
+                $uri->withPath('xrpc/app.bsky.graph.getSuggestedFollowsByActor')
+                    ->withQuery(http_build_query(array_filter([
+                    'actor' => $actor,
+                ])))
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request;
+    }
+}
