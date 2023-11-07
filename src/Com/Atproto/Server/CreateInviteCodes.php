9aa0e4e980b5efe1b5e1e14c63220f57b113af0c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-07 13:06:53 -0600

    Create CreateInviteCodes.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Com/Atproto/Server/CreateInviteCodes.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Com\Atproto\Server;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * Create an invite code.
+ *
+ * @see \Ghostwriter\AtProtocol\Tests\Unit\Com\Atproto\Server\CreateInviteCodesTest
+ */
+final readonly class CreateInviteCodes
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+        private StreamFactoryInterface $streamFactory,
+    ) {}
+
+    public function __invoke(
+        UriInterface $uri,
+        int $codeCount = null,
+        int $useCount = null,
+        ?array $forAccounts = null,
+    ): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'POST',
+                $uri->withPath('xrpc/com.atproto.server.createInviteCodes')
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        $jsonBody = json_encode(array_filter([
+            'codeCount' => $codeCount,
+            'useCount' => $useCount,
+            'forAccounts' => $forAccounts,
+        ]));
+
+        if (false === $jsonBody){
+            throw new \RuntimeException('Failed to encode JSON');
+        }
+
+        return $request->withBody(
+            $this->streamFactory->createStream(
+                $jsonBody
+            )
+        );
+    }
+}
