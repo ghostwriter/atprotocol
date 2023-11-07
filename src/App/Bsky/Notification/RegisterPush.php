ae8ff3c4c06541fb614e5968d2c989f09239a217
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-07 13:00:21 -0600

    Create RegisterPush.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/App/Bsky/Notification/RegisterPush.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\App\Bsky\Notification;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * Register for push notifications with a service
+ *
+ * @see \Ghostwriter\AtProtocol\Tests\Unit\App\Bsky\Notification\RegisterPushTest
+ */
+final readonly class RegisterPush
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+        private StreamFactoryInterface $streamFactory,
+    ) {}
+
+    public function __invoke(
+        UriInterface $uri,
+        string $serviceDid = null,
+        string $token = null,
+        string $platform = null,
+        string $appId = null,
+    ): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'POST',
+                $uri->withPath('xrpc/app.bsky.notification.registerPush')
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        $jsonBody = json_encode(array_filter([
+            'serviceDid' => $serviceDid,
+            'token' => $token,
+            'platform' => $platform,
+            'appId' => $appId,
+        ]));
+
+        if (false === $jsonBody){
+            throw new \RuntimeException('Failed to encode JSON');
+        }
+
+        return $request->withBody(
+            $this->streamFactory->createStream(
+                $jsonBody
+            )
+        );
+    }
+}
