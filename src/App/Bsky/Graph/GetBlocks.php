81a287a4c966832a5c5361b99baadb93b019f846
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-07 12:58:32 -0600

    Create GetBlocks.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/App/Bsky/Graph/GetBlocks.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\App\Bsky\Graph;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * Who is the requester's account blocking?
+ *
+ * @see \Ghostwriter\AtProtocol\Tests\Unit\App\Bsky\Graph\GetBlocksTest
+ */
+final readonly class GetBlocks
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+    ) {}
+
+    public function __invoke(
+        UriInterface $uri,
+        ?int $limit = null,
+        ?string $cursor = null,
+    ): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'GET',
+                $uri->withPath('xrpc/app.bsky.graph.getBlocks')
+                    ->withQuery(http_build_query(array_filter([
+                    'limit' => $limit,
+                    'cursor' => $cursor,
+                ])))
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request;
+    }
+}
