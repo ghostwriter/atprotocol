887075be724aef209b3a9e24645ec9945f3322e3
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-07 13:11:19 -0600

    Create Handle.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Value/Handle.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Value;
+
+use Stringable;
+use InvalidArgumentException;
+
+final readonly class Handle implements Stringable
+{
+    public function __construct(
+        private readonly string $handle,
+    ) {
+        if (
+            !preg_match(
+                '#^([a-zA-Z0-9]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?$#',
+                $this->handle
+            )
+        ) {
+            throw new InvalidArgumentException(
+                'Invalid handle: ' . $this->handle
+            );
+        }
+    }
+
+    public function __toString(): string
+    {
+        return $this->handle;
+    }
+}
