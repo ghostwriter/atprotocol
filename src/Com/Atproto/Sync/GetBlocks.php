5d0d2d814fa249b42ca0dd05dcc1f96313e592a5
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-07 13:08:57 -0600

    Create GetBlocks.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Com/Atproto/Sync/GetBlocks.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Com\Atproto\Sync;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * Gets blocks from a given repo.
+ *
+ * @see \Ghostwriter\AtProtocol\Tests\Unit\Com\Atproto\Sync\GetBlocksTest
+ */
+final readonly class GetBlocks
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+    ) {}
+
+    public function __invoke(
+        UriInterface $uri,
+        string $did = null,
+        array $cids = null,
+    ): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'GET',
+                $uri->withPath('xrpc/com.atproto.sync.getBlocks')
+                    ->withQuery(http_build_query(array_filter([
+                    'did' => $did,
+                    'cids' => $cids,
+                ])))
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/vnd.ipld.car; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request;
+    }
+}
