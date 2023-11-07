7df20c807ed78ed9b385c8c1fbfc28860368a904
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-07 13:00:42 -0600

    Create GetPopularFeedGenerators.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/App/Bsky/Unspecced/GetPopularFeedGenerators.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\App\Bsky\Unspecced;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * An unspecced view of globally popular feed generators
+ *
+ * @see \Ghostwriter\AtProtocol\Tests\Unit\App\Bsky\Unspecced\GetPopularFeedGeneratorsTest
+ */
+final readonly class GetPopularFeedGenerators
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+    ) {}
+
+    public function __invoke(
+        UriInterface $uri,
+        ?int $limit = null,
+        ?string $cursor = null,
+        ?string $query = null,
+    ): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'GET',
+                $uri->withPath('xrpc/app.bsky.unspecced.getPopularFeedGenerators')
+                    ->withQuery(http_build_query(array_filter([
+                    'limit' => $limit,
+                    'cursor' => $cursor,
+                    'query' => $query,
+                ])))
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request;
+    }
+}
