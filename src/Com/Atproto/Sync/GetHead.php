5c8982c88a78c0f3cf4bd6258720bbe423a5b986
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-07 13:09:09 -0600

    Create GetHead.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Com/Atproto/Sync/GetHead.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Com\Atproto\Sync;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * DEPRECATED - please use com.atproto.sync.getLatestCommit instead
+ *
+ * @see \Ghostwriter\AtProtocol\Tests\Unit\Com\Atproto\Sync\GetHeadTest
+ */
+final readonly class GetHead
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+    ) {}
+
+    public function __invoke(
+        UriInterface $uri,
+        string $did = null,
+    ): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'GET',
+                $uri->withPath('xrpc/com.atproto.sync.getHead')
+                    ->withQuery(http_build_query(array_filter([
+                    'did' => $did,
+                ])))
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request;
+    }
+}
