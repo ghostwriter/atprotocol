3c0a61b8af931dfe52c4148188a96ff3df8704ac
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-07 13:03:53 -0600

    Create GetSubjectStatus.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Com/Atproto/Admin/GetSubjectStatus.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Com\Atproto\Admin;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * Fetch the service-specific the admin status of a subject (account, record, or blob)
+ *
+ * @see \Ghostwriter\AtProtocol\Tests\Unit\Com\Atproto\Admin\GetSubjectStatusTest
+ */
+final readonly class GetSubjectStatus
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+    ) {}
+
+    public function __invoke(
+        UriInterface $uri,
+        ?string $did = null,
+        ?string $uri = null,
+        ?string $blob = null,
+    ): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'GET',
+                $uri->withPath('xrpc/com.atproto.admin.getSubjectStatus')
+                    ->withQuery(http_build_query(array_filter([
+                    'did' => $did,
+                    'uri' => $uri,
+                    'blob' => $blob,
+                ])))
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request;
+    }
+}
