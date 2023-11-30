40218bb14e178298f1e6d97b58d9637e63375a1d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-30 07:51:00 -0600

    Update RequestAccountDelete.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Server/RequestAccountDelete.php
+++ src/Com/Atproto/Server/RequestAccountDelete.php
@@ -22,13 +22,13 @@ final readonly class RequestAccountDelete
     ) {}
 
     public function __invoke(
-        UriInterface $uri,
+        UriInterface $pdsUri,
     ): RequestInterface
     {
         $request = $this->requestFactory
             ->createRequest(
                 'POST',
-                $uri->withPath('xrpc/com.atproto.server.requestAccountDelete')
+                $pdsUri->withPath('xrpc/com.atproto.server.requestAccountDelete')
             );
 
         $headers = [
