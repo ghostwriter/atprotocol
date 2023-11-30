b741627bb6e216fde7ff6bbc1d694f5106bc8ed5
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-30 07:50:58 -0600

    Update DeleteSession.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Server/DeleteSession.php
+++ src/Com/Atproto/Server/DeleteSession.php
@@ -22,13 +22,13 @@ final readonly class DeleteSession
     ) {}
 
     public function __invoke(
-        UriInterface $uri,
+        UriInterface $pdsUri,
     ): RequestInterface
     {
         $request = $this->requestFactory
             ->createRequest(
                 'POST',
-                $uri->withPath('xrpc/com.atproto.server.deleteSession')
+                $pdsUri->withPath('xrpc/com.atproto.server.deleteSession')
             );
 
         $headers = [
