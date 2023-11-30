4772e6cd0be5be69cd0085063666f0ee783b5bd7
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-30 07:50:53 -0600

    Update FetchLabels.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Com/Atproto/Temp/FetchLabels.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Com\Atproto\Temp;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * Fetch all labels from a labeler created after a certain date.
+ *
+ * @see \Ghostwriter\AtProtocol\Tests\Unit\Com\Atproto\Temp\FetchLabelsTest
+ */
+final readonly class FetchLabels
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+    ) {}
+
+    public function __invoke(
+        UriInterface $pdsUri,
+        ?int $since = null,
+        ?int $limit = null,
+    ): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'GET',
+                $pdsUri->withPath('xrpc/com.atproto.temp.fetchLabels')
+                    ->withQuery(http_build_query(array_filter([
+                    'since' => $since,
+                    'limit' => $limit,
+                ])))
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request;
+    }
+}
