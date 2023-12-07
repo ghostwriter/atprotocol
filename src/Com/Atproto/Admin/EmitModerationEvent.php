99b0067c47a3f1d87a09bbd06e2e43aefa663e7e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-12-06 20:57:04 -0600

    Update EmitModerationEvent.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Com/Atproto/Admin/EmitModerationEvent.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Com\Atproto\Admin;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * Take a moderation action on an actor.
+ *
+ * @see \Ghostwriter\AtProtocol\Tests\Unit\Com\Atproto\Admin\EmitModerationEventTest
+ */
+final readonly class EmitModerationEvent
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+        private StreamFactoryInterface $streamFactory,
+    ) {}
+
+    public function __invoke(
+        UriInterface $pdsUri,
+        string $event = null,
+        string $subject = null,
+        string $createdBy = null,
+        ?array $subjectBlobCids = null,
+    ): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'POST',
+                $pdsUri->withPath('xrpc/com.atproto.admin.emitModerationEvent')
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        $jsonBody = json_encode(array_filter([
+            'event' => $event,
+            'subject' => $subject,
+            'subjectBlobCids' => $subjectBlobCids,
+            'createdBy' => $createdBy,
+        ]));
+
+        if (false === $jsonBody){
+            throw new \RuntimeException('Failed to encode JSON');
+        }
+
+        return $request->withBody(
+            $this->streamFactory->createStream(
+                $jsonBody
+            )
+        );
+    }
+}
