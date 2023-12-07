9d004d680b35952cf833b698357b4d7725c6ae82
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-12-06 20:57:05 -0600

    Update QueryModerationEvents.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Com/Atproto/Admin/QueryModerationEvents.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Com\Atproto\Admin;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * List moderation events related to a subject.
+ *
+ * @see \Ghostwriter\AtProtocol\Tests\Unit\Com\Atproto\Admin\QueryModerationEventsTest
+ */
+final readonly class QueryModerationEvents
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+    ) {}
+
+    public function __invoke(
+        UriInterface $pdsUri,
+        ?array $types = null,
+        ?string $createdBy = null,
+        ?string $sortDirection = null,
+        ?string $subject = null,
+        ?bool $includeAllUserRecords = null,
+        ?int $limit = null,
+        ?string $cursor = null,
+    ): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'GET',
+                $pdsUri->withPath('xrpc/com.atproto.admin.queryModerationEvents')
+                    ->withQuery(http_build_query(array_filter([
+                    'types' => $types,
+                    'createdBy' => $createdBy,
+                    'sortDirection' => $sortDirection,
+                    'subject' => $subject,
+                    'includeAllUserRecords' => $includeAllUserRecords,
+                    'limit' => $limit,
+                    'cursor' => $cursor,
+                ])))
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request;
+    }
+}
