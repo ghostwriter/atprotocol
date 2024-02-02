cbe9b968dc7c054166d3177b4392fbafef2f8e12
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-02 17:58:40 -0600

    Update GetRelationships.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/App/Bsky/Graph/GetRelationships.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\App\Bsky\Graph;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * Enumerates public relationships between one account, and a list of other accounts
+ *
+ * @see \Ghostwriter\AtProtocol\Tests\Unit\App\Bsky\Graph\GetRelationshipsTest
+ */
+final readonly class GetRelationships
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+    ) {}
+
+    public function __invoke(
+        UriInterface $pdsUri,
+        string $actor = null,
+        ?array $others = null,
+    ): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'GET',
+                $pdsUri->withPath('xrpc/app.bsky.graph.getRelationships')
+                    ->withQuery(http_build_query(array_filter([
+                    'actor' => $actor,
+                    'others' => $others,
+                ])))
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request;
+    }
+}
