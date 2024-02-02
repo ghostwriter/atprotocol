ad6414e572403f98ed424f638b78e36132198069
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-02 17:58:42 -0600

    Update UpdateCommunicationTemplate.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Com/Atproto/Admin/UpdateCommunicationTemplate.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Com\Atproto\Admin;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * Administrative action to update an existing communication template. Allows passing partial fields to patch specific fields only.
+ *
+ * @see \Ghostwriter\AtProtocol\Tests\Unit\Com\Atproto\Admin\UpdateCommunicationTemplateTest
+ */
+final readonly class UpdateCommunicationTemplate
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+        private StreamFactoryInterface $streamFactory,
+    ) {}
+
+    public function __invoke(
+        UriInterface $pdsUri,
+        string $id = null,
+        ?string $name = null,
+        ?string $contentMarkdown = null,
+        ?string $subject = null,
+        ?string $updatedBy = null,
+        ?bool $disabled = null,
+    ): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'POST',
+                $pdsUri->withPath('xrpc/com.atproto.admin.updateCommunicationTemplate')
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        $jsonBody = json_encode(array_filter([
+            'id' => $id,
+            'name' => $name,
+            'contentMarkdown' => $contentMarkdown,
+            'subject' => $subject,
+            'updatedBy' => $updatedBy,
+            'disabled' => $disabled,
+        ]));
+
+        if (false === $jsonBody){
+            throw new \RuntimeException('Failed to encode JSON');
+        }
+
+        return $request->withBody(
+            $this->streamFactory->createStream(
+                $jsonBody
+            )
+        );
+    }
+}
