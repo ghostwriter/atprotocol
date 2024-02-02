959b5059baa76dad9617723c9d432dae17eb96c9
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-02 17:58:39 -0600

    Update GetTaggedSuggestions.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/App/Bsky/Unspecced/GetTaggedSuggestions.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\App\Bsky\Unspecced;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * Get a list of suggestions (feeds and users) tagged with categories
+ *
+ * @see \Ghostwriter\AtProtocol\Tests\Unit\App\Bsky\Unspecced\GetTaggedSuggestionsTest
+ */
+final readonly class GetTaggedSuggestions
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+    ) {}
+
+    public function __invoke(
+        UriInterface $pdsUri,
+    ): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'GET',
+                $pdsUri->withPath('xrpc/app.bsky.unspecced.getTaggedSuggestions')
+                    ->withQuery(http_build_query(array_filter([])))
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request;
+    }
+}
