735a0b584008cee15715177453bfc2372c3d3339
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-02 17:58:42 -0600

    Update DeleteCommunicationTemplate.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Com/Atproto/Admin/DeleteCommunicationTemplate.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Com\Atproto\Admin;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * Delete a communication template.
+ *
+ * @see \Ghostwriter\AtProtocol\Tests\Unit\Com\Atproto\Admin\DeleteCommunicationTemplateTest
+ */
+final readonly class DeleteCommunicationTemplate
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+        private StreamFactoryInterface $streamFactory,
+    ) {}
+
+    public function __invoke(
+        UriInterface $pdsUri,
+        string $id = null,
+    ): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'POST',
+                $pdsUri->withPath('xrpc/com.atproto.admin.deleteCommunicationTemplate')
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        $jsonBody = json_encode(array_filter([
+            'id' => $id,
+        ]));
+
+        if (false === $jsonBody){
+            throw new \RuntimeException('Failed to encode JSON');
+        }
+
+        return $request->withBody(
+            $this->streamFactory->createStream(
+                $jsonBody
+            )
+        );
+    }
+}
