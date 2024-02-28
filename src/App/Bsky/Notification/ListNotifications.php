c1b0e0b613917fc74a0a461c14284367d21cfc38
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-28 08:54:21 -0600

    Update ListNotifications.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Notification/ListNotifications.php
+++ src/App/Bsky/Notification/ListNotifications.php
@@ -9,7 +9,7 @@ use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
 /**
- * Get a list of notifications.
+ * Enumerate notifications for the requesting account. Requires auth.
  *
  * @see \Ghostwriter\AtProtocol\Tests\Unit\App\Bsky\Notification\ListNotificationsTest
  */
