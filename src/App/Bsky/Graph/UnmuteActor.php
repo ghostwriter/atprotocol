b465cbe295078b77e14a974f68cbd403424bfd68
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-28 08:54:22 -0600

    Update UnmuteActor.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Graph/UnmuteActor.php
+++ src/App/Bsky/Graph/UnmuteActor.php
@@ -10,7 +10,7 @@ use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
 
 /**
- * Unmute an actor by DID or handle.
+ * Unmutes the specified account. Requires auth.
  *
  * @see \Ghostwriter\AtProtocol\Tests\Unit\App\Bsky\Graph\UnmuteActorTest
  */
