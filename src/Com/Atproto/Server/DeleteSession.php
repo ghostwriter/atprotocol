d20e2d97ba1cbcb2894629ec59f5ceb05fc1cad9
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-28 08:54:25 -0600

    Update DeleteSession.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Server/DeleteSession.php
+++ src/Com/Atproto/Server/DeleteSession.php
@@ -10,7 +10,7 @@ use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
 
 /**
- * Delete the current session.
+ * Delete the current session. Requires auth.
  *
  * @see \Ghostwriter\AtProtocol\Tests\Unit\Com\Atproto\Server\DeleteSessionTest
  */
