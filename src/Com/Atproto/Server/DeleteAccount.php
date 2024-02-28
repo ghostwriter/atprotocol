f5a614957757fee7e2fdd01fcd26c0b04a431cd0
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-28 08:54:25 -0600

    Update DeleteAccount.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Server/DeleteAccount.php
+++ src/Com/Atproto/Server/DeleteAccount.php
@@ -10,7 +10,7 @@ use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
 
 /**
- * Delete an actor's account with a token and password.
+ * Delete an actor's account with a token and password. Can only be called after requesting a deletion token. Requires auth.
  *
  * @see \Ghostwriter\AtProtocol\Tests\Unit\Com\Atproto\Server\DeleteAccountTest
  */
