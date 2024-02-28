d8ea05096fab4f8c0537db059e32beb90a60bc26
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-28 08:54:26 -0600

    Update PutRecord.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Repo/PutRecord.php
+++ src/Com/Atproto/Repo/PutRecord.php
@@ -10,7 +10,7 @@ use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
 
 /**
- * Write a record, creating or updating it as needed.
+ * Write a repository record, creating or updating it as needed. Requires auth, implemented by PDS.
  *
  * @see \Ghostwriter\AtProtocol\Tests\Unit\Com\Atproto\Repo\PutRecordTest
  */
