2baaf0a606af3527115b056a984c8c20c9e5dd1e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-28 08:54:26 -0600

    Update ApplyWrites.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Repo/ApplyWrites.php
+++ src/Com/Atproto/Repo/ApplyWrites.php
@@ -10,7 +10,7 @@ use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
 
 /**
- * Apply a batch transaction of creates, updates, and deletes.
+ * Apply a batch transaction of repository creates, updates, and deletes. Requires auth, implemented by PDS.
  *
  * @see \Ghostwriter\AtProtocol\Tests\Unit\Com\Atproto\Repo\ApplyWritesTest
  */
