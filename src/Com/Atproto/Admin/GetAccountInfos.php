dcd37ec58a368cfc9d9006b7ba8f5451938ab365
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-28 09:11:10 -0600

    Update GetAccountInfos.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Admin/GetAccountInfos.php
+++ src/Com/Atproto/Admin/GetAccountInfos.php
@@ -11,7 +11,7 @@ use Psr\Http\Message\UriInterface;
 /**
  * Get details about some accounts.
  *
- * @see \Ghostwriter\AtProtocol\Tests\Unit\Com\Atproto\Admin\GetAccountInfosTest
+ * @see \Ghostwriter\AtProtocolTests\Unit\Com\Atproto\Admin\GetAccountInfosTest
  */
 final readonly class GetAccountInfos
 {
