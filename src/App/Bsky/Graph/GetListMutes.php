4d691cdaf4c1daeefd4fc14c609ae91f04a7aa20
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-28 09:11:04 -0600

    Update GetListMutes.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Graph/GetListMutes.php
+++ src/App/Bsky/Graph/GetListMutes.php
@@ -11,7 +11,7 @@ use Psr\Http\Message\UriInterface;
 /**
  * Enumerates mod lists that the requesting account (actor) currently has muted. Requires auth.
  *
- * @see \Ghostwriter\AtProtocol\Tests\Unit\App\Bsky\Graph\GetListMutesTest
+ * @see \Ghostwriter\AtProtocolTests\Unit\App\Bsky\Graph\GetListMutesTest
  */
 final readonly class GetListMutes
 {
