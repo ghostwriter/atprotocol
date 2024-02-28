8f5f31505a9f806188d2dc0ed3a3d89ff6410a2c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-28 08:54:26 -0600

    Update CreateRecord.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Repo/CreateRecord.php
+++ src/Com/Atproto/Repo/CreateRecord.php
@@ -10,7 +10,7 @@ use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
 
 /**
- * Create a new record.
+ * Create a single new repository record. Requires auth, implemented by PDS.
  *
  * @see \Ghostwriter\AtProtocol\Tests\Unit\Com\Atproto\Repo\CreateRecordTest
  */
