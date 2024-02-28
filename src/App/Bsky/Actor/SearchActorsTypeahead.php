7dea0231244e6a3811280d1f2c135bc26cf269ba
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-28 09:11:05 -0600

    Update SearchActorsTypeahead.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Actor/SearchActorsTypeahead.php
+++ src/App/Bsky/Actor/SearchActorsTypeahead.php
@@ -11,7 +11,7 @@ use Psr\Http\Message\UriInterface;
 /**
  * Find actor suggestions for a prefix search term. Expected use is for auto-completion during text field entry. Does not require auth.
  *
- * @see \Ghostwriter\AtProtocol\Tests\Unit\App\Bsky\Actor\SearchActorsTypeaheadTest
+ * @see \Ghostwriter\AtProtocolTests\Unit\App\Bsky\Actor\SearchActorsTypeaheadTest
  */
 final readonly class SearchActorsTypeahead
 {
