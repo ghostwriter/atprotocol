5c5be8f813abc00d3fea2e02532e64abf80b1dd6
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-28 09:11:05 -0600

    Update SearchActors.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Actor/SearchActors.php
+++ src/App/Bsky/Actor/SearchActors.php
@@ -11,7 +11,7 @@ use Psr\Http\Message\UriInterface;
 /**
  * Find actors (profiles) matching search criteria. Does not require auth.
  *
- * @see \Ghostwriter\AtProtocol\Tests\Unit\App\Bsky\Actor\SearchActorsTest
+ * @see \Ghostwriter\AtProtocolTests\Unit\App\Bsky\Actor\SearchActorsTest
  */
 final readonly class SearchActors
 {
