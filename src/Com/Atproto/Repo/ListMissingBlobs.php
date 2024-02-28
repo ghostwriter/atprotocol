fb6ef93db85e9bf8fa8b3d5a176341cf0fa9548a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-28 08:54:26 -0600

    Update ListMissingBlobs.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Com/Atproto/Repo/ListMissingBlobs.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Com\Atproto\Repo;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * Returns a list of missing blobs for the requesting account. Intended to be used in the account migration flow.
+ *
+ * @see \Ghostwriter\AtProtocol\Tests\Unit\Com\Atproto\Repo\ListMissingBlobsTest
+ */
+final readonly class ListMissingBlobs
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+    ) {}
+
+    public function __invoke(
+        UriInterface $pdsUri,
+        ?int $limit = null,
+        ?string $cursor = null,
+    ): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'GET',
+                $pdsUri->withPath('xrpc/com.atproto.repo.listMissingBlobs')
+                    ->withQuery(http_build_query(array_filter([
+                    'limit' => $limit,
+                    'cursor' => $cursor,
+                ])))
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request;
+    }
+}
