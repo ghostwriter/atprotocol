4f20e5039f84f33f09596fc15a357428f184c182
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-28 08:54:24 -0600

    Update SignPlcOperation.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Com/Atproto/Identity/SignPlcOperation.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Com\Atproto\Identity;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * Signs a PLC operation to update some value(s) in the requesting DID's document.
+ *
+ * @see \Ghostwriter\AtProtocol\Tests\Unit\Com\Atproto\Identity\SignPlcOperationTest
+ */
+final readonly class SignPlcOperation
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+        private StreamFactoryInterface $streamFactory,
+    ) {}
+
+    public function __invoke(
+        UriInterface $pdsUri,
+        ?string $token = null,
+        ?array $rotationKeys = null,
+        ?array $alsoKnownAs = null,
+        ?string $verificationMethods = null,
+        ?string $services = null,
+    ): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'POST',
+                $pdsUri->withPath('xrpc/com.atproto.identity.signPlcOperation')
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        $jsonBody = json_encode(array_filter([
+            'token' => $token,
+            'rotationKeys' => $rotationKeys,
+            'alsoKnownAs' => $alsoKnownAs,
+            'verificationMethods' => $verificationMethods,
+            'services' => $services,
+        ]));
+
+        if (false === $jsonBody){
+            throw new \RuntimeException('Failed to encode JSON');
+        }
+
+        return $request->withBody(
+            $this->streamFactory->createStream(
+                $jsonBody
+            )
+        );
+    }
+}
