5e4551a2890d297c9d3cf665fda97976eb6b47b8
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-03-13 22:57:03 -0500

    Update QueryEvents.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Tools/Ozone/Moderation/QueryEvents.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Tools\Ozone\Moderation;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * List moderation events related to a subject.
+ *
+ * @see \Ghostwriter\AtProtocolTests\Unit\Tools\Ozone\Moderation\QueryEventsTest
+ */
+final readonly class QueryEvents
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+    ) {}
+
+    public function __invoke(
+        UriInterface $pdsUri,
+        ?array $types = null,
+        ?string $createdBy = null,
+        ?string $sortDirection = null,
+        ?string $createdAfter = null,
+        ?string $createdBefore = null,
+        ?string $subject = null,
+        ?bool $includeAllUserRecords = null,
+        ?int $limit = null,
+        ?bool $hasComment = null,
+        ?string $comment = null,
+        ?array $addedLabels = null,
+        ?array $removedLabels = null,
+        ?array $addedTags = null,
+        ?array $removedTags = null,
+        ?array $reportTypes = null,
+        ?string $cursor = null,
+    ): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'GET',
+                $pdsUri->withPath('xrpc/tools.ozone.moderation.queryEvents')
+                    ->withQuery(http_build_query(array_filter([
+                    'types' => $types,
+                    'createdBy' => $createdBy,
+                    'sortDirection' => $sortDirection,
+                    'createdAfter' => $createdAfter,
+                    'createdBefore' => $createdBefore,
+                    'subject' => $subject,
+                    'includeAllUserRecords' => $includeAllUserRecords,
+                    'limit' => $limit,
+                    'hasComment' => $hasComment,
+                    'comment' => $comment,
+                    'addedLabels' => $addedLabels,
+                    'removedLabels' => $removedLabels,
+                    'addedTags' => $addedTags,
+                    'removedTags' => $removedTags,
+                    'reportTypes' => $reportTypes,
+                    'cursor' => $cursor,
+                ])))
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request;
+    }
+}
