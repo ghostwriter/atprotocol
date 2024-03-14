cccfdc505e0376627e8098ebd99b433360521aa1
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-03-13 22:57:03 -0500

    Update SearchRepos.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Tools/Ozone/Moderation/SearchRepos.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Tools\Ozone\Moderation;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * Find repositories based on a search term.
+ *
+ * @see \Ghostwriter\AtProtocolTests\Unit\Tools\Ozone\Moderation\SearchReposTest
+ */
+final readonly class SearchRepos
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+    ) {}
+
+    public function __invoke(
+        UriInterface $pdsUri,
+        ?string $q = null,
+        ?int $limit = null,
+        ?string $cursor = null,
+    ): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'GET',
+                $pdsUri->withPath('xrpc/tools.ozone.moderation.searchRepos')
+                    ->withQuery(http_build_query(array_filter([
+                    'q' => $q,
+                    'limit' => $limit,
+                    'cursor' => $cursor,
+                ])))
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request;
+    }
+}
