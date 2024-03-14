acd65338a6aa82c665348b55614ce0d836c14b0c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-03-13 22:57:03 -0500

    Update GetRecord.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Tools/Ozone/Moderation/GetRecord.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Tools\Ozone\Moderation;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * Get details about a record.
+ *
+ * @see \Ghostwriter\AtProtocolTests\Unit\Tools\Ozone\Moderation\GetRecordTest
+ */
+final readonly class GetRecord
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+    ) {}
+
+    public function __invoke(
+        UriInterface $pdsUri,
+        string $uri = null,
+        ?string $cid = null,
+    ): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'GET',
+                $pdsUri->withPath('xrpc/tools.ozone.moderation.getRecord')
+                    ->withQuery(http_build_query(array_filter([
+                    'uri' => $uri,
+                    'cid' => $cid,
+                ])))
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request;
+    }
+}
