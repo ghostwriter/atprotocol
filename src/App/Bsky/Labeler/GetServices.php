2ad42cced7edfd65cb1800cbecdd8d202bbbf3d5
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-03-13 22:57:03 -0500

    Update GetServices.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/App/Bsky/Labeler/GetServices.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\App\Bsky\Labeler;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * Get information about a list of labeler services.
+ *
+ * @see \Ghostwriter\AtProtocolTests\Unit\App\Bsky\Labeler\GetServicesTest
+ */
+final readonly class GetServices
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+    ) {}
+
+    public function __invoke(
+        UriInterface $pdsUri,
+        array $dids = null,
+        ?bool $detailed = null,
+    ): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'GET',
+                $pdsUri->withPath('xrpc/app.bsky.labeler.getServices')
+                    ->withQuery(http_build_query(array_filter([
+                    'dids' => $dids,
+                    'detailed' => $detailed,
+                ])))
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request;
+    }
+}
