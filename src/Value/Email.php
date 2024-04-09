9eb7672eceae4507ce278da052ffb70e8963a586
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 08:21:18 -0500

    Update Email.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Value/Email.php
+++ src/Value/Email.php
@@ -7,19 +7,22 @@ namespace Ghostwriter\AtProtocol\Value;
 use Stringable;
 use InvalidArgumentException;
 
+use function filter_var;
+
+use const FILTER_VALIDATE_EMAIL;
+
 final readonly class Email implements Stringable
 {
     public function __construct(
         private readonly string $email,
     ) {
         if (! filter_var($this->email, FILTER_VALIDATE_EMAIL)) {
-            throw new InvalidArgumentException(
-                'Invalid Email: ' . $this->email
-            );
+            throw new InvalidArgumentException('Invalid Email: ' . $this->email);
         }
     }
 
-    public function __toString(): string {
+    public function __toString(): string
+    {
         return $this->email;
     }
 }
