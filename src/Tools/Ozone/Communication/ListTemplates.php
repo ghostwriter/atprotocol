4d471fbf02a5fe3b87614f370f8e669890b631ae
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 08:21:22 -0500

    Update ListTemplates.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Tools/Ozone/Communication/ListTemplates.php
+++ src/Tools/Ozone/Communication/ListTemplates.php
@@ -8,6 +8,9 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
+use function array_filter;
+use function http_build_query;
+
 /**
  * Get list of all communication templates.
  *
@@ -19,9 +22,7 @@ final readonly class ListTemplates
         private RequestFactoryInterface $requestFactory,
     ) {}
 
-    public function __invoke(
-        UriInterface $pdsUri,
-    ): RequestInterface
+    public function __invoke(UriInterface $pdsUri): RequestInterface
     {
         $request = $this->requestFactory
             ->createRequest(
