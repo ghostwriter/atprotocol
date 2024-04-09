0ed3116f080a347b56afcb5dbf262322f7fae1e2
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 08:21:26 -0500

    Update GetRepo.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Sync/GetRepo.php
+++ src/Com/Atproto/Sync/GetRepo.php
@@ -8,6 +8,9 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
+use function array_filter;
+use function http_build_query;
+
 /**
  * Download a repository export as CAR file. Optionally only a 'diff' since a previous revision. Does not require auth; implemented by PDS.
  *
@@ -19,20 +22,16 @@ final readonly class GetRepo
         private RequestFactoryInterface $requestFactory,
     ) {}
 
-    public function __invoke(
-        UriInterface $pdsUri,
-        string $did = null,
-        ?string $since = null,
-    ): RequestInterface
+    public function __invoke(UriInterface $pdsUri, string $did = null, ?string $since = null): RequestInterface
     {
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
                 $pdsUri->withPath('xrpc/com.atproto.sync.getRepo')
                     ->withQuery(http_build_query(array_filter([
-                    'did' => $did,
-                    'since' => $since,
-                ])))
+                        'did' => $did,
+                        'since' => $since,
+                    ])))
             );
 
         $headers = [
