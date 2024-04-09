3d1a845c897805dc8bd54edbfb10ba36fa6f381d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 08:21:29 -0500

    Update GetServiceAuth.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Server/GetServiceAuth.php
+++ src/Com/Atproto/Server/GetServiceAuth.php
@@ -8,6 +8,9 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
+use function array_filter;
+use function http_build_query;
+
 /**
  * Get a signed token on behalf of the requesting DID for the requested service.
  *
@@ -19,18 +22,15 @@ final readonly class GetServiceAuth
         private RequestFactoryInterface $requestFactory,
     ) {}
 
-    public function __invoke(
-        UriInterface $pdsUri,
-        string $aud = null,
-    ): RequestInterface
+    public function __invoke(UriInterface $pdsUri, string $aud = null): RequestInterface
     {
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
                 $pdsUri->withPath('xrpc/com.atproto.server.getServiceAuth')
                     ->withQuery(http_build_query(array_filter([
-                    'aud' => $aud,
-                ])))
+                        'aud' => $aud,
+                    ])))
             );
 
         $headers = [
