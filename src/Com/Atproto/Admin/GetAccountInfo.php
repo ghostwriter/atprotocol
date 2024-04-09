bb8fa99efe029f87eb85ef64d584335178c3eb14
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 08:21:39 -0500

    Update GetAccountInfo.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Admin/GetAccountInfo.php
+++ src/Com/Atproto/Admin/GetAccountInfo.php
@@ -8,6 +8,9 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
+use function array_filter;
+use function http_build_query;
+
 /**
  * Get details about an account.
  *
@@ -19,18 +22,15 @@ final readonly class GetAccountInfo
         private RequestFactoryInterface $requestFactory,
     ) {}
 
-    public function __invoke(
-        UriInterface $pdsUri,
-        string $did = null,
-    ): RequestInterface
+    public function __invoke(UriInterface $pdsUri, string $did = null): RequestInterface
     {
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
                 $pdsUri->withPath('xrpc/com.atproto.admin.getAccountInfo')
                     ->withQuery(http_build_query(array_filter([
-                    'did' => $did,
-                ])))
+                        'did' => $did,
+                    ])))
             );
 
         $headers = [
