a1171fb31ae0781b168b613fcfc5f03df475fad1
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 08:21:35 -0500

    Update GetRecommendedDidCredentials.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Identity/GetRecommendedDidCredentials.php
+++ src/Com/Atproto/Identity/GetRecommendedDidCredentials.php
@@ -8,6 +8,9 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
+use function array_filter;
+use function http_build_query;
+
 /**
  * Describe the credentials that should be included in the DID doc of an account that is migrating to this service.
  *
@@ -19,9 +22,7 @@ final readonly class GetRecommendedDidCredentials
         private RequestFactoryInterface $requestFactory,
     ) {}
 
-    public function __invoke(
-        UriInterface $pdsUri,
-    ): RequestInterface
+    public function __invoke(UriInterface $pdsUri): RequestInterface
     {
         $request = $this->requestFactory
             ->createRequest(
