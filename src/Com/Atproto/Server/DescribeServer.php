17d738845add35a82a14e3e605e94ec79ecc1d67
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 08:21:29 -0500

    Update DescribeServer.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Server/DescribeServer.php
+++ src/Com/Atproto/Server/DescribeServer.php
@@ -8,6 +8,9 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
+use function array_filter;
+use function http_build_query;
+
 /**
  * Describes the server's account creation requirements and capabilities. Implemented by PDS.
  *
@@ -19,9 +22,7 @@ final readonly class DescribeServer
         private RequestFactoryInterface $requestFactory,
     ) {}
 
-    public function __invoke(
-        UriInterface $pdsUri,
-    ): RequestInterface
+    public function __invoke(UriInterface $pdsUri): RequestInterface
     {
         $request = $this->requestFactory
             ->createRequest(
