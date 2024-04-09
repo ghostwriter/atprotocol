e811a497ba225fe0fe0029d2ce46ad21c83cd060
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 08:21:41 -0500

    Update MuteActor.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Graph/MuteActor.php
+++ src/App/Bsky/Graph/MuteActor.php
@@ -8,6 +8,10 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
+use RuntimeException;
+
+use function array_filter;
+use function json_encode;
 
 /**
  * Creates a mute relationship for the specified account. Mutes are private in Bluesky. Requires auth.
@@ -21,16 +25,10 @@ final readonly class MuteActor
         private StreamFactoryInterface $streamFactory,
     ) {}
 
-    public function __invoke(
-        UriInterface $pdsUri,
-        string $actor = null,
-    ): RequestInterface
+    public function __invoke(UriInterface $pdsUri, string $actor = null): RequestInterface
     {
         $request = $this->requestFactory
-            ->createRequest(
-                'POST',
-                $pdsUri->withPath('xrpc/app.bsky.graph.muteActor')
-            );
+            ->createRequest('POST', $pdsUri->withPath('xrpc/app.bsky.graph.muteActor'));
 
         $headers = [
             'Accept' => 'application/json',
@@ -45,14 +43,10 @@ final readonly class MuteActor
             'actor' => $actor,
         ]));
 
-        if (false === $jsonBody){
-            throw new \RuntimeException('Failed to encode JSON');
+        if ($jsonBody === false) {
+            throw new RuntimeException('Failed to encode JSON');
         }
 
-        return $request->withBody(
-            $this->streamFactory->createStream(
-                $jsonBody
-            )
-        );
+        return $request->withBody($this->streamFactory->createStream($jsonBody));
     }
 }
