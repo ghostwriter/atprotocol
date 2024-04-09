8469c8880a1dd2b3db2fad2e6d643aad79a90361
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 08:21:42 -0500

    Update GetRepostedBy.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Feed/GetRepostedBy.php
+++ src/App/Bsky/Feed/GetRepostedBy.php
@@ -8,6 +8,9 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
+use function array_filter;
+use function http_build_query;
+
 /**
  * Get a list of reposts for a given post.
  *
@@ -25,18 +28,17 @@ final readonly class GetRepostedBy
         ?string $cid = null,
         ?int $limit = null,
         ?string $cursor = null,
-    ): RequestInterface
-    {
+    ): RequestInterface {
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
                 $pdsUri->withPath('xrpc/app.bsky.feed.getRepostedBy')
                     ->withQuery(http_build_query(array_filter([
-                    'uri' => $uri,
-                    'cid' => $cid,
-                    'limit' => $limit,
-                    'cursor' => $cursor,
-                ])))
+                        'uri' => $uri,
+                        'cid' => $cid,
+                        'limit' => $limit,
+                        'cursor' => $cursor,
+                    ])))
             );
 
         $headers = [
