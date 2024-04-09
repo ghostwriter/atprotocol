a18938b924b3caa326d55a290fae3482b4e29f8b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 08:21:31 -0500

    Update ConfirmEmail.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Server/ConfirmEmail.php
+++ src/Com/Atproto/Server/ConfirmEmail.php
@@ -8,6 +8,10 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
+use RuntimeException;
+
+use function array_filter;
+use function json_encode;
 
 /**
  * Confirm an email using a token from com.atproto.server.requestEmailConfirmation.
@@ -21,17 +25,10 @@ final readonly class ConfirmEmail
         private StreamFactoryInterface $streamFactory,
     ) {}
 
-    public function __invoke(
-        UriInterface $pdsUri,
-        string $email = null,
-        string $token = null,
-    ): RequestInterface
+    public function __invoke(UriInterface $pdsUri, string $email = null, string $token = null): RequestInterface
     {
         $request = $this->requestFactory
-            ->createRequest(
-                'POST',
-                $pdsUri->withPath('xrpc/com.atproto.server.confirmEmail')
-            );
+            ->createRequest('POST', $pdsUri->withPath('xrpc/com.atproto.server.confirmEmail'));
 
         $headers = [
             'Accept' => 'application/json',
@@ -47,14 +44,10 @@ final readonly class ConfirmEmail
             'token' => $token,
         ]));
 
-        if (false === $jsonBody){
-            throw new \RuntimeException('Failed to encode JSON');
+        if ($jsonBody === false) {
+            throw new RuntimeException('Failed to encode JSON');
         }
 
-        return $request->withBody(
-            $this->streamFactory->createStream(
-                $jsonBody
-            )
-        );
+        return $request->withBody($this->streamFactory->createStream($jsonBody));
     }
 }
