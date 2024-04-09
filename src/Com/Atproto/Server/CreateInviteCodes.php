dbed752e7cc0d919576470a42ab2a3cbe4de7445
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 08:21:30 -0500

    Update CreateInviteCodes.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Server/CreateInviteCodes.php
+++ src/Com/Atproto/Server/CreateInviteCodes.php
@@ -8,6 +8,10 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
+use RuntimeException;
+
+use function array_filter;
+use function json_encode;
 
 /**
  * Create invite codes.
@@ -26,13 +30,9 @@ final readonly class CreateInviteCodes
         int $codeCount = null,
         int $useCount = null,
         ?array $forAccounts = null,
-    ): RequestInterface
-    {
+    ): RequestInterface {
         $request = $this->requestFactory
-            ->createRequest(
-                'POST',
-                $pdsUri->withPath('xrpc/com.atproto.server.createInviteCodes')
-            );
+            ->createRequest('POST', $pdsUri->withPath('xrpc/com.atproto.server.createInviteCodes'));
 
         $headers = [
             'Accept' => 'application/json',
@@ -49,14 +49,10 @@ final readonly class CreateInviteCodes
             'forAccounts' => $forAccounts,
         ]));
 
-        if (false === $jsonBody){
-            throw new \RuntimeException('Failed to encode JSON');
+        if ($jsonBody === false) {
+            throw new RuntimeException('Failed to encode JSON');
         }
 
-        return $request->withBody(
-            $this->streamFactory->createStream(
-                $jsonBody
-            )
-        );
+        return $request->withBody($this->streamFactory->createStream($jsonBody));
     }
 }
