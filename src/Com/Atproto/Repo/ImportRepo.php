d15d6b738b74a954ed72dbd3ca18966b439008c5
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 08:21:33 -0500

    Update ImportRepo.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Repo/ImportRepo.php
+++ src/Com/Atproto/Repo/ImportRepo.php
@@ -8,6 +8,10 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
+use RuntimeException;
+
+use function array_filter;
+use function json_encode;
 
 /**
  * Import a repo in the form of a CAR file. Requires Content-Length HTTP header to be set.
@@ -21,15 +25,10 @@ final readonly class ImportRepo
         private StreamFactoryInterface $streamFactory,
     ) {}
 
-    public function __invoke(
-        UriInterface $pdsUri,
-    ): RequestInterface
+    public function __invoke(UriInterface $pdsUri): RequestInterface
     {
         $request = $this->requestFactory
-            ->createRequest(
-                'POST',
-                $pdsUri->withPath('xrpc/com.atproto.repo.importRepo')
-            );
+            ->createRequest('POST', $pdsUri->withPath('xrpc/com.atproto.repo.importRepo'));
 
         $headers = [
             'Accept' => 'application/vnd.ipld.car',
@@ -40,18 +39,12 @@ final readonly class ImportRepo
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = json_encode(array_filter([
-            
-        ]));
+        $jsonBody = json_encode(array_filter([]));
 
-        if (false === $jsonBody){
-            throw new \RuntimeException('Failed to encode JSON');
+        if ($jsonBody === false) {
+            throw new RuntimeException('Failed to encode JSON');
         }
 
-        return $request->withBody(
-            $this->streamFactory->createStream(
-                $jsonBody
-            )
-        );
+        return $request->withBody($this->streamFactory->createStream($jsonBody));
     }
 }
