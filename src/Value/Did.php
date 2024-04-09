ee562ec450c2e71186e3ee5fc0683d85d0297c4a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 08:21:18 -0500

    Update Did.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Value/Did.php
+++ src/Value/Did.php
@@ -8,7 +8,9 @@ use JsonSerializable;
 use Stringable;
 use InvalidArgumentException;
 
-final readonly class Did implements Stringable , JsonSerializable
+use function preg_match;
+
+final readonly class Did implements JsonSerializable, Stringable
 {
     public function __construct(
         private string $did,
@@ -16,21 +18,18 @@ final readonly class Did implements Stringable , JsonSerializable
         // should we split the value into parts and validate each part?
         // and store the parts in the object
         if (
-            !preg_match(
-                '#^did:[a-z]+:[a-zA-Z0-9._:%-]*[a-zA-Z0-9._-]$#',
-                $this->did
-            )
+            ! preg_match('#^did:[a-z]+:[a-zA-Z0-9._:%-]*[a-zA-Z0-9._-]$#', $this->did)
         ) {
             // Todo: create custom exception
-            throw new InvalidArgumentException(
-                'Invalid DID: ' . $this->did
-            );
+            throw new InvalidArgumentException('Invalid DID: ' . $this->did);
         }
     }
 
     public function jsonSerialize(): array
     {
-        return ['did' => $this->did];
+        return [
+            'did' => $this->did,
+        ];
     }
 
     public function __toString(): string
