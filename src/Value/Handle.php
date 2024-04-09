1eef777f221e0b813bf508694268ed3ab12bd652
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 08:21:18 -0500

    Update Handle.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Value/Handle.php
+++ src/Value/Handle.php
@@ -7,20 +7,20 @@ namespace Ghostwriter\AtProtocol\Value;
 use Stringable;
 use InvalidArgumentException;
 
+use function preg_match;
+
 final readonly class Handle implements Stringable
 {
     public function __construct(
         private readonly string $handle,
     ) {
         if (
-            !preg_match(
+            ! preg_match(
                 '#^([a-zA-Z0-9]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?$#',
                 $this->handle
             )
         ) {
-            throw new InvalidArgumentException(
-                'Invalid handle: ' . $this->handle
-            );
+            throw new InvalidArgumentException('Invalid handle: ' . $this->handle);
         }
     }
 
