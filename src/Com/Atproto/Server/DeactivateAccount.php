3622c1f80c21b463e07db5fbee5e0eebeb893819
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 08:21:30 -0500

    Update DeactivateAccount.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Server/DeactivateAccount.php
+++ src/Com/Atproto/Server/DeactivateAccount.php
@@ -8,6 +8,10 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
+use RuntimeException;
+
+use function array_filter;
+use function json_encode;
 
 /**
  * Deactivates a currently active account. Stops serving of repo, and future writes to repo until reactivated. Used to finalize account migration with the old host after the account has been activated on the new host.
@@ -21,16 +25,10 @@ final readonly class DeactivateAccount
         private StreamFactoryInterface $streamFactory,
     ) {}
 
-    public function __invoke(
-        UriInterface $pdsUri,
-        ?string $deleteAfter = null,
-    ): RequestInterface
+    public function __invoke(UriInterface $pdsUri, ?string $deleteAfter = null): RequestInterface
     {
         $request = $this->requestFactory
-            ->createRequest(
-                'POST',
-                $pdsUri->withPath('xrpc/com.atproto.server.deactivateAccount')
-            );
+            ->createRequest('POST', $pdsUri->withPath('xrpc/com.atproto.server.deactivateAccount'));
 
         $headers = [
             'Accept' => 'application/json',
@@ -45,14 +43,10 @@ final readonly class DeactivateAccount
             'deleteAfter' => $deleteAfter,
         ]));
 
-        if (false === $jsonBody){
-            throw new \RuntimeException('Failed to encode JSON');
+        if ($jsonBody === false) {
+            throw new RuntimeException('Failed to encode JSON');
         }
 
-        return $request->withBody(
-            $this->streamFactory->createStream(
-                $jsonBody
-            )
-        );
+        return $request->withBody($this->streamFactory->createStream($jsonBody));
     }
 }
