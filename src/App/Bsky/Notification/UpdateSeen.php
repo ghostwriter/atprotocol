54f2559eac8b6f1b6d2a8f5cd5fafb68e617e4e3
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 08:21:40 -0500

    Update UpdateSeen.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Notification/UpdateSeen.php
+++ src/App/Bsky/Notification/UpdateSeen.php
@@ -8,6 +8,10 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
+use RuntimeException;
+
+use function array_filter;
+use function json_encode;
 
 /**
  * Notify server that the requesting account has seen notifications. Requires auth.
@@ -21,16 +25,10 @@ final readonly class UpdateSeen
         private StreamFactoryInterface $streamFactory,
     ) {}
 
-    public function __invoke(
-        UriInterface $pdsUri,
-        string $seenAt = null,
-    ): RequestInterface
+    public function __invoke(UriInterface $pdsUri, string $seenAt = null): RequestInterface
     {
         $request = $this->requestFactory
-            ->createRequest(
-                'POST',
-                $pdsUri->withPath('xrpc/app.bsky.notification.updateSeen')
-            );
+            ->createRequest('POST', $pdsUri->withPath('xrpc/app.bsky.notification.updateSeen'));
 
         $headers = [
             'Accept' => 'application/json',
@@ -45,14 +43,10 @@ final readonly class UpdateSeen
             'seenAt' => $seenAt,
         ]));
 
-        if (false === $jsonBody){
-            throw new \RuntimeException('Failed to encode JSON');
+        if ($jsonBody === false) {
+            throw new RuntimeException('Failed to encode JSON');
         }
 
-        return $request->withBody(
-            $this->streamFactory->createStream(
-                $jsonBody
-            )
-        );
+        return $request->withBody($this->streamFactory->createStream($jsonBody));
     }
 }
