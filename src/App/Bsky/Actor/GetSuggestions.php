02adaa450a154bf0a9956d68522c2209590b9af2
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 08:21:44 -0500

    Update GetSuggestions.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Actor/GetSuggestions.php
+++ src/App/Bsky/Actor/GetSuggestions.php
@@ -8,6 +8,9 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
+use function array_filter;
+use function http_build_query;
+
 /**
  * Get a list of suggested actors. Expected use is discovery of accounts to follow during new account onboarding.
  *
@@ -19,20 +22,16 @@ final readonly class GetSuggestions
         private RequestFactoryInterface $requestFactory,
     ) {}
 
-    public function __invoke(
-        UriInterface $pdsUri,
-        ?int $limit = null,
-        ?string $cursor = null,
-    ): RequestInterface
+    public function __invoke(UriInterface $pdsUri, ?int $limit = null, ?string $cursor = null): RequestInterface
     {
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
                 $pdsUri->withPath('xrpc/app.bsky.actor.getSuggestions')
                     ->withQuery(http_build_query(array_filter([
-                    'limit' => $limit,
-                    'cursor' => $cursor,
-                ])))
+                        'limit' => $limit,
+                        'cursor' => $cursor,
+                    ])))
             );
 
         $headers = [
