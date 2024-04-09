c3e8eb84d7bc41584630a432c4a86b69bcc196e8
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 08:21:41 -0500

    Update GetListMutes.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Graph/GetListMutes.php
+++ src/App/Bsky/Graph/GetListMutes.php
@@ -8,6 +8,9 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
+use function array_filter;
+use function http_build_query;
+
 /**
  * Enumerates mod lists that the requesting account (actor) currently has muted. Requires auth.
  *
@@ -19,20 +22,16 @@ final readonly class GetListMutes
         private RequestFactoryInterface $requestFactory,
     ) {}
 
-    public function __invoke(
-        UriInterface $pdsUri,
-        ?int $limit = null,
-        ?string $cursor = null,
-    ): RequestInterface
+    public function __invoke(UriInterface $pdsUri, ?int $limit = null, ?string $cursor = null): RequestInterface
     {
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
                 $pdsUri->withPath('xrpc/app.bsky.graph.getListMutes')
                     ->withQuery(http_build_query(array_filter([
-                    'limit' => $limit,
-                    'cursor' => $cursor,
-                ])))
+                        'limit' => $limit,
+                        'cursor' => $cursor,
+                    ])))
             );
 
         $headers = [
