13e3174ad848007a2584301add0db0b46a4d35c3
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 08:21:25 -0500

    Update CheckSignupQueue.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Temp/CheckSignupQueue.php
+++ src/Com/Atproto/Temp/CheckSignupQueue.php
@@ -8,6 +8,9 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
+use function array_filter;
+use function http_build_query;
+
 /**
  * Check accounts location in signup queue.
  *
@@ -19,9 +22,7 @@ final readonly class CheckSignupQueue
         private RequestFactoryInterface $requestFactory,
     ) {}
 
-    public function __invoke(
-        UriInterface $pdsUri,
-    ): RequestInterface
+    public function __invoke(UriInterface $pdsUri): RequestInterface
     {
         $request = $this->requestFactory
             ->createRequest(
