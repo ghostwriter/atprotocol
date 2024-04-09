f10a3b450245d18e3e0189865b9f583dcd0bf86e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 08:21:28 -0500

    Update RevokeAppPassword.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Server/RevokeAppPassword.php
+++ src/Com/Atproto/Server/RevokeAppPassword.php
@@ -8,6 +8,10 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
+use RuntimeException;
+
+use function array_filter;
+use function json_encode;
 
 /**
  * Revoke an App Password by name.
@@ -21,16 +25,10 @@ final readonly class RevokeAppPassword
         private StreamFactoryInterface $streamFactory,
     ) {}
 
-    public function __invoke(
-        UriInterface $pdsUri,
-        string $name = null,
-    ): RequestInterface
+    public function __invoke(UriInterface $pdsUri, string $name = null): RequestInterface
     {
         $request = $this->requestFactory
-            ->createRequest(
-                'POST',
-                $pdsUri->withPath('xrpc/com.atproto.server.revokeAppPassword')
-            );
+            ->createRequest('POST', $pdsUri->withPath('xrpc/com.atproto.server.revokeAppPassword'));
 
         $headers = [
             'Accept' => 'application/json',
@@ -45,14 +43,10 @@ final readonly class RevokeAppPassword
             'name' => $name,
         ]));
 
-        if (false === $jsonBody){
-            throw new \RuntimeException('Failed to encode JSON');
+        if ($jsonBody === false) {
+            throw new RuntimeException('Failed to encode JSON');
         }
 
-        return $request->withBody(
-            $this->streamFactory->createStream(
-                $jsonBody
-            )
-        );
+        return $request->withBody($this->streamFactory->createStream($jsonBody));
     }
 }
