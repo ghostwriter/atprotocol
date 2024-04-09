9cde3e04dcd06ffb3a42161c29a7c6c2656d9b2e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 08:21:30 -0500

    Update CreateSession.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Server/CreateSession.php
+++ src/Com/Atproto/Server/CreateSession.php
@@ -8,6 +8,10 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
+use RuntimeException;
+
+use function array_filter;
+use function json_encode;
 
 /**
  * Create an authentication session.
@@ -25,13 +29,9 @@ final readonly class CreateSession
         UriInterface $pdsUri,
         string $identifier = null,
         string $password = null,
-    ): RequestInterface
-    {
+    ): RequestInterface {
         $request = $this->requestFactory
-            ->createRequest(
-                'POST',
-                $pdsUri->withPath('xrpc/com.atproto.server.createSession')
-            );
+            ->createRequest('POST', $pdsUri->withPath('xrpc/com.atproto.server.createSession'));
 
         $headers = [
             'Accept' => 'application/json',
@@ -47,14 +47,10 @@ final readonly class CreateSession
             'password' => $password,
         ]));
 
-        if (false === $jsonBody){
-            throw new \RuntimeException('Failed to encode JSON');
+        if ($jsonBody === false) {
+            throw new RuntimeException('Failed to encode JSON');
         }
 
-        return $request->withBody(
-            $this->streamFactory->createStream(
-                $jsonBody
-            )
-        );
+        return $request->withBody($this->streamFactory->createStream($jsonBody));
     }
 }
