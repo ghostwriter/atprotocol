b5a00ab80aa106df306e7cbc684d0bfd5c427a27
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 08:21:27 -0500

    Update GetBlob.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Sync/GetBlob.php
+++ src/Com/Atproto/Sync/GetBlob.php
@@ -8,6 +8,9 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
+use function array_filter;
+use function http_build_query;
+
 /**
  * Get a blob associated with a given account. Returns the full blob as originally uploaded. Does not require auth; implemented by PDS.
  *
@@ -19,20 +22,16 @@ final readonly class GetBlob
         private RequestFactoryInterface $requestFactory,
     ) {}
 
-    public function __invoke(
-        UriInterface $pdsUri,
-        string $did = null,
-        string $cid = null,
-    ): RequestInterface
+    public function __invoke(UriInterface $pdsUri, string $did = null, string $cid = null): RequestInterface
     {
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
                 $pdsUri->withPath('xrpc/com.atproto.sync.getBlob')
                     ->withQuery(http_build_query(array_filter([
-                    'did' => $did,
-                    'cid' => $cid,
-                ])))
+                        'did' => $did,
+                        'cid' => $cid,
+                    ])))
             );
 
         $headers = [
