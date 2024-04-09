2e390455f73483ba0643bb60210e9556ff7f203e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 08:21:34 -0500

    Update QueryLabels.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Label/QueryLabels.php
+++ src/Com/Atproto/Label/QueryLabels.php
@@ -8,6 +8,9 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
+use function array_filter;
+use function http_build_query;
+
 /**
  * Find labels relevant to the provided AT-URI patterns. Public endpoint for moderation services, though may return different or additional results with auth.
  *
@@ -25,18 +28,17 @@ final readonly class QueryLabels
         ?array $sources = null,
         ?int $limit = null,
         ?string $cursor = null,
-    ): RequestInterface
-    {
+    ): RequestInterface {
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
                 $pdsUri->withPath('xrpc/com.atproto.label.queryLabels')
                     ->withQuery(http_build_query(array_filter([
-                    'uriPatterns' => $uriPatterns,
-                    'sources' => $sources,
-                    'limit' => $limit,
-                    'cursor' => $cursor,
-                ])))
+                        'uriPatterns' => $uriPatterns,
+                        'sources' => $sources,
+                        'limit' => $limit,
+                        'cursor' => $cursor,
+                    ])))
             );
 
         $headers = [
