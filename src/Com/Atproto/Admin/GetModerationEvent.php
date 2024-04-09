10053fbc0472ee2b6b7106762d944472f98f3b58
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 08:21:38 -0500

    Update GetModerationEvent.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Admin/GetModerationEvent.php
+++ src/Com/Atproto/Admin/GetModerationEvent.php
@@ -8,6 +8,9 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
+use function array_filter;
+use function http_build_query;
+
 /**
  * Get details about a moderation event.
  *
@@ -19,18 +22,15 @@ final readonly class GetModerationEvent
         private RequestFactoryInterface $requestFactory,
     ) {}
 
-    public function __invoke(
-        UriInterface $pdsUri,
-        int $id = null,
-    ): RequestInterface
+    public function __invoke(UriInterface $pdsUri, int $id = null): RequestInterface
     {
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
                 $pdsUri->withPath('xrpc/com.atproto.admin.getModerationEvent')
                     ->withQuery(http_build_query(array_filter([
-                    'id' => $id,
-                ])))
+                        'id' => $id,
+                    ])))
             );
 
         $headers = [
