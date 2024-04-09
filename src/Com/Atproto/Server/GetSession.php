59e73bc03787660e4f530abc680457656ce243e5
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 08:21:29 -0500

    Update GetSession.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Server/GetSession.php
+++ src/Com/Atproto/Server/GetSession.php
@@ -8,6 +8,9 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
+use function array_filter;
+use function http_build_query;
+
 /**
  * Get information about the current auth session. Requires auth.
  *
@@ -19,9 +22,7 @@ final readonly class GetSession
         private RequestFactoryInterface $requestFactory,
     ) {}
 
-    public function __invoke(
-        UriInterface $pdsUri,
-    ): RequestInterface
+    public function __invoke(UriInterface $pdsUri): RequestInterface
     {
         $request = $this->requestFactory
             ->createRequest(
