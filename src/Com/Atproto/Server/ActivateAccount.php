3b6cb8cb1467427a7aa93b53fb4f2184801a7f7c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 08:21:32 -0500

    Update ActivateAccount.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Server/ActivateAccount.php
+++ src/Com/Atproto/Server/ActivateAccount.php
@@ -8,6 +8,10 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
+use RuntimeException;
+
+use function array_filter;
+use function json_encode;
 
 /**
  * Activates a currently deactivated account. Used to finalize account migration after the account's repo is imported and identity is setup.
@@ -21,15 +25,10 @@ final readonly class ActivateAccount
         private StreamFactoryInterface $streamFactory,
     ) {}
 
-    public function __invoke(
-        UriInterface $pdsUri,
-    ): RequestInterface
+    public function __invoke(UriInterface $pdsUri): RequestInterface
     {
         $request = $this->requestFactory
-            ->createRequest(
-                'POST',
-                $pdsUri->withPath('xrpc/com.atproto.server.activateAccount')
-            );
+            ->createRequest('POST', $pdsUri->withPath('xrpc/com.atproto.server.activateAccount'));
 
         $headers = [
             'Accept' => 'application/json',
@@ -40,18 +39,12 @@ final readonly class ActivateAccount
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = json_encode(array_filter([
-            
-        ]));
+        $jsonBody = json_encode(array_filter([]));
 
-        if (false === $jsonBody){
-            throw new \RuntimeException('Failed to encode JSON');
+        if ($jsonBody === false) {
+            throw new RuntimeException('Failed to encode JSON');
         }
 
-        return $request->withBody(
-            $this->streamFactory->createStream(
-                $jsonBody
-            )
-        );
+        return $request->withBody($this->streamFactory->createStream($jsonBody));
     }
 }
