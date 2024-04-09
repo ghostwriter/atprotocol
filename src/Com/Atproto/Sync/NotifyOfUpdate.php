3dfd913b7cf891a304d04f8d93774b6bf3b965c6
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 08:21:26 -0500

    Update NotifyOfUpdate.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Sync/NotifyOfUpdate.php
+++ src/Com/Atproto/Sync/NotifyOfUpdate.php
@@ -8,6 +8,10 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
+use RuntimeException;
+
+use function array_filter;
+use function json_encode;
 
 /**
  * Notify a crawling service of a recent update, and that crawling should resume. Intended use is after a gap between repo stream events caused the crawling service to disconnect. Does not require auth; implemented by Relay.
@@ -21,16 +25,10 @@ final readonly class NotifyOfUpdate
         private StreamFactoryInterface $streamFactory,
     ) {}
 
-    public function __invoke(
-        UriInterface $pdsUri,
-        string $hostname = null,
-    ): RequestInterface
+    public function __invoke(UriInterface $pdsUri, string $hostname = null): RequestInterface
     {
         $request = $this->requestFactory
-            ->createRequest(
-                'POST',
-                $pdsUri->withPath('xrpc/com.atproto.sync.notifyOfUpdate')
-            );
+            ->createRequest('POST', $pdsUri->withPath('xrpc/com.atproto.sync.notifyOfUpdate'));
 
         $headers = [
             'Accept' => 'application/json',
@@ -45,14 +43,10 @@ final readonly class NotifyOfUpdate
             'hostname' => $hostname,
         ]));
 
-        if (false === $jsonBody){
-            throw new \RuntimeException('Failed to encode JSON');
+        if ($jsonBody === false) {
+            throw new RuntimeException('Failed to encode JSON');
         }
 
-        return $request->withBody(
-            $this->streamFactory->createStream(
-                $jsonBody
-            )
-        );
+        return $request->withBody($this->streamFactory->createStream($jsonBody));
     }
 }
