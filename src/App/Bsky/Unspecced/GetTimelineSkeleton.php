497fe6e58f566f2fbc31c78b22802e271e6c6841
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 08:21:40 -0500

    Update GetTimelineSkeleton.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Unspecced/GetTimelineSkeleton.php
+++ src/App/Bsky/Unspecced/GetTimelineSkeleton.php
@@ -8,6 +8,9 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
+use function array_filter;
+use function http_build_query;
+
 /**
  * DEPRECATED: a skeleton of a timeline. Unspecced and will be unavailable soon.
  *
@@ -19,20 +22,16 @@ final readonly class GetTimelineSkeleton
         private RequestFactoryInterface $requestFactory,
     ) {}
 
-    public function __invoke(
-        UriInterface $pdsUri,
-        ?int $limit = null,
-        ?string $cursor = null,
-    ): RequestInterface
+    public function __invoke(UriInterface $pdsUri, ?int $limit = null, ?string $cursor = null): RequestInterface
     {
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
                 $pdsUri->withPath('xrpc/app.bsky.unspecced.getTimelineSkeleton')
                     ->withQuery(http_build_query(array_filter([
-                    'limit' => $limit,
-                    'cursor' => $cursor,
-                ])))
+                        'limit' => $limit,
+                        'cursor' => $cursor,
+                    ])))
             );
 
         $headers = [
