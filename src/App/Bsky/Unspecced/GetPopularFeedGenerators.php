57969be784025200bbd73ac2587d1941b9fd95e1
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-04-09 08:21:40 -0500

    Update GetPopularFeedGenerators.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Unspecced/GetPopularFeedGenerators.php
+++ src/App/Bsky/Unspecced/GetPopularFeedGenerators.php
@@ -8,6 +8,9 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
+use function array_filter;
+use function http_build_query;
+
 /**
  * An unspecced view of globally popular feed generators.
  *
@@ -24,17 +27,16 @@ final readonly class GetPopularFeedGenerators
         ?int $limit = null,
         ?string $cursor = null,
         ?string $query = null,
-    ): RequestInterface
-    {
+    ): RequestInterface {
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
                 $pdsUri->withPath('xrpc/app.bsky.unspecced.getPopularFeedGenerators')
                     ->withQuery(http_build_query(array_filter([
-                    'limit' => $limit,
-                    'cursor' => $cursor,
-                    'query' => $query,
-                ])))
+                        'limit' => $limit,
+                        'cursor' => $cursor,
+                        'query' => $query,
+                    ])))
             );
 
         $headers = [
