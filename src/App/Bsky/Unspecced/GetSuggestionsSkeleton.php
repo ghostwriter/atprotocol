df7df1759104319da591d741100dc7f409864263
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-10 09:58:59 -0500

    Update GetSuggestionsSkeleton.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/App/Bsky/Unspecced/GetSuggestionsSkeleton.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\App\Bsky\Unspecced;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+use function array_filter;
+use function http_build_query;
+
+/**
+ * Get a skeleton of suggested actors. Intended to be called and then hydrated through app.bsky.actor.getSuggestions.
+ *
+ * @see \Ghostwriter\AtProtocolTests\Unit\App\Bsky\Unspecced\GetSuggestionsSkeletonTest
+ */
+final readonly class GetSuggestionsSkeleton
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+    ) {}
+
+    public function __invoke(
+        UriInterface $pdsUri,
+        ?string $viewer = null,
+        ?int $limit = null,
+        ?string $cursor = null,
+    ): RequestInterface {
+        $request = $this->requestFactory
+            ->createRequest(
+                'GET',
+                $pdsUri->withPath('xrpc/app.bsky.unspecced.getSuggestionsSkeleton')
+                    ->withQuery(http_build_query(array_filter([
+                        'viewer' => $viewer,
+                        'limit' => $limit,
+                        'cursor' => $cursor,
+                    ])))
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request;
+    }
+}
