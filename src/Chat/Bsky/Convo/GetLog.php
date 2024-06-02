e7f65df3412ff7dae642c7228b6dabc97ef69fbd
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-02 07:24:33 -0500

    Update GetLog.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Chat/Bsky/Convo/GetLog.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Chat\Bsky\Convo;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+use function array_filter;
+use function http_build_query;
+
+/**
+ * GetLog.
+ *
+ * @see \Ghostwriter\AtProtocolTests\Unit\Chat\Bsky\Convo\GetLogTest
+ */
+final readonly class GetLog
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+    ) {}
+
+    public function __invoke(UriInterface $pdsUri, ?string $cursor = null): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'GET',
+                $pdsUri->withPath('xrpc/chat.bsky.convo.getLog')
+                    ->withQuery(http_build_query(array_filter([
+                        'cursor' => $cursor,
+                    ])))
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request;
+    }
+}
