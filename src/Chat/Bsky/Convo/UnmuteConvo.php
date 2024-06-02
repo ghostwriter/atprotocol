1a32bdffd5b05d075df2d982602911b4b2e60af3
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-02 07:24:32 -0500

    Update UnmuteConvo.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Chat/Bsky/Convo/UnmuteConvo.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Chat\Bsky\Convo;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\UriInterface;
+use RuntimeException;
+
+use function array_filter;
+use function json_encode;
+
+/**
+ * UnmuteConvo.
+ *
+ * @see \Ghostwriter\AtProtocolTests\Unit\Chat\Bsky\Convo\UnmuteConvoTest
+ */
+final readonly class UnmuteConvo
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+        private StreamFactoryInterface $streamFactory,
+    ) {}
+
+    public function __invoke(UriInterface $pdsUri, string $convoId = null): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest('POST', $pdsUri->withPath('xrpc/chat.bsky.convo.unmuteConvo'));
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        $jsonBody = json_encode(array_filter([
+            'convoId' => $convoId,
+        ]));
+
+        if ($jsonBody === false) {
+            throw new RuntimeException('Failed to encode JSON');
+        }
+
+        return $request->withBody($this->streamFactory->createStream($jsonBody));
+    }
+}
