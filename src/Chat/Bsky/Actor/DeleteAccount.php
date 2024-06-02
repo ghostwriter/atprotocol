e843325cd228a5886d24ab99398c5d5a681e1fe8
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-02 07:24:41 -0500

    Update DeleteAccount.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Chat/Bsky/Actor/DeleteAccount.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Chat\Bsky\Actor;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\UriInterface;
+use RuntimeException;
+
+use function array_filter;
+use function json_encode;
+
+/**
+ * DeleteAccount.
+ *
+ * @see \Ghostwriter\AtProtocolTests\Unit\Chat\Bsky\Actor\DeleteAccountTest
+ */
+final readonly class DeleteAccount
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+        private StreamFactoryInterface $streamFactory,
+    ) {}
+
+    public function __invoke(UriInterface $pdsUri): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest('POST', $pdsUri->withPath('xrpc/chat.bsky.actor.deleteAccount'));
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        $jsonBody = json_encode(array_filter([]));
+
+        if ($jsonBody === false) {
+            throw new RuntimeException('Failed to encode JSON');
+        }
+
+        return $request->withBody($this->streamFactory->createStream($jsonBody));
+    }
+}
