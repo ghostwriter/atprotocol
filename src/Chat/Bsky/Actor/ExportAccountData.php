902a924ab660de25e3967ba4909710c508394c62
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-02 07:24:40 -0500

    Update ExportAccountData.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Chat/Bsky/Actor/ExportAccountData.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Chat\Bsky\Actor;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+use function array_filter;
+use function http_build_query;
+
+/**
+ * ExportAccountData.
+ *
+ * @see \Ghostwriter\AtProtocolTests\Unit\Chat\Bsky\Actor\ExportAccountDataTest
+ */
+final readonly class ExportAccountData
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+    ) {}
+
+    public function __invoke(UriInterface $pdsUri): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'GET',
+                $pdsUri->withPath('xrpc/chat.bsky.actor.exportAccountData')
+                    ->withQuery(http_build_query(array_filter([])))
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/jsonl; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request;
+    }
+}
