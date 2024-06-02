4efe584551e328bf9f4338af59c1107f803190b0
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-02 07:24:42 -0500

    Update GetActorMetadata.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Chat/Bsky/Moderation/GetActorMetadata.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Chat\Bsky\Moderation;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+use function array_filter;
+use function http_build_query;
+
+/**
+ * GetActorMetadata.
+ *
+ * @see \Ghostwriter\AtProtocolTests\Unit\Chat\Bsky\Moderation\GetActorMetadataTest
+ */
+final readonly class GetActorMetadata
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+    ) {}
+
+    public function __invoke(UriInterface $pdsUri, string $actor = null): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'GET',
+                $pdsUri->withPath('xrpc/chat.bsky.moderation.getActorMetadata')
+                    ->withQuery(http_build_query(array_filter([
+                        'actor' => $actor,
+                    ])))
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request;
+    }
+}
