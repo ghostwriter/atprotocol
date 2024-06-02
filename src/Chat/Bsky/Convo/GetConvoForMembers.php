b2c8c2163ff46da90d89c02d2daa808cd897d314
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-02 07:24:39 -0500

    Update GetConvoForMembers.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Chat/Bsky/Convo/GetConvoForMembers.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Chat\Bsky\Convo;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+use function array_filter;
+use function http_build_query;
+
+/**
+ * GetConvoForMembers.
+ *
+ * @see \Ghostwriter\AtProtocolTests\Unit\Chat\Bsky\Convo\GetConvoForMembersTest
+ */
+final readonly class GetConvoForMembers
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+    ) {}
+
+    public function __invoke(UriInterface $pdsUri, array $members = null): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'GET',
+                $pdsUri->withPath('xrpc/chat.bsky.convo.getConvoForMembers')
+                    ->withQuery(http_build_query(array_filter([
+                        'members' => $members,
+                    ])))
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request;
+    }
+}
