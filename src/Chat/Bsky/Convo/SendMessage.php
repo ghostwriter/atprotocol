cd741dcf18609f73bc3a0fe0791590c7b8252582
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-12 14:05:51 -0500

    Update SendMessage.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Chat/Bsky/Convo/SendMessage.php
+++ src/Chat/Bsky/Convo/SendMessage.php
@@ -16,14 +16,15 @@ use function json_encode;
 /**
  * SendMessage.
  *
- * @see \Ghostwriter\AtProtocolTests\Unit\Chat\Bsky\Convo\SendMessageTest
+ * @see \Tests\Unit\Chat\Bsky\Convo\SendMessageTest
  */
 final readonly class SendMessage
 {
     public function __construct(
         private RequestFactoryInterface $requestFactory,
         private StreamFactoryInterface $streamFactory,
-    ) {}
+    ) {
+    }
 
     public function __invoke(
         UriInterface $pdsUri,
