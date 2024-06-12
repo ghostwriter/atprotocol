7dd435005662e17991b944b7fe21c09e6ba55509
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-12 14:01:55 -0500

    Update HandleTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Value/HandleTest.php
+++ tests/Unit/Value/HandleTest.php
@@ -2,29 +2,52 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\AtProtocol\Tests\Unit\Value;
+namespace Tests\Unit\Value;
 
 use Generator;
+use Ghostwriter\AtProtocol\Value\Handle;
+use InvalidArgumentException;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
-use Ghostwriter\AtProtocol\Value\Handle;
-use InvalidArgumentException;
 
 #[CoversClass(Handle::class)]
 final class HandleTest extends TestCase
 {
-    public static function validSyntaxHandleDataProvider(): Generator
+    #[DataProvider('invalidHandleDataProvider')]
+    public function testInvalidHandle(string $handle): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        new Handle($handle);
+    }
+
+    #[DataProvider('validHandleDataProvider')]
+    public function testValidHandle(string $handle): void
+    {
+        self::assertSame($handle, (string) new Handle($handle));
+    }
+
+    #[DataProvider('validSyntaxHandleDataProvider')]
+    public function testValidSyntaxHandle(string $handle): void
+    {
+        self::assertSame($handle, (string) new Handle($handle));
+    }
+
+    public static function invalidHandleDataProvider(): Generator
     {
         // Test data copied from https://atproto.com/specs/handle
         // Copyright (c) 2023 Bluesky, PBC.
         $handles = [
-            '2gzyxa5ihm7nsggfxnu52rck2vv4rvmdlkiu3zzui5du4xyclen53wid.onion',
-            'laptop.local',
-            'blah.arpa',
-            // not a real TLD, but syntax ok
-            'name.t--t',
-            'example.t',
+            'jo@hn.test',
+            '💩.test',
+            'john..test',
+            'xn--bcher-.tld',
+            'john.0',
+            'cn.8',
+            'www.masełkowski.pl.com',
+            'org',
+            'name.org.',
         ];
 
         foreach ($handles as $handle) {
@@ -51,44 +74,21 @@ final class HandleTest extends TestCase
         }
     }
 
-    public static function invalidHandleDataProvider(): Generator
+    public static function validSyntaxHandleDataProvider(): Generator
     {
         // Test data copied from https://atproto.com/specs/handle
         // Copyright (c) 2023 Bluesky, PBC.
         $handles = [
-            'jo@hn.test',
-            '💩.test',
-            'john..test',
-            'xn--bcher-.tld',
-            'john.0',
-            'cn.8',
-            'www.masełkowski.pl.com',
-            'org',
-            'name.org.',
+            '2gzyxa5ihm7nsggfxnu52rck2vv4rvmdlkiu3zzui5du4xyclen53wid.onion',
+            'laptop.local',
+            'blah.arpa',
+            // not a real TLD, but syntax ok
+            'name.t--t',
+            'example.t',
         ];
 
         foreach ($handles as $handle) {
             yield $handle => [$handle];
         }
-    }
-
-    #[DataProvider('validSyntaxHandleDataProvider')]
-    public function testValidSyntaxHandle(string $handle): void
-    {
-        self::assertSame($handle, (string) new Handle($handle));
-    }
-
-    #[DataProvider('validHandleDataProvider')]
-    public function testValidHandle(string $handle): void
-    {
-        self::assertSame($handle, (string) new Handle($handle));
-    }
-
-    #[DataProvider('invalidHandleDataProvider')]
-    public function testInvalidHandle(string $handle): void
-    {
-        $this->expectException(InvalidArgumentException::class);
-
-        new Handle($handle);
     }
 }
