17e65b295a2cb092f2b70ebc20cf7e3495791140
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-12 14:05:30 -0500

    Update GetKnownFollowers.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/App/Bsky/Graph/GetKnownFollowers.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\App\Bsky\Graph;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+use function array_filter;
+use function http_build_query;
+
+/**
+ * Enumerates accounts which follow a specified account (actor) and are followed by the viewer.
+ *
+ * @see \Tests\Unit\App\Bsky\Graph\GetKnownFollowersTest
+ */
+final readonly class GetKnownFollowers
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+    ) {
+    }
+
+    public function __invoke(
+        UriInterface $pdsUri,
+        string $actor = null,
+        ?int $limit = null,
+        ?string $cursor = null,
+    ): RequestInterface {
+        $request = $this->requestFactory
+            ->createRequest(
+                'GET',
+                $pdsUri->withPath('xrpc/app.bsky.graph.getKnownFollowers')
+                    ->withQuery(http_build_query(array_filter([
+                        'actor' => $actor,
+                        'limit' => $limit,
+                        'cursor' => $cursor,
+                    ])))
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request;
+    }
+}
