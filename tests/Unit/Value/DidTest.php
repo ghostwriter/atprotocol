b77df3b2d78d4bb9817ebfa58f01170752f9bee7
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-12 14:02:00 -0500

    Update DidTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Value/DidTest.php
+++ tests/Unit/Value/DidTest.php
@@ -2,39 +2,43 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\AtProtocol\Tests\Unit\Value;
+namespace Tests\Unit\Value;
 
 use Generator;
 use Ghostwriter\AtProtocol\Value\Did;
+use InvalidArgumentException;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
-use InvalidArgumentException;
-
-use function json_encode;
 
 use const JSON_THROW_ON_ERROR;
+
+use function json_encode;
 
 #[CoversClass(Did::class)]
 final class DidTest extends TestCase
 {
-    public static function validDidDataProvider(): Generator
+    #[DataProvider('invalidDidDataProvider')]
+    public function testInvalidDid(string $did): void
     {
-        // Test data copied from https://atproto.com/specs/did
-        // Copyright (c) 2023 Bluesky, PBC.
-        $dids = [
-            'did:plc:z72i7hdynmk6r22z27h6tvur',
-            'did:web:blueskyweb.xyz',
-            'did:method:val:two',
-            'did:m:v',
-            'did:method::::val',
-            'did:method:-:_:.',
-            'did:key:zQ3shZc2QzApp2oymGvQbzP8eKheVshBHbU4ZYjeXqwSKEn6N',
-        ];
+        $this->expectException(InvalidArgumentException::class);
 
-        foreach ($dids as $did) {
-            yield $did => [$did];
-        }
+        new Did($did);
+    }
+
+    #[DataProvider('validDidDataProvider')]
+    public function testValidDid(string $did): void
+    {
+        $sut = new Did($did);
+
+        self::assertSame($did, (string) $sut);
+
+        self::assertSame(
+            json_encode([
+                'did' => $did,
+            ], JSON_THROW_ON_ERROR),
+            json_encode($sut, JSON_THROW_ON_ERROR)
+        );
     }
 
     public static function invalidDidDataProvider(): Generator
@@ -56,26 +60,22 @@ final class DidTest extends TestCase
         }
     }
 
-    #[DataProvider('validDidDataProvider')]
-    public function testValidDid(string $did): void
+    public static function validDidDataProvider(): Generator
     {
-        $sut = new Did($did);
+        // Test data copied from https://atproto.com/specs/did
+        // Copyright (c) 2023 Bluesky, PBC.
+        $dids = [
+            'did:plc:z72i7hdynmk6r22z27h6tvur',
+            'did:web:blueskyweb.xyz',
+            'did:method:val:two',
+            'did:m:v',
+            'did:method::::val',
+            'did:method:-:_:.',
+            'did:key:zQ3shZc2QzApp2oymGvQbzP8eKheVshBHbU4ZYjeXqwSKEn6N',
+        ];
 
-        self::assertSame($did, (string) $sut);
-
-        self::assertSame(
-            json_encode([
-                'did' => $did,
-            ], JSON_THROW_ON_ERROR),
-            json_encode($sut, JSON_THROW_ON_ERROR)
-        );
-    }
-
-    #[DataProvider('invalidDidDataProvider')]
-    public function testInvalidDid(string $did): void
-    {
-        $this->expectException(InvalidArgumentException::class);
-
-        new Did($did);
+        foreach ($dids as $did) {
+            yield $did => [$did];
+        }
     }
 }
