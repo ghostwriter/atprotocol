8d2b60e5c59e7bd453839b6e0ea58573022a2830
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-12 14:12:03 -0500

    Update Password.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Value/Password.php
+++ src/Value/Password.php
@@ -4,21 +4,25 @@ declare(strict_types=1);
 
 namespace Ghostwriter\AtProtocol\Value;
 
-use Stringable;
 use InvalidArgumentException;
+use Override;
+use SensitiveParameter;
+use Stringable;
 
 use function mb_strlen;
 
 final readonly class Password implements Stringable
 {
     public function __construct(
-        private readonly string $password,
+        #[SensitiveParameter]
+        private string $password,
     ) {
         if (mb_strlen($this->password) < 3) {
             throw new InvalidArgumentException('Invalid Password length: ' . $this->password);
         }
     }
 
+    #[Override]
     public function __toString(): string
     {
         return $this->password;
