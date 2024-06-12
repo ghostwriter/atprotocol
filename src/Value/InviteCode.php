258d636762813bd900e14cd50fc272de24292ba4
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-12 14:02:20 -0500

    Create InviteCode.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Value/InviteCode.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Value;
+
+use InvalidArgumentException;
+use Stringable;
+
+use function preg_match;
+
+final readonly class InviteCode implements Stringable
+{
+    public function __construct(
+        private readonly string $invitecode,
+    ) {
+        if (! preg_match('#^invitecode:[a-z]+:[a-zA-Z0-9._:%-]*[a-zA-Z0-9._-]$#', $this->invitecode)) {
+            throw new InvalidArgumentException('Invalid InviteCode: ' . $this->invitecode);
+        }
+    }
+
+    public function __toString(): string
+    {
+        return $this->invitecode;
+    }
+}
