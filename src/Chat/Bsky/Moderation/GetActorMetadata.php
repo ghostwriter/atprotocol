5fc7ae7b295ec8d3681a6ec085f0ec99bb93328b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-12 14:05:57 -0500

    Update GetActorMetadata.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Chat/Bsky/Moderation/GetActorMetadata.php
+++ src/Chat/Bsky/Moderation/GetActorMetadata.php
@@ -14,13 +14,14 @@ use function http_build_query;
 /**
  * GetActorMetadata.
  *
- * @see \Ghostwriter\AtProtocolTests\Unit\Chat\Bsky\Moderation\GetActorMetadataTest
+ * @see \Tests\Unit\Chat\Bsky\Moderation\GetActorMetadataTest
  */
 final readonly class GetActorMetadata
 {
     public function __construct(
         private RequestFactoryInterface $requestFactory,
-    ) {}
+    ) {
+    }
 
     public function __invoke(UriInterface $pdsUri, string $actor = null): RequestInterface
     {
