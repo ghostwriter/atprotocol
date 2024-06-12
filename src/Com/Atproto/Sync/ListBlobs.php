8a8e1fcd6c2638059d161baeec91dd804a5d2640
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-12 14:06:33 -0500

    Update ListBlobs.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Sync/ListBlobs.php
+++ src/Com/Atproto/Sync/ListBlobs.php
@@ -14,13 +14,14 @@ use function http_build_query;
 /**
  * List blob CIDso for an account, since some repo revision. Does not require auth; implemented by PDS.
  *
- * @see \Ghostwriter\AtProtocolTests\Unit\Com\Atproto\Sync\ListBlobsTest
+ * @see \Tests\Unit\Com\Atproto\Sync\ListBlobsTest
  */
 final readonly class ListBlobs
 {
     public function __construct(
         private RequestFactoryInterface $requestFactory,
-    ) {}
+    ) {
+    }
 
     public function __invoke(
         UriInterface $pdsUri,
