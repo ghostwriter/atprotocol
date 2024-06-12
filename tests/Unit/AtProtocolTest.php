b397f03915fd4855f9e6b24d6e2dce57672d366f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-12 14:02:03 -0500

    Update AtProtocolTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/AtProtocolTest.php
+++ tests/Unit/AtProtocolTest.php
@@ -2,16 +2,13 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\AtProtocol\Tests\Unit;
+namespace Tests\Unit;
 
 use Ghostwriter\AtProtocol\AtProtocol;
-use Ghostwriter\AtProtocol\Com\Atproto\Server\CreateSession;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\UsesClass;
 use PHPUnit\Framework\TestCase;
 
 #[CoversClass(AtProtocol::class)]
-#[UsesClass(CreateSession::class)]
 final class AtProtocolTest extends TestCase
 {
     public function testAtProtocol(): void
