6ca1e3cf812326002ef272a35f563ad87e481008
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-12 14:18:12 -0500

    Create PersonalDataServer.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Value/PersonalDataServer.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Value;
+
+use Laminas\Diactoros\UriFactory;
+use Psr\Http\Message\UriInterface;
+
+final readonly class PersonalDataServer
+{
+    public function __construct(
+        private UriInterface $uri
+    ) {
+    }
+
+    public function toString(): string
+    {
+        return $this->uri->__toString();
+    }
+
+    public static function new(string $personalDataServer): self
+    {
+        return new self((new UriFactory())->createUri($personalDataServer));
+    }
+}
