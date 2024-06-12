5f7e98c6ec60312cd517555caa37fab9673bded5
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-12 14:18:26 -0500

    Update AtProtocol.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/AtProtocol.php
+++ src/AtProtocol.php
@@ -6,6 +6,9 @@ namespace Ghostwriter\AtProtocol;
 
 use Ghostwriter\AtProtocol\Trait\HttpTrait;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\UploadedFileInterface;
+
+use function json_encode;
 
 /** @see AtProtocolTest */
 final class AtProtocol
@@ -16,5 +19,23 @@ final class AtProtocol
     {
         // replace this block of code with a separate class
         return $this->httpClient->sendRequest(($this->createSession)($username, $password));
+    }
+
+    /**
+     * @param array<UploadedFileInterface> $attachments
+     */
+    public function updateStatus(string $content, array $attachments = []): ResponseInterface
+    {
+        return $this->post(
+            $this->path('xrpc/com.atproto.server.updateStatus'),
+            json_encode([
+                'content' => $content,
+                'attachments' => $attachments,
+            ]) ?: '',
+            [
+                'Accept' => 'application/json',
+                // 'Content-Type' => 'application/json; charset=utf-8',
+            ]
+        );
     }
 }
