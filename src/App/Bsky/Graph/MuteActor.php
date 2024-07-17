57a1bc469864d95508bef7bcfdb100d28cceb09f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-17 10:36:42 -0500

    Update MuteActor.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Graph/MuteActor.php
+++ src/App/Bsky/Graph/MuteActor.php
@@ -26,7 +26,7 @@ final readonly class MuteActor
     ) {
     }
 
-    public function __invoke(UriInterface $pdsUri, string $actor = null): RequestInterface
+    public function __invoke(UriInterface $pdsUri, ?string $actor = null): RequestInterface
     {
         $request = $this->requestFactory
             ->createRequest('POST', $pdsUri->withPath('xrpc/app.bsky.graph.muteActor'));
