259e33b218cd3b95bcd57ccc48be32e4471ff865
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-17 10:36:39 -0500

    Update RequestPhoneVerification.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Temp/RequestPhoneVerification.php
+++ src/Com/Atproto/Temp/RequestPhoneVerification.php
@@ -26,7 +26,7 @@ final readonly class RequestPhoneVerification
     ) {
     }
 
-    public function __invoke(UriInterface $pdsUri, string $phoneNumber = null): RequestInterface
+    public function __invoke(UriInterface $pdsUri, ?string $phoneNumber = null): RequestInterface
     {
         $request = $this->requestFactory
             ->createRequest('POST', $pdsUri->withPath('xrpc/com.atproto.temp.requestPhoneVerification'));
