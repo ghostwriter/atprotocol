eded63dec3302ae06d6255a4115e066144ad1508
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-17 10:36:40 -0500

    Update GetHead.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Sync/GetHead.php
+++ src/Com/Atproto/Sync/GetHead.php
@@ -23,7 +23,7 @@ final readonly class GetHead
     ) {
     }
 
-    public function __invoke(UriInterface $pdsUri, string $did = null): RequestInterface
+    public function __invoke(UriInterface $pdsUri, ?string $did = null): RequestInterface
     {
         $request = $this->requestFactory
             ->createRequest(
