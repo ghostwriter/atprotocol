ac7f8aeea4ccb26e66a85cc6a2e94102c05133e9
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-17 10:36:40 -0500

    Update RequestCrawl.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Sync/RequestCrawl.php
+++ src/Com/Atproto/Sync/RequestCrawl.php
@@ -26,7 +26,7 @@ final readonly class RequestCrawl
     ) {
     }
 
-    public function __invoke(UriInterface $pdsUri, string $hostname = null): RequestInterface
+    public function __invoke(UriInterface $pdsUri, ?string $hostname = null): RequestInterface
     {
         $request = $this->requestFactory
             ->createRequest('POST', $pdsUri->withPath('xrpc/com.atproto.sync.requestCrawl'));
