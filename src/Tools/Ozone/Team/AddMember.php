6468f693103f11dfe4cac7b17c8587ba86fcae7e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-08-31 09:42:24 -0500

    Add AddMember.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Tools/Ozone/Team/AddMember.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Tools\Ozone\Team;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\UriInterface;
+use RuntimeException;
+
+use function array_filter;
+use function json_encode;
+
+/**
+ * Add a member to the ozone team. Requires admin role.
+ *
+ * @see AddMemberTest
+ */
+final readonly class AddMember
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+        private StreamFactoryInterface $streamFactory,
+    ) {
+    }
+
+    public function __invoke(UriInterface $uri, ?string $did = null, ?string $role = null): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest('POST', $uri->withPath('xrpc/tools.ozone.team.addMember'));
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        $jsonBody = json_encode(array_filter([
+            'did' => $did,
+            'role' => $role,
+        ]));
+
+        if ($jsonBody === false) {
+            throw new RuntimeException('Failed to encode JSON');
+        }
+
+        return $request->withBody($this->streamFactory->createStream($jsonBody));
+    }
+}
