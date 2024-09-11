7a8811e3f54fac0516b0533d0e25e7b7c99f0a74
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:04 -0500

    Update ListConvos.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Chat/Bsky/Convo/ListConvos.php
+++ src/Chat/Bsky/Convo/ListConvos.php
@@ -8,9 +8,6 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
-use function array_filter;
-use function http_build_query;
-
 /**
  *
  * @see ListConvosTest
@@ -28,7 +25,7 @@ final readonly class ListConvos
             ->createRequest(
                 'GET',
                 $uri->withPath('xrpc/chat.bsky.convo.listConvos')
-                    ->withQuery(http_build_query(array_filter([
+                    ->withQuery(\http_build_query(\array_filter([
                         'limit' => $limit,
                         'cursor' => $cursor,
                     ])))
