7c2cc23466ca8defe5a5f554b7a084190969e924
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:03 -0500

    Update GetConvo.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Chat/Bsky/Convo/GetConvo.php
+++ src/Chat/Bsky/Convo/GetConvo.php
@@ -8,9 +8,6 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
-use function array_filter;
-use function http_build_query;
-
 /**
  *
  * @see GetConvoTest
@@ -28,7 +25,7 @@ final readonly class GetConvo
             ->createRequest(
                 'GET',
                 $uri->withPath('xrpc/chat.bsky.convo.getConvo')
-                    ->withQuery(http_build_query(array_filter([
+                    ->withQuery(\http_build_query(\array_filter([
                         'convoId' => $convoId,
                     ])))
             );
