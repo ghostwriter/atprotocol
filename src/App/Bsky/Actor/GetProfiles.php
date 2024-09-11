7a4a9a918609e3d498c7f4cf47093598bfce17c9
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:06:58 -0500

    Update GetProfiles.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Actor/GetProfiles.php
+++ src/App/Bsky/Actor/GetProfiles.php
@@ -8,9 +8,6 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
-use function array_filter;
-use function http_build_query;
-
 /**
  * Get detailed profile views of multiple actors.
  *
@@ -29,7 +26,7 @@ final readonly class GetProfiles
             ->createRequest(
                 'GET',
                 $uri->withPath('xrpc/app.bsky.actor.getProfiles')
-                    ->withQuery(http_build_query(array_filter([
+                    ->withQuery(\http_build_query(\array_filter([
                         'actors' => $actors,
                     ])))
             );
