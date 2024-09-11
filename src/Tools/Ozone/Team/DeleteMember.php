081efec4819b45ff544391e0e0af7173f47a5b90
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:10 -0500

    Update DeleteMember.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Tools/Ozone/Team/DeleteMember.php
+++ src/Tools/Ozone/Team/DeleteMember.php
@@ -10,9 +10,6 @@ use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
 use RuntimeException;
 
-use function array_filter;
-use function json_encode;
-
 /**
  * Delete a member from ozone team. Requires admin role.
  *
@@ -40,7 +37,7 @@ final readonly class DeleteMember
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = json_encode(array_filter([
+        $jsonBody = \json_encode(\array_filter([
             'did' => $did,
         ]));
 
