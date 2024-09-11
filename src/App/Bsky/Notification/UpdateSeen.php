8ae6ee7b1a797035e783d952910b608b3d1b6231
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:02 -0500

    Update UpdateSeen.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Notification/UpdateSeen.php
+++ src/App/Bsky/Notification/UpdateSeen.php
@@ -10,9 +10,6 @@ use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
 use RuntimeException;
 
-use function array_filter;
-use function json_encode;
-
 /**
  * Notify server that the requesting account has seen notifications. Requires auth.
  *
@@ -40,7 +37,7 @@ final readonly class UpdateSeen
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = json_encode(array_filter([
+        $jsonBody = \json_encode(\array_filter([
             'seenAt' => $seenAt,
         ]));
 
