1aebea2cd55bb8ca098bfe4a1242846594dba9c1
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:09 -0500

    Update GetHead.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Sync/GetHead.php
+++ src/Com/Atproto/Sync/GetHead.php
@@ -8,9 +8,6 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
-use function array_filter;
-use function http_build_query;
-
 /**
  * DEPRECATED - please use com.atproto.sync.getLatestCommit instead.
  *
@@ -29,7 +26,7 @@ final readonly class GetHead
             ->createRequest(
                 'GET',
                 $uri->withPath('xrpc/com.atproto.sync.getHead')
-                    ->withQuery(http_build_query(array_filter([
+                    ->withQuery(\http_build_query(\array_filter([
                         'did' => $did,
                     ])))
             );
