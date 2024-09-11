4931dc28c029099927793eb3840bf8172d62b3c6
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:10 -0500

    Update QueryEvents.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Tools/Ozone/Moderation/QueryEvents.php
+++ src/Tools/Ozone/Moderation/QueryEvents.php
@@ -8,9 +8,6 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
-use function array_filter;
-use function http_build_query;
-
 /**
  * List moderation events related to a subject.
  *
@@ -46,7 +43,7 @@ final readonly class QueryEvents
             ->createRequest(
                 'GET',
                 $uri->withPath('xrpc/tools.ozone.moderation.queryEvents')
-                    ->withQuery(http_build_query(array_filter([
+                    ->withQuery(\http_build_query(\array_filter([
                         'types' => $types,
                         'createdBy' => $createdBy,
                         'sortDirection' => $sortDirection,
