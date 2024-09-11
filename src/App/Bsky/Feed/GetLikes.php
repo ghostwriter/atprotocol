7054d4fb3eae2a0525afafa600dadfa4f9a60e1b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:06:59 -0500

    Update GetLikes.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Feed/GetLikes.php
+++ src/App/Bsky/Feed/GetLikes.php
@@ -8,9 +8,6 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
-use function array_filter;
-use function http_build_query;
-
 /**
  * Get like records which reference a subject (by AT-URI and CID).
  *
@@ -34,7 +31,7 @@ final readonly class GetLikes
             ->createRequest(
                 'GET',
                 $uri->withPath('xrpc/app.bsky.feed.getLikes')
-                    ->withQuery(http_build_query(array_filter([
+                    ->withQuery(\http_build_query(\array_filter([
                         'uri' => $uri,
                         'cid' => $cid,
                         'limit' => $limit,
