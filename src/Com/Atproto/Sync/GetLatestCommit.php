8cce66c8c67fd71b4648ac9a729e794071f54344
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:09 -0500

    Update GetLatestCommit.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Sync/GetLatestCommit.php
+++ src/Com/Atproto/Sync/GetLatestCommit.php
@@ -8,9 +8,6 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
-use function array_filter;
-use function http_build_query;
-
 /**
  * Get the current commit CID & revision of the specified repo. Does not require auth.
  *
@@ -29,7 +26,7 @@ final readonly class GetLatestCommit
             ->createRequest(
                 'GET',
                 $uri->withPath('xrpc/com.atproto.sync.getLatestCommit')
-                    ->withQuery(http_build_query(array_filter([
+                    ->withQuery(\http_build_query(\array_filter([
                         'did' => $did,
                     ])))
             );
