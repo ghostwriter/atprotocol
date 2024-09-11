fb94140193b25b1660e1971cd89395cd61e35d6c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:04 -0500

    Update LeaveConvo.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Chat/Bsky/Convo/LeaveConvo.php
+++ src/Chat/Bsky/Convo/LeaveConvo.php
@@ -10,9 +10,6 @@ use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
 use RuntimeException;
 
-use function array_filter;
-use function json_encode;
-
 /**
  *
  * @see LeaveConvoTest
@@ -39,7 +36,7 @@ final readonly class LeaveConvo
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = json_encode(array_filter([
+        $jsonBody = \json_encode(\array_filter([
             'convoId' => $convoId,
         ]));
 
