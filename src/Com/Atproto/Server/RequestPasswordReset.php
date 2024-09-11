740fe2b12b738d404f25131a6223cb22e44385de
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:08 -0500

    Update RequestPasswordReset.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Server/RequestPasswordReset.php
+++ src/Com/Atproto/Server/RequestPasswordReset.php
@@ -10,9 +10,6 @@ use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
 use RuntimeException;
 
-use function array_filter;
-use function json_encode;
-
 /**
  * Initiate a user account password reset via email.
  *
@@ -40,7 +37,7 @@ final readonly class RequestPasswordReset
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = json_encode(array_filter([
+        $jsonBody = \json_encode(\array_filter([
             'email' => $email,
         ]));
 
