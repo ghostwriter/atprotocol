d725b72f595713e8524958897c66537a00a7868f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:05 -0500

    Update GetInviteCodes.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Admin/GetInviteCodes.php
+++ src/Com/Atproto/Admin/GetInviteCodes.php
@@ -8,9 +8,6 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
-use function array_filter;
-use function http_build_query;
-
 /**
  * Get an admin view of invite codes.
  *
@@ -33,7 +30,7 @@ final readonly class GetInviteCodes
             ->createRequest(
                 'GET',
                 $uri->withPath('xrpc/com.atproto.admin.getInviteCodes')
-                    ->withQuery(http_build_query(array_filter([
+                    ->withQuery(\http_build_query(\array_filter([
                         'sort' => $sort,
                         'limit' => $limit,
                         'cursor' => $cursor,
