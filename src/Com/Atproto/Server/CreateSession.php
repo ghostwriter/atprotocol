25e8aa36e4d14b03411d46c85385bb4e93086271
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:07 -0500

    Update CreateSession.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Server/CreateSession.php
+++ src/Com/Atproto/Server/CreateSession.php
@@ -10,9 +10,6 @@ use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
 use RuntimeException;
 
-use function array_filter;
-use function json_encode;
-
 /**
  * Create an authentication session.
  *
@@ -44,7 +41,7 @@ final readonly class CreateSession
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = json_encode(array_filter([
+        $jsonBody = \json_encode(\array_filter([
             'identifier' => $identifier,
             'password' => $password,
             'authFactorToken' => $authFactorToken,
