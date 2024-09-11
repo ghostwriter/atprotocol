96a0e1e49cb3da03a494a991b7f68a30ee1f84e3
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:01 -0500

    Update GetMutes.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Graph/GetMutes.php
+++ src/App/Bsky/Graph/GetMutes.php
@@ -8,9 +8,6 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
-use function array_filter;
-use function http_build_query;
-
 /**
  * Enumerates accounts that the requesting account (actor) currently has muted. Requires auth.
  *
@@ -29,7 +26,7 @@ final readonly class GetMutes
             ->createRequest(
                 'GET',
                 $uri->withPath('xrpc/app.bsky.graph.getMutes')
-                    ->withQuery(http_build_query(array_filter([
+                    ->withQuery(\http_build_query(\array_filter([
                         'limit' => $limit,
                         'cursor' => $cursor,
                     ])))
