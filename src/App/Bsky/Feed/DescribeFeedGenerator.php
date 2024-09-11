51636f2598f90934ffda284aa4b4c009f8141b51
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:06:59 -0500

    Update DescribeFeedGenerator.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Feed/DescribeFeedGenerator.php
+++ src/App/Bsky/Feed/DescribeFeedGenerator.php
@@ -8,9 +8,6 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
-use function array_filter;
-use function http_build_query;
-
 /**
  * Get information about a feed generator, including policies and offered feed URIs. Does not require auth; implemented by Feed Generator services (not App View).
  *
@@ -29,7 +26,7 @@ final readonly class DescribeFeedGenerator
             ->createRequest(
                 'GET',
                 $uri->withPath('xrpc/app.bsky.feed.describeFeedGenerator')
-                    ->withQuery(http_build_query(array_filter([])))
+                    ->withQuery(\http_build_query(\array_filter([])))
             );
 
         $headers = [
