f7301d42eff7de1189fe407fc38337af1965a96e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:03 -0500

    Update GetLog.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Chat/Bsky/Convo/GetLog.php
+++ src/Chat/Bsky/Convo/GetLog.php
@@ -8,9 +8,6 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
-use function array_filter;
-use function http_build_query;
-
 /**
  *
  * @see GetLogTest
@@ -28,7 +25,7 @@ final readonly class GetLog
             ->createRequest(
                 'GET',
                 $uri->withPath('xrpc/chat.bsky.convo.getLog')
-                    ->withQuery(http_build_query(array_filter([
+                    ->withQuery(\http_build_query(\array_filter([
                         'cursor' => $cursor,
                     ])))
             );
