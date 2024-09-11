63c5f8458fd5b28a396889c95268352cd07e5ae7
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:08 -0500

    Update DescribeServer.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Server/DescribeServer.php
+++ src/Com/Atproto/Server/DescribeServer.php
@@ -8,9 +8,6 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
-use function array_filter;
-use function http_build_query;
-
 /**
  * Describes the server's account creation requirements and capabilities. Implemented by PDS.
  *
@@ -29,7 +26,7 @@ final readonly class DescribeServer
             ->createRequest(
                 'GET',
                 $uri->withPath('xrpc/com.atproto.server.describeServer')
-                    ->withQuery(http_build_query(array_filter([])))
+                    ->withQuery(\http_build_query(\array_filter([])))
             );
 
         $headers = [
