1b85facc7e9e52cf95a7132814bd46c9499a1d1d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:06:59 -0500

    Update GetActorFeeds.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Feed/GetActorFeeds.php
+++ src/App/Bsky/Feed/GetActorFeeds.php
@@ -8,9 +8,6 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
-use function array_filter;
-use function http_build_query;
-
 /**
  * Get a list of feeds (feed generator records) created by the actor (in the actor's repo).
  *
@@ -33,7 +30,7 @@ final readonly class GetActorFeeds
             ->createRequest(
                 'GET',
                 $uri->withPath('xrpc/app.bsky.feed.getActorFeeds')
-                    ->withQuery(http_build_query(array_filter([
+                    ->withQuery(\http_build_query(\array_filter([
                         'actor' => $actor,
                         'limit' => $limit,
                         'cursor' => $cursor,
