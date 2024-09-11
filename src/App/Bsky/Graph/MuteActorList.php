5e63912d200c52681871067864c64e1690ddf320
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:02 -0500

    Update MuteActorList.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Graph/MuteActorList.php
+++ src/App/Bsky/Graph/MuteActorList.php
@@ -10,9 +10,6 @@ use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
 use RuntimeException;
 
-use function array_filter;
-use function json_encode;
-
 /**
  * Creates a mute relationship for the specified list of accounts. Mutes are private in Bluesky. Requires auth.
  *
@@ -40,7 +37,7 @@ final readonly class MuteActorList
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = json_encode(array_filter([
+        $jsonBody = \json_encode(\array_filter([
             'list' => $list,
         ]));
 
