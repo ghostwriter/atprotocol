f3a5411fc5c5b569d531167f5e339e7c4b96a5ba
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:01 -0500

    Update GetLists.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Graph/GetLists.php
+++ src/App/Bsky/Graph/GetLists.php
@@ -8,9 +8,6 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
-use function array_filter;
-use function http_build_query;
-
 /**
  * Enumerates the lists created by a specified account (actor).
  *
@@ -33,7 +30,7 @@ final readonly class GetLists
             ->createRequest(
                 'GET',
                 $uri->withPath('xrpc/app.bsky.graph.getLists')
-                    ->withQuery(http_build_query(array_filter([
+                    ->withQuery(\http_build_query(\array_filter([
                         'actor' => $actor,
                         'limit' => $limit,
                         'cursor' => $cursor,
