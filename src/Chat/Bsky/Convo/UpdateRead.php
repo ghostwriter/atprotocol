e87d0cdd73bc6559bb849bb1b92f1398dd945b4b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:04 -0500

    Update UpdateRead.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Chat/Bsky/Convo/UpdateRead.php
+++ src/Chat/Bsky/Convo/UpdateRead.php
@@ -10,9 +10,6 @@ use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
 use RuntimeException;
 
-use function array_filter;
-use function json_encode;
-
 /**
  *
  * @see UpdateReadTest
@@ -42,7 +39,7 @@ final readonly class UpdateRead
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = json_encode(array_filter([
+        $jsonBody = \json_encode(\array_filter([
             'convoId' => $convoId,
             'messageId' => $messageId,
         ]));
