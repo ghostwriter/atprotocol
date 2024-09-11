0252147d3526b43ef0d707be8e4ee92b45228f33
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:04 -0500

    Update SendMessageBatch.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Chat/Bsky/Convo/SendMessageBatch.php
+++ src/Chat/Bsky/Convo/SendMessageBatch.php
@@ -10,9 +10,6 @@ use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
 use RuntimeException;
 
-use function array_filter;
-use function json_encode;
-
 /**
  *
  * @see SendMessageBatchTest
@@ -39,7 +36,7 @@ final readonly class SendMessageBatch
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = json_encode(array_filter([
+        $jsonBody = \json_encode(\array_filter([
             'items' => $items,
         ]));
 
