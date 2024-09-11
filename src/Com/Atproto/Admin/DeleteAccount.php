e787b6bf69c6e59163c428ecd8f3ab2bd09bc7a0
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:05 -0500

    Update DeleteAccount.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Admin/DeleteAccount.php
+++ src/Com/Atproto/Admin/DeleteAccount.php
@@ -10,9 +10,6 @@ use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
 use RuntimeException;
 
-use function array_filter;
-use function json_encode;
-
 /**
  * Delete a user account as an administrator.
  *
@@ -40,7 +37,7 @@ final readonly class DeleteAccount
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = json_encode(array_filter([
+        $jsonBody = \json_encode(\array_filter([
             'did' => $did,
         ]));
 
