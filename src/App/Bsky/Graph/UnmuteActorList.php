a3b2bf98971fbb7f443241e6b61453beafed419f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:02 -0500

    Update UnmuteActorList.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Graph/UnmuteActorList.php
+++ src/App/Bsky/Graph/UnmuteActorList.php
@@ -10,9 +10,6 @@ use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
 use RuntimeException;
 
-use function array_filter;
-use function json_encode;
-
 /**
  * Unmutes the specified list of accounts. Requires auth.
  *
@@ -40,7 +37,7 @@ final readonly class UnmuteActorList
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = json_encode(array_filter([
+        $jsonBody = \json_encode(\array_filter([
             'list' => $list,
         ]));
 
