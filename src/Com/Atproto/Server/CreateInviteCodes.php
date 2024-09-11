6a832403331d8b973d4c7efb23b2221119100a67
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:07 -0500

    Update CreateInviteCodes.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Server/CreateInviteCodes.php
+++ src/Com/Atproto/Server/CreateInviteCodes.php
@@ -10,9 +10,6 @@ use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
 use RuntimeException;
 
-use function array_filter;
-use function json_encode;
-
 /**
  * Create invite codes.
  *
@@ -44,7 +41,7 @@ final readonly class CreateInviteCodes
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = json_encode(array_filter([
+        $jsonBody = \json_encode(\array_filter([
             'codeCount' => $codeCount,
             'useCount' => $useCount,
             'forAccounts' => $forAccounts,
