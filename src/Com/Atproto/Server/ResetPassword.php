6adc4ab10191fdc818784813d6720356fc0d756e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:08 -0500

    Update ResetPassword.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Server/ResetPassword.php
+++ src/Com/Atproto/Server/ResetPassword.php
@@ -10,9 +10,6 @@ use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
 use RuntimeException;
 
-use function array_filter;
-use function json_encode;
-
 /**
  * Reset a user account password using a token.
  *
@@ -40,7 +37,7 @@ final readonly class ResetPassword
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = json_encode(array_filter([
+        $jsonBody = \json_encode(\array_filter([
             'token' => $token,
             'password' => $password,
         ]));
