ed37b9642a2eb02a3f51ace09b8cef39e06d19eb
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:00 -0500

    Update GetTimeline.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Feed/GetTimeline.php
+++ src/App/Bsky/Feed/GetTimeline.php
@@ -8,9 +8,6 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
-use function array_filter;
-use function http_build_query;
-
 /**
  * Get a view of the requesting account's home timeline. This is expected to be some form of reverse-chronological feed.
  *
@@ -33,7 +30,7 @@ final readonly class GetTimeline
             ->createRequest(
                 'GET',
                 $uri->withPath('xrpc/app.bsky.feed.getTimeline')
-                    ->withQuery(http_build_query(array_filter([
+                    ->withQuery(\http_build_query(\array_filter([
                         'algorithm' => $algorithm,
                         'limit' => $limit,
                         'cursor' => $cursor,
