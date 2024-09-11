6414d9b5a25d7deecacfd2b73050f89b2b0db716
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:09 -0500

    Update RequestPhoneVerification.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Temp/RequestPhoneVerification.php
+++ src/Com/Atproto/Temp/RequestPhoneVerification.php
@@ -10,9 +10,6 @@ use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
 use RuntimeException;
 
-use function array_filter;
-use function json_encode;
-
 /**
  * Request a verification code to be sent to the supplied phone number.
  *
@@ -40,7 +37,7 @@ final readonly class RequestPhoneVerification
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = json_encode(array_filter([
+        $jsonBody = \json_encode(\array_filter([
             'phoneNumber' => $phoneNumber,
         ]));
 
