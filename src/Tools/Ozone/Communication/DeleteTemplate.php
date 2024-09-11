124695d434c6f19ca83694f123543209ec7923bf
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:09 -0500

    Update DeleteTemplate.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Tools/Ozone/Communication/DeleteTemplate.php
+++ src/Tools/Ozone/Communication/DeleteTemplate.php
@@ -10,9 +10,6 @@ use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
 use RuntimeException;
 
-use function array_filter;
-use function json_encode;
-
 /**
  * Delete a communication template.
  *
@@ -40,7 +37,7 @@ final readonly class DeleteTemplate
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = json_encode(array_filter([
+        $jsonBody = \json_encode(\array_filter([
             'id' => $id,
         ]));
 
