ef36f056bf9ee4547233c2f62975f94ffe33cb2b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:08 -0500

    Update RequestAccountDelete.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Server/RequestAccountDelete.php
+++ src/Com/Atproto/Server/RequestAccountDelete.php
@@ -10,9 +10,6 @@ use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
 use RuntimeException;
 
-use function array_filter;
-use function json_encode;
-
 /**
  * Initiate a user account deletion via email.
  *
@@ -40,7 +37,7 @@ final readonly class RequestAccountDelete
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = json_encode(array_filter([]));
+        $jsonBody = \json_encode(\array_filter([]));
 
         if ($jsonBody === false) {
             throw new RuntimeException('Failed to encode JSON');
