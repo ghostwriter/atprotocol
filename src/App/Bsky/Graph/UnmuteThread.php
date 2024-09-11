ff14697f4f94cdcb713c95fba75d7522a246fb73
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:02 -0500

    Update UnmuteThread.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Graph/UnmuteThread.php
+++ src/App/Bsky/Graph/UnmuteThread.php
@@ -10,9 +10,6 @@ use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
 use RuntimeException;
 
-use function array_filter;
-use function json_encode;
-
 /**
  * Unmutes the specified thread. Requires auth.
  *
@@ -40,7 +37,7 @@ final readonly class UnmuteThread
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = json_encode(array_filter([
+        $jsonBody = \json_encode(\array_filter([
             'root' => $root,
         ]));
 
