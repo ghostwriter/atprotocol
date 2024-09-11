4c00599dbfa8339217bea9c4fadba27203670ea7
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:02 -0500

    Update RegisterPush.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Notification/RegisterPush.php
+++ src/App/Bsky/Notification/RegisterPush.php
@@ -10,9 +10,6 @@ use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
 use RuntimeException;
 
-use function array_filter;
-use function json_encode;
-
 /**
  * Register to receive push notifications, via a specified service, for the requesting account. Requires auth.
  *
@@ -45,7 +42,7 @@ final readonly class RegisterPush
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = json_encode(array_filter([
+        $jsonBody = \json_encode(\array_filter([
             'serviceDid' => $serviceDid,
             'token' => $token,
             'platform' => $platform,
