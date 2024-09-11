208f04479e68acccd194316f241e671d4c879d90
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:10 -0500

    Update GetRecord.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Tools/Ozone/Moderation/GetRecord.php
+++ src/Tools/Ozone/Moderation/GetRecord.php
@@ -8,9 +8,6 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
-use function array_filter;
-use function http_build_query;
-
 /**
  * Get details about a record.
  *
@@ -29,7 +26,7 @@ final readonly class GetRecord
             ->createRequest(
                 'GET',
                 $uri->withPath('xrpc/tools.ozone.moderation.getRecord')
-                    ->withQuery(http_build_query(array_filter([
+                    ->withQuery(\http_build_query(\array_filter([
                         'uri' => $uri,
                         'cid' => $cid,
                     ])))
