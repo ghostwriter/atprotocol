aa94c4816e433d9681faa02c28abc2b67e6df6b8
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:05 -0500

    Update DisableAccountInvites.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Admin/DisableAccountInvites.php
+++ src/Com/Atproto/Admin/DisableAccountInvites.php
@@ -10,9 +10,6 @@ use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
 use RuntimeException;
 
-use function array_filter;
-use function json_encode;
-
 /**
  * Disable an account from receiving new invite codes, but does not invalidate existing codes.
  *
@@ -40,7 +37,7 @@ final readonly class DisableAccountInvites
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = json_encode(array_filter([
+        $jsonBody = \json_encode(\array_filter([
             'account' => $account,
             'note' => $note,
         ]));
