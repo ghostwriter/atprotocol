38df9d43ec121d135920102b71025042e8a1794c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:10 -0500

    Update UpdateTemplate.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Tools/Ozone/Communication/UpdateTemplate.php
+++ src/Tools/Ozone/Communication/UpdateTemplate.php
@@ -10,9 +10,6 @@ use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
 use RuntimeException;
 
-use function array_filter;
-use function json_encode;
-
 /**
  * Administrative action to update an existing communication template. Allows passing partial fields to patch specific fields only.
  *
@@ -48,7 +45,7 @@ final readonly class UpdateTemplate
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = json_encode(array_filter([
+        $jsonBody = \json_encode(\array_filter([
             'id' => $id,
             'name' => $name,
             'lang' => $lang,
