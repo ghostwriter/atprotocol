c01a1dc705aaea94655ccefd5109947fb4a8d0f5
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:02 -0500

    Update GetUnreadCount.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Notification/GetUnreadCount.php
+++ src/App/Bsky/Notification/GetUnreadCount.php
@@ -8,9 +8,6 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
-use function array_filter;
-use function http_build_query;
-
 /**
  * Count the number of unread notifications for the requesting account. Requires auth.
  *
@@ -29,7 +26,7 @@ final readonly class GetUnreadCount
             ->createRequest(
                 'GET',
                 $uri->withPath('xrpc/app.bsky.notification.getUnreadCount')
-                    ->withQuery(http_build_query(array_filter([
+                    ->withQuery(\http_build_query(\array_filter([
                         'priority' => $priority,
                         'seenAt' => $seenAt,
                     ])))
