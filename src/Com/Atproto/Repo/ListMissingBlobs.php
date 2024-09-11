03eb309193e996964bf7213308ba390d6205f825
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:07 -0500

    Update ListMissingBlobs.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Repo/ListMissingBlobs.php
+++ src/Com/Atproto/Repo/ListMissingBlobs.php
@@ -8,9 +8,6 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
-use function array_filter;
-use function http_build_query;
-
 /**
  * Returns a list of missing blobs for the requesting account. Intended to be used in the account migration flow.
  *
@@ -29,7 +26,7 @@ final readonly class ListMissingBlobs
             ->createRequest(
                 'GET',
                 $uri->withPath('xrpc/com.atproto.repo.listMissingBlobs')
-                    ->withQuery(http_build_query(array_filter([
+                    ->withQuery(\http_build_query(\array_filter([
                         'limit' => $limit,
                         'cursor' => $cursor,
                     ])))
