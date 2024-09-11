395e980fbf374ace6ec1f411c3d6d8a849910010
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:09 -0500

    Update NotifyOfUpdate.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Sync/NotifyOfUpdate.php
+++ src/Com/Atproto/Sync/NotifyOfUpdate.php
@@ -10,9 +10,6 @@ use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
 use RuntimeException;
 
-use function array_filter;
-use function json_encode;
-
 /**
  * Notify a crawling service of a recent update, and that crawling should resume. Intended use is after a gap between repo stream events caused the crawling service to disconnect. Does not require auth; implemented by Relay.
  *
@@ -40,7 +37,7 @@ final readonly class NotifyOfUpdate
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = json_encode(array_filter([
+        $jsonBody = \json_encode(\array_filter([
             'hostname' => $hostname,
         ]));
 
