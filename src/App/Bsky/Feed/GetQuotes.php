af5b96dd42b9f8c6cba1b70b572a4a37ec379b6b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:00 -0500

    Update GetQuotes.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Feed/GetQuotes.php
+++ src/App/Bsky/Feed/GetQuotes.php
@@ -8,9 +8,6 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
-use function array_filter;
-use function http_build_query;
-
 /**
  * Get a list of quotes for a given post.
  *
@@ -34,7 +31,7 @@ final readonly class GetQuotes
             ->createRequest(
                 'GET',
                 $uri->withPath('xrpc/app.bsky.feed.getQuotes')
-                    ->withQuery(http_build_query(array_filter([
+                    ->withQuery(\http_build_query(\array_filter([
                         'uri' => $uri,
                         'cid' => $cid,
                         'limit' => $limit,
