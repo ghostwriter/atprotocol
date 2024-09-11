25ea3e952145d0c642b424578c9d05a6858b38e9
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:08 -0500

    Update RequestEmailUpdate.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Server/RequestEmailUpdate.php
+++ src/Com/Atproto/Server/RequestEmailUpdate.php
@@ -10,9 +10,6 @@ use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
 use RuntimeException;
 
-use function array_filter;
-use function json_encode;
-
 /**
  * Request a token in order to update email.
  *
@@ -40,7 +37,7 @@ final readonly class RequestEmailUpdate
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = json_encode(array_filter([]));
+        $jsonBody = \json_encode(\array_filter([]));
 
         if ($jsonBody === false) {
             throw new RuntimeException('Failed to encode JSON');
