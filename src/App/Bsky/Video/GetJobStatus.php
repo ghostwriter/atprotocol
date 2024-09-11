0cdecd8499c683e49e68c3dbd1644aa8347d6df5
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:03 -0500

    Update GetJobStatus.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Video/GetJobStatus.php
+++ src/App/Bsky/Video/GetJobStatus.php
@@ -8,9 +8,6 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
-use function array_filter;
-use function http_build_query;
-
 /**
  * Get status details for a video processing job.
  *
@@ -29,7 +26,7 @@ final readonly class GetJobStatus
             ->createRequest(
                 'GET',
                 $uri->withPath('xrpc/app.bsky.video.getJobStatus')
-                    ->withQuery(http_build_query(array_filter([
+                    ->withQuery(\http_build_query(\array_filter([
                         'jobId' => $jobId,
                     ])))
             );
