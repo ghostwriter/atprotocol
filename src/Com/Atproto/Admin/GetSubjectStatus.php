c4d5ce8a533c92085f87182d84c0b1a0ce37e137
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:05 -0500

    Update GetSubjectStatus.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Admin/GetSubjectStatus.php
+++ src/Com/Atproto/Admin/GetSubjectStatus.php
@@ -8,9 +8,6 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
-use function array_filter;
-use function http_build_query;
-
 /**
  * Get the service-specific admin status of a subject (account, record, or blob).
  *
@@ -33,7 +30,7 @@ final readonly class GetSubjectStatus
             ->createRequest(
                 'GET',
                 $uri->withPath('xrpc/com.atproto.admin.getSubjectStatus')
-                    ->withQuery(http_build_query(array_filter([
+                    ->withQuery(\http_build_query(\array_filter([
                         'did' => $did,
                         'uri' => $uri,
                         'blob' => $blob,
