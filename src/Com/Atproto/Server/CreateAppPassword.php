99d448928bf29f9e2e20a2458891f94d7fce1b64
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:07 -0500

    Update CreateAppPassword.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Server/CreateAppPassword.php
+++ src/Com/Atproto/Server/CreateAppPassword.php
@@ -10,9 +10,6 @@ use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
 use RuntimeException;
 
-use function array_filter;
-use function json_encode;
-
 /**
  * Create an App Password.
  *
@@ -40,7 +37,7 @@ final readonly class CreateAppPassword
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = json_encode(array_filter([
+        $jsonBody = \json_encode(\array_filter([
             'name' => $name,
             'privileged' => $privileged,
         ]));
