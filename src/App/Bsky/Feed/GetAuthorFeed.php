ea69013bb4b1ca8e610e27296918a293f674d668
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:06:59 -0500

    Update GetAuthorFeed.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Feed/GetAuthorFeed.php
+++ src/App/Bsky/Feed/GetAuthorFeed.php
@@ -8,9 +8,6 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
-use function array_filter;
-use function http_build_query;
-
 /**
  * Get a view of an actor's 'author feed' (post and reposts by the author). Does not require auth.
  *
@@ -34,7 +31,7 @@ final readonly class GetAuthorFeed
             ->createRequest(
                 'GET',
                 $uri->withPath('xrpc/app.bsky.feed.getAuthorFeed')
-                    ->withQuery(http_build_query(array_filter([
+                    ->withQuery(\http_build_query(\array_filter([
                         'actor' => $actor,
                         'limit' => $limit,
                         'cursor' => $cursor,
