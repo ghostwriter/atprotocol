e8109cb763f63dfdab0c25fcf5ab7f138538bfe0
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:02 -0500

    Update PutPreferences.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Notification/PutPreferences.php
+++ src/App/Bsky/Notification/PutPreferences.php
@@ -10,9 +10,6 @@ use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
 use RuntimeException;
 
-use function array_filter;
-use function json_encode;
-
 /**
  * Set notification-related preferences for an account. Requires auth.
  *
@@ -40,7 +37,7 @@ final readonly class PutPreferences
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = json_encode(array_filter([
+        $jsonBody = \json_encode(\array_filter([
             'priority' => $priority,
         ]));
 
