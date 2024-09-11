d33396d04fb2dba56b60c60432c6d6ac10621b67
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:05 -0500

    Update SearchAccounts.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Admin/SearchAccounts.php
+++ src/Com/Atproto/Admin/SearchAccounts.php
@@ -8,9 +8,6 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
-use function array_filter;
-use function http_build_query;
-
 /**
  * Get list of accounts that matches your search query.
  *
@@ -33,7 +30,7 @@ final readonly class SearchAccounts
             ->createRequest(
                 'GET',
                 $uri->withPath('xrpc/com.atproto.admin.searchAccounts')
-                    ->withQuery(http_build_query(array_filter([
+                    ->withQuery(\http_build_query(\array_filter([
                         'email' => $email,
                         'cursor' => $cursor,
                         'limit' => $limit,
