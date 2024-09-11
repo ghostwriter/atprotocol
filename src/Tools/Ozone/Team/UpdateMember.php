7ac186abb3510b9c7ce90c0b966864b56694ee06
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:10 -0500

    Update UpdateMember.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Tools/Ozone/Team/UpdateMember.php
+++ src/Tools/Ozone/Team/UpdateMember.php
@@ -10,9 +10,6 @@ use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
 use RuntimeException;
 
-use function array_filter;
-use function json_encode;
-
 /**
  * Update a member in the ozone service. Requires admin role.
  *
@@ -44,7 +41,7 @@ final readonly class UpdateMember
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = json_encode(array_filter([
+        $jsonBody = \json_encode(\array_filter([
             'did' => $did,
             'disabled' => $disabled,
             'role' => $role,
