02122f8570914f40ce1f57ff1b99561be5a0a0b7
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:07 -0500

    Update DeactivateAccount.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Server/DeactivateAccount.php
+++ src/Com/Atproto/Server/DeactivateAccount.php
@@ -10,9 +10,6 @@ use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
 use RuntimeException;
 
-use function array_filter;
-use function json_encode;
-
 /**
  * Deactivates a currently active account. Stops serving of repo, and future writes to repo until reactivated. Used to finalize account migration with the old host after the account has been activated on the new host.
  *
@@ -40,7 +37,7 @@ final readonly class DeactivateAccount
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = json_encode(array_filter([
+        $jsonBody = \json_encode(\array_filter([
             'deleteAfter' => $deleteAfter,
         ]));
 
