3f762ae040a09539f21e3b034722a42e0687a7ce
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-11 10:07:01 -0500

    Update GetStarterPack.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Graph/GetStarterPack.php
+++ src/App/Bsky/Graph/GetStarterPack.php
@@ -8,9 +8,6 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
-use function array_filter;
-use function http_build_query;
-
 /**
  * Gets a view of a starter pack.
  *
@@ -29,7 +26,7 @@ final readonly class GetStarterPack
             ->createRequest(
                 'GET',
                 $uri->withPath('xrpc/app.bsky.graph.getStarterPack')
-                    ->withQuery(http_build_query(array_filter([
+                    ->withQuery(\http_build_query(\array_filter([
                         'starterPack' => $starterPack,
                     ])))
             );
