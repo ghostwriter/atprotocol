fdfa771884a7935935c788350156372c14132da4
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-13 15:40:41 -0500

    Update GetActorStarterPacks.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Graph/GetActorStarterPacks.php
+++ src/App/Bsky/Graph/GetActorStarterPacks.php
@@ -21,7 +21,7 @@ final readonly class GetActorStarterPacks
     }
 
     public function __invoke(
-        UriInterface $uri,
+        UriInterface $pdsUri,
         ?string $actor = null,
         ?int $limit = null,
         ?string $cursor = null,
@@ -29,7 +29,7 @@ final readonly class GetActorStarterPacks
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
-                $uri->withPath('xrpc/app.bsky.graph.getActorStarterPacks')
+                $pdsUri->withPath('xrpc/app.bsky.graph.getActorStarterPacks')
                     ->withQuery(\http_build_query(\array_filter([
                         'actor' => $actor,
                         'limit' => $limit,
