271cb425befba6861749a88d165dc85acef89682
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-13 15:40:55 -0500

    Update GetBlob.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Sync/GetBlob.php
+++ src/Com/Atproto/Sync/GetBlob.php
@@ -20,12 +20,12 @@ final readonly class GetBlob
     ) {
     }
 
-    public function __invoke(UriInterface $uri, ?string $did = null, ?string $cid = null): RequestInterface
+    public function __invoke(UriInterface $pdsUri, ?string $did = null, ?string $cid = null): RequestInterface
     {
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
-                $uri->withPath('xrpc/com.atproto.sync.getBlob')
+                $pdsUri->withPath('xrpc/com.atproto.sync.getBlob')
                     ->withQuery(\http_build_query(\array_filter([
                         'did' => $did,
                         'cid' => $cid,
