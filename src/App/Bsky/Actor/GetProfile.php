b7f1447d1698edd5aa08a670dde49e6e9f5fa379
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-13 15:40:38 -0500

    Update GetProfile.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Actor/GetProfile.php
+++ src/App/Bsky/Actor/GetProfile.php
@@ -20,12 +20,12 @@ final readonly class GetProfile
     ) {
     }
 
-    public function __invoke(UriInterface $uri, ?string $actor = null): RequestInterface
+    public function __invoke(UriInterface $pdsUri, ?string $actor = null): RequestInterface
     {
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
-                $uri->withPath('xrpc/app.bsky.actor.getProfile')
+                $pdsUri->withPath('xrpc/app.bsky.actor.getProfile')
                     ->withQuery(\http_build_query(\array_filter([
                         'actor' => $actor,
                     ])))
