6f56f20ab80d058d3b756fa03c1a6aa0b768b7b2
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-13 15:40:51 -0500

    Update ResolveHandle.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Identity/ResolveHandle.php
+++ src/Com/Atproto/Identity/ResolveHandle.php
@@ -20,12 +20,12 @@ final readonly class ResolveHandle
     ) {
     }
 
-    public function __invoke(UriInterface $uri, ?string $handle = null): RequestInterface
+    public function __invoke(UriInterface $pdsUri, ?string $handle = null): RequestInterface
     {
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
-                $uri->withPath('xrpc/com.atproto.identity.resolveHandle')
+                $pdsUri->withPath('xrpc/com.atproto.identity.resolveHandle')
                     ->withQuery(\http_build_query(\array_filter([
                         'handle' => $handle,
                     ])))
