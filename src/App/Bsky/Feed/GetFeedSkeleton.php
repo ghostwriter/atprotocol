1ac89c7aa5fcfcfc4939a77c945e62dcfa8f113c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-13 15:40:40 -0500

    Update GetFeedSkeleton.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Feed/GetFeedSkeleton.php
+++ src/App/Bsky/Feed/GetFeedSkeleton.php
@@ -21,7 +21,7 @@ final readonly class GetFeedSkeleton
     }
 
     public function __invoke(
-        UriInterface $uri,
+        UriInterface $pdsUri,
         ?string $feed = null,
         ?int $limit = null,
         ?string $cursor = null,
@@ -29,7 +29,7 @@ final readonly class GetFeedSkeleton
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
-                $uri->withPath('xrpc/app.bsky.feed.getFeedSkeleton')
+                $pdsUri->withPath('xrpc/app.bsky.feed.getFeedSkeleton')
                     ->withQuery(\http_build_query(\array_filter([
                         'feed' => $feed,
                         'limit' => $limit,
