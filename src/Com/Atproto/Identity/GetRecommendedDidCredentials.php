0ef54d6abd58938c74adbefb5ebff6e376526640
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-13 15:40:51 -0500

    Update GetRecommendedDidCredentials.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Identity/GetRecommendedDidCredentials.php
+++ src/Com/Atproto/Identity/GetRecommendedDidCredentials.php
@@ -20,12 +20,12 @@ final readonly class GetRecommendedDidCredentials
     ) {
     }
 
-    public function __invoke(UriInterface $uri): RequestInterface
+    public function __invoke(UriInterface $pdsUri): RequestInterface
     {
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
-                $uri->withPath('xrpc/com.atproto.identity.getRecommendedDidCredentials')
+                $pdsUri->withPath('xrpc/com.atproto.identity.getRecommendedDidCredentials')
                     ->withQuery(\http_build_query(\array_filter([])))
             );
 
