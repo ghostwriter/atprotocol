dcde9d0c78bef14c660875ff3fd1c6da611ff1e7
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-13 15:40:55 -0500

    Update RequestEmailConfirmation.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Server/RequestEmailConfirmation.php
+++ src/Com/Atproto/Server/RequestEmailConfirmation.php
@@ -23,10 +23,10 @@ final readonly class RequestEmailConfirmation
     ) {
     }
 
-    public function __invoke(UriInterface $uri): RequestInterface
+    public function __invoke(UriInterface $pdsUri): RequestInterface
     {
         $request = $this->requestFactory
-            ->createRequest('POST', $uri->withPath('xrpc/com.atproto.server.requestEmailConfirmation'));
+            ->createRequest('POST', $pdsUri->withPath('xrpc/com.atproto.server.requestEmailConfirmation'));
 
         $headers = [
             'Accept' => 'application/json',
