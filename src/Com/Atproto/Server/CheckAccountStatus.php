9a4e9e83181b8adc9bbb3aee829933c12fdc8406
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-13 15:40:53 -0500

    Update CheckAccountStatus.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Server/CheckAccountStatus.php
+++ src/Com/Atproto/Server/CheckAccountStatus.php
@@ -20,12 +20,12 @@ final readonly class CheckAccountStatus
     ) {
     }
 
-    public function __invoke(UriInterface $uri): RequestInterface
+    public function __invoke(UriInterface $pdsUri): RequestInterface
     {
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
-                $uri->withPath('xrpc/com.atproto.server.checkAccountStatus')
+                $pdsUri->withPath('xrpc/com.atproto.server.checkAccountStatus')
                     ->withQuery(\http_build_query(\array_filter([])))
             );
 
