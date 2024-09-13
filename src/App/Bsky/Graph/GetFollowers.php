39ffc700cf58faa2f3599c65144721a13362d861
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-13 15:40:41 -0500

    Update GetFollowers.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Graph/GetFollowers.php
+++ src/App/Bsky/Graph/GetFollowers.php
@@ -21,7 +21,7 @@ final readonly class GetFollowers
     }
 
     public function __invoke(
-        UriInterface $uri,
+        UriInterface $pdsUri,
         ?string $actor = null,
         ?int $limit = null,
         ?string $cursor = null,
@@ -29,7 +29,7 @@ final readonly class GetFollowers
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
-                $uri->withPath('xrpc/app.bsky.graph.getFollowers')
+                $pdsUri->withPath('xrpc/app.bsky.graph.getFollowers')
                     ->withQuery(\http_build_query(\array_filter([
                         'actor' => $actor,
                         'limit' => $limit,
