1e186832aea82d058c218c033b6fe02bc0ef4269
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-13 15:40:54 -0500

    Update GetSession.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Server/GetSession.php
+++ src/Com/Atproto/Server/GetSession.php
@@ -20,12 +20,12 @@ final readonly class GetSession
     ) {
     }
 
-    public function __invoke(UriInterface $uri): RequestInterface
+    public function __invoke(UriInterface $pdsUri): RequestInterface
     {
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
-                $uri->withPath('xrpc/com.atproto.server.getSession')
+                $pdsUri->withPath('xrpc/com.atproto.server.getSession')
                     ->withQuery(\http_build_query(\array_filter([])))
             );
 
