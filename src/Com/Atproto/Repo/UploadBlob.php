6bd259d20ffbc8321b66b5c93fd25769329b33a0
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-13 15:40:53 -0500

    Update UploadBlob.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Repo/UploadBlob.php
+++ src/Com/Atproto/Repo/UploadBlob.php
@@ -23,10 +23,10 @@ final readonly class UploadBlob
     ) {
     }
 
-    public function __invoke(UriInterface $uri): RequestInterface
+    public function __invoke(UriInterface $pdsUri): RequestInterface
     {
         $request = $this->requestFactory
-            ->createRequest('POST', $uri->withPath('xrpc/com.atproto.repo.uploadBlob'));
+            ->createRequest('POST', $pdsUri->withPath('xrpc/com.atproto.repo.uploadBlob'));
 
         $headers = [
             'Accept' => '*/*',
