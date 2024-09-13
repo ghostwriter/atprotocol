170c51ecf6262c57e6be684360888e571a3ede56
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-13 15:40:58 -0500

    Update ListTemplates.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Tools/Ozone/Communication/ListTemplates.php
+++ src/Tools/Ozone/Communication/ListTemplates.php
@@ -20,12 +20,12 @@ final readonly class ListTemplates
     ) {
     }
 
-    public function __invoke(UriInterface $uri): RequestInterface
+    public function __invoke(UriInterface $pdsUri): RequestInterface
     {
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
-                $uri->withPath('xrpc/tools.ozone.communication.listTemplates')
+                $pdsUri->withPath('xrpc/tools.ozone.communication.listTemplates')
                     ->withQuery(\http_build_query(\array_filter([])))
             );
 
