cd761fae4250d598b85bf027b4bbc1d0f7c9aeb2
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-13 15:40:53 -0500

    Update ActivateAccount.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Server/ActivateAccount.php
+++ src/Com/Atproto/Server/ActivateAccount.php
@@ -23,10 +23,10 @@ final readonly class ActivateAccount
     ) {
     }
 
-    public function __invoke(UriInterface $uri): RequestInterface
+    public function __invoke(UriInterface $pdsUri): RequestInterface
     {
         $request = $this->requestFactory
-            ->createRequest('POST', $uri->withPath('xrpc/com.atproto.server.activateAccount'));
+            ->createRequest('POST', $pdsUri->withPath('xrpc/com.atproto.server.activateAccount'));
 
         $headers = [
             'Accept' => 'application/json',
