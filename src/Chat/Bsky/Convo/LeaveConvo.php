5599a177b25d99250f45140f658ed0931af3baec
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-13 15:40:49 -0500

    Update LeaveConvo.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Chat/Bsky/Convo/LeaveConvo.php
+++ src/Chat/Bsky/Convo/LeaveConvo.php
@@ -22,10 +22,10 @@ final readonly class LeaveConvo
     ) {
     }
 
-    public function __invoke(UriInterface $uri, ?string $convoId = null): RequestInterface
+    public function __invoke(UriInterface $pdsUri, ?string $convoId = null): RequestInterface
     {
         $request = $this->requestFactory
-            ->createRequest('POST', $uri->withPath('xrpc/chat.bsky.convo.leaveConvo'));
+            ->createRequest('POST', $pdsUri->withPath('xrpc/chat.bsky.convo.leaveConvo'));
 
         $headers = [
             'Accept' => 'application/json',
