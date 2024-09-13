bae39e7dd9b52bf4b0ee8dc8d645e75ea931dfb5
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-13 15:40:48 -0500

    Update GetUploadLimits.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Video/GetUploadLimits.php
+++ src/App/Bsky/Video/GetUploadLimits.php
@@ -20,12 +20,12 @@ final readonly class GetUploadLimits
     ) {
     }
 
-    public function __invoke(UriInterface $uri): RequestInterface
+    public function __invoke(UriInterface $pdsUri): RequestInterface
     {
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
-                $uri->withPath('xrpc/app.bsky.video.getUploadLimits')
+                $pdsUri->withPath('xrpc/app.bsky.video.getUploadLimits')
                     ->withQuery(\http_build_query(\array_filter([])))
             );
 
