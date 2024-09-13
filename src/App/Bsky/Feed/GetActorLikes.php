af611c96d48d7aec9f91b82c94879671d9978883
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-09-13 15:40:39 -0500

    Update GetActorLikes.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Feed/GetActorLikes.php
+++ src/App/Bsky/Feed/GetActorLikes.php
@@ -21,7 +21,7 @@ final readonly class GetActorLikes
     }
 
     public function __invoke(
-        UriInterface $uri,
+        UriInterface $pdsUri,
         ?string $actor = null,
         ?int $limit = null,
         ?string $cursor = null,
@@ -29,7 +29,7 @@ final readonly class GetActorLikes
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
-                $uri->withPath('xrpc/app.bsky.feed.getActorLikes')
+                $pdsUri->withPath('xrpc/app.bsky.feed.getActorLikes')
                     ->withQuery(\http_build_query(\array_filter([
                         'actor' => $actor,
                         'limit' => $limit,
