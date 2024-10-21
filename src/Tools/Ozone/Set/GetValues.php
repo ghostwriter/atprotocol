82775eb2255569b4fdcbed7caf92125bf21aa05f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-21 06:42:01 -0500

    Add GetValues.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Tools/Ozone/Set/GetValues.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Tools\Ozone\Set;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * Get a specific set and its values.
+ *
+ * @see GetValuesTest
+ */
+final readonly class GetValues
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+    ) {
+    }
+
+    public function __invoke(
+        UriInterface $pdsUri,
+        ?string $name = null,
+        ?int $limit = null,
+        ?string $cursor = null,
+    ): RequestInterface {
+        $request = $this->requestFactory
+            ->createRequest(
+                'GET',
+                $pdsUri->withPath('xrpc/tools.ozone.set.getValues')
+                    ->withQuery(\http_build_query(\array_filter([
+                        'name' => $name,
+                        'limit' => $limit,
+                        'cursor' => $cursor,
+                    ])))
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request;
+    }
+}
