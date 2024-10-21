f727040f4ee7211a2734ec96dcc57c2b13d78cf5
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-21 06:42:00 -0500

    Add AddValues.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Tools/Ozone/Set/AddValues.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Tools\Ozone\Set;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\UriInterface;
+use RuntimeException;
+
+/**
+ * Add values to a specific set. Attempting to add values to a set that does not exist will result in an error.
+ *
+ * @see AddValuesTest
+ */
+final readonly class AddValues
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+        private StreamFactoryInterface $streamFactory,
+    ) {
+    }
+
+    public function __invoke(UriInterface $pdsUri, ?string $name = null, ?array $values = null): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest('POST', $pdsUri->withPath('xrpc/tools.ozone.set.addValues'));
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        $jsonBody = \json_encode(\array_filter([
+            'name' => $name,
+            'values' => $values,
+        ]));
+
+        if ($jsonBody === false) {
+            throw new RuntimeException('Failed to encode JSON');
+        }
+
+        return $request->withBody($this->streamFactory->createStream($jsonBody));
+    }
+}
