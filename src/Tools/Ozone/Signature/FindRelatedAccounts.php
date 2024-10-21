31f2a9dfbc037e1402b49c3e469fc79d3ecf54c4
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-21 06:42:02 -0500

    Add FindRelatedAccounts.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Tools/Ozone/Signature/FindRelatedAccounts.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Tools\Ozone\Signature;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * Get accounts that share some matching threat signatures with the root account.
+ *
+ * @see FindRelatedAccountsTest
+ */
+final readonly class FindRelatedAccounts
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+    ) {
+    }
+
+    public function __invoke(
+        UriInterface $pdsUri,
+        ?string $did = null,
+        ?string $cursor = null,
+        ?int $limit = null,
+    ): RequestInterface {
+        $request = $this->requestFactory
+            ->createRequest(
+                'GET',
+                $pdsUri->withPath('xrpc/tools.ozone.signature.findRelatedAccounts')
+                    ->withQuery(\http_build_query(\array_filter([
+                        'did' => $did,
+                        'cursor' => $cursor,
+                        'limit' => $limit,
+                    ])))
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request;
+    }
+}
