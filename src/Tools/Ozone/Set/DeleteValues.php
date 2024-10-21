020bd60501205d461289a2383f5e4a8ab86be8e7
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-21 06:42:01 -0500

    Add DeleteValues.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Tools/Ozone/Set/DeleteValues.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Tools\Ozone\Set;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\UriInterface;
+use RuntimeException;
+
+/**
+ * Delete values from a specific set. Attempting to delete values that are not in the set will not result in an error.
+ *
+ * @see DeleteValuesTest
+ */
+final readonly class DeleteValues
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+        private StreamFactoryInterface $streamFactory,
+    ) {
+    }
+
+    public function __invoke(UriInterface $pdsUri, ?string $name = null, ?array $values = null): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest('POST', $pdsUri->withPath('xrpc/tools.ozone.set.deleteValues'));
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        $jsonBody = \json_encode(\array_filter([
+            'name' => $name,
+            'values' => $values,
+        ]));
+
+        if ($jsonBody === false) {
+            throw new RuntimeException('Failed to encode JSON');
+        }
+
+        return $request->withBody($this->streamFactory->createStream($jsonBody));
+    }
+}
