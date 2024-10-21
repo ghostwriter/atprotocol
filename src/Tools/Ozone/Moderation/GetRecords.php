07633676dae3826c636c732f1fc3618d33029e1a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-21 06:42:00 -0500

    Add GetRecords.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Tools/Ozone/Moderation/GetRecords.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\Tools\Ozone\Moderation;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * Get details about some records.
+ *
+ * @see GetRecordsTest
+ */
+final readonly class GetRecords
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+    ) {
+    }
+
+    public function __invoke(UriInterface $pdsUri, ?array $uris = null): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'GET',
+                $pdsUri->withPath('xrpc/tools.ozone.moderation.getRecords')
+                    ->withQuery(\http_build_query(\array_filter([
+                        'uris' => $uris,
+                    ])))
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request;
+    }
+}
