74d8fe9b3983e7c93ff88d9f699df0c33efc1cd1
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-31 17:54:00 -0500

    Add GetConfig.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/App/Bsky/Unspecced/GetConfig.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\App\Bsky\Unspecced;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * Get miscellaneous runtime configuration.
+ *
+ * @see GetConfigTest
+ */
+final readonly class GetConfig
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+    ) {
+    }
+
+    public function __invoke(UriInterface $pdsUri): RequestInterface
+    {
+        $request = $this->requestFactory
+            ->createRequest(
+                'GET',
+                $pdsUri->withPath('xrpc/app.bsky.unspecced.getConfig')
+                    ->withQuery(\http_build_query(\array_filter([])))
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request;
+    }
+}
