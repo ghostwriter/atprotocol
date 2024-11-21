2043c41b2020394e0c8065656c69ddcf702a87b4
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-11-21 10:24:11 -0600

    Add SearchStarterPacksSkeleton.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/App/Bsky/Unspecced/SearchStarterPacksSkeleton.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AtProtocol\App\Bsky\Unspecced;
+
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * Backend Starter Pack search, returns only skeleton.
+ *
+ * @see SearchStarterPacksSkeletonTest
+ */
+final readonly class SearchStarterPacksSkeleton
+{
+    public function __construct(
+        private RequestFactoryInterface $requestFactory,
+    ) {
+    }
+
+    public function __invoke(
+        UriInterface $pdsUri,
+        ?string $q = null,
+        ?string $viewer = null,
+        ?int $limit = null,
+        ?string $cursor = null,
+    ): RequestInterface {
+        $request = $this->requestFactory
+            ->createRequest(
+                'GET',
+                $pdsUri->withPath('xrpc/app.bsky.unspecced.searchStarterPacksSkeleton')
+                    ->withQuery(\http_build_query(\array_filter([
+                        'q' => $q,
+                        'viewer' => $viewer,
+                        'limit' => $limit,
+                        'cursor' => $cursor,
+                    ])))
+            );
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json; charset=utf-8',
+        ];
+
+        foreach ($headers as $name => $value) {
+            $request = $request->withHeader($name, $value);
+        }
+
+        return $request;
+    }
+}
