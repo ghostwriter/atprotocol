8829f1f0dabdc3979c0ed15752352d1743909d97
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 15:06:52 -0600

    Update GetRecords.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Tools/Ozone/Moderation/GetRecords.php
+++ src/Tools/Ozone/Moderation/GetRecords.php
@@ -8,6 +8,9 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
+use function array_filter;
+use function http_build_query;
+
 /**
  * Get details about some records.
  *
@@ -17,21 +20,17 @@ final readonly class GetRecords
 {
     public function __construct(
         private RequestFactoryInterface $requestFactory,
-    ) {
-    }
+    ) {}
 
-    public function __invoke(
-        UriInterface $pdsUri,
-        ?array $uris = null,
-    ): RequestInterface
+    public function __invoke(UriInterface $pdsUri, ?array $uris = null): RequestInterface
     {
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
                 $pdsUri->withPath('xrpc/tools.ozone.moderation.getRecords')
-                    ->withQuery(\http_build_query(\array_filter([
-                    'uris' => $uris,
-                ])))
+                    ->withQuery(http_build_query(array_filter([
+                        'uris' => $uris,
+                    ])))
             );
 
         $headers = [
