19550af1216d9e7f1e5248ee1d5da3fa74185e8e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 15:06:37 -0600

    Update UploadBlob.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Repo/UploadBlob.php
+++ src/Com/Atproto/Repo/UploadBlob.php
@@ -11,6 +11,9 @@ use Psr\Http\Message\UriInterface;
 
 use const JSON_THROW_ON_ERROR;
 
+use function array_filter;
+use function json_encode;
+
 /**
  * Upload a new blob, to be referenced from a repository record. The blob will be deleted if it is not referenced within a time window (eg, minutes). Blob restrictions (mimetype, size, etc) are enforced when the reference is created. Requires auth, implemented by PDS.
  *
@@ -21,18 +24,12 @@ final readonly class UploadBlob
     public function __construct(
         private RequestFactoryInterface $requestFactory,
         private StreamFactoryInterface $streamFactory,
-    ) {
-    }
+    ) {}
 
-    public function __invoke(
-        UriInterface $pdsUri,
-    ): RequestInterface
+    public function __invoke(UriInterface $pdsUri): RequestInterface
     {
         $request = $this->requestFactory
-            ->createRequest(
-                'POST',
-                $pdsUri->withPath('xrpc/com.atproto.repo.uploadBlob')
-            );
+            ->createRequest('POST', $pdsUri->withPath('xrpc/com.atproto.repo.uploadBlob'));
 
         $headers = [
             'Accept' => '*/*',
@@ -43,14 +40,8 @@ final readonly class UploadBlob
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = \json_encode(\array_filter([
-            
-        ]), JSON_THROW_ON_ERROR);
+        $jsonBody = json_encode(array_filter([]), JSON_THROW_ON_ERROR);
 
-        return $request->withBody(
-            $this->streamFactory->createStream(
-                $jsonBody
-            )
-        );
+        return $request->withBody($this->streamFactory->createStream($jsonBody));
     }
 }
