3e3d9babf881194f7d7510731a322527c1324d0b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 15:06:10 -0600

    Update GetActorFeeds.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Feed/GetActorFeeds.php
+++ src/App/Bsky/Feed/GetActorFeeds.php
@@ -8,6 +8,9 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
+use function array_filter;
+use function http_build_query;
+
 /**
  * Get a list of feeds (feed generator records) created by the actor (in the actor's repo).
  *
@@ -17,25 +20,23 @@ final readonly class GetActorFeeds
 {
     public function __construct(
         private RequestFactoryInterface $requestFactory,
-    ) {
-    }
+    ) {}
 
     public function __invoke(
         UriInterface $pdsUri,
         ?string $actor = null,
         ?int $limit = null,
         ?string $cursor = null,
-    ): RequestInterface
-    {
+    ): RequestInterface {
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
                 $pdsUri->withPath('xrpc/app.bsky.feed.getActorFeeds')
-                    ->withQuery(\http_build_query(\array_filter([
-                    'actor' => $actor,
-                    'limit' => $limit,
-                    'cursor' => $cursor,
-                ])))
+                    ->withQuery(http_build_query(array_filter([
+                        'actor' => $actor,
+                        'limit' => $limit,
+                        'cursor' => $cursor,
+                    ])))
             );
 
         $headers = [
