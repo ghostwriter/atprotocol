96c526dc7731d016142bca29c5ee1e7d05e1f556
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 15:06:39 -0600

    Update DeactivateAccount.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Server/DeactivateAccount.php
+++ src/Com/Atproto/Server/DeactivateAccount.php
@@ -11,6 +11,9 @@ use Psr\Http\Message\UriInterface;
 
 use const JSON_THROW_ON_ERROR;
 
+use function array_filter;
+use function json_encode;
+
 /**
  * Deactivates a currently active account. Stops serving of repo, and future writes to repo until reactivated. Used to finalize account migration with the old host after the account has been activated on the new host.
  *
@@ -21,19 +24,12 @@ final readonly class DeactivateAccount
     public function __construct(
         private RequestFactoryInterface $requestFactory,
         private StreamFactoryInterface $streamFactory,
-    ) {
-    }
+    ) {}
 
-    public function __invoke(
-        UriInterface $pdsUri,
-        ?string $deleteAfter = null,
-    ): RequestInterface
+    public function __invoke(UriInterface $pdsUri, ?string $deleteAfter = null): RequestInterface
     {
         $request = $this->requestFactory
-            ->createRequest(
-                'POST',
-                $pdsUri->withPath('xrpc/com.atproto.server.deactivateAccount')
-            );
+            ->createRequest('POST', $pdsUri->withPath('xrpc/com.atproto.server.deactivateAccount'));
 
         $headers = [
             'Accept' => 'application/json',
@@ -44,14 +40,10 @@ final readonly class DeactivateAccount
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = \json_encode(\array_filter([
+        $jsonBody = json_encode(array_filter([
             'deleteAfter' => $deleteAfter,
         ]), JSON_THROW_ON_ERROR);
 
-        return $request->withBody(
-            $this->streamFactory->createStream(
-                $jsonBody
-            )
-        );
+        return $request->withBody($this->streamFactory->createStream($jsonBody));
     }
 }
