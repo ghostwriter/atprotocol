51b0fcc3275697596c12bef6563bc69663ebdaae
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 15:06:12 -0600

    Update GetFeedGenerators.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Feed/GetFeedGenerators.php
+++ src/App/Bsky/Feed/GetFeedGenerators.php
@@ -8,6 +8,9 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
+use function array_filter;
+use function http_build_query;
+
 /**
  * Get information about a list of feed generators.
  *
@@ -17,21 +20,17 @@ final readonly class GetFeedGenerators
 {
     public function __construct(
         private RequestFactoryInterface $requestFactory,
-    ) {
-    }
+    ) {}
 
-    public function __invoke(
-        UriInterface $pdsUri,
-        ?array $feeds = null,
-    ): RequestInterface
+    public function __invoke(UriInterface $pdsUri, ?array $feeds = null): RequestInterface
     {
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
                 $pdsUri->withPath('xrpc/app.bsky.feed.getFeedGenerators')
-                    ->withQuery(\http_build_query(\array_filter([
-                    'feeds' => $feeds,
-                ])))
+                    ->withQuery(http_build_query(array_filter([
+                        'feeds' => $feeds,
+                    ])))
             );
 
         $headers = [
