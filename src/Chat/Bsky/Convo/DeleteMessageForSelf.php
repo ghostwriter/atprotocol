2c88b73ed1486013d4929b2e2b0680d5368bb302
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 15:06:27 -0600

    Update DeleteMessageForSelf.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Chat/Bsky/Convo/DeleteMessageForSelf.php
+++ src/Chat/Bsky/Convo/DeleteMessageForSelf.php
@@ -11,8 +11,10 @@ use Psr\Http\Message\UriInterface;
 
 use const JSON_THROW_ON_ERROR;
 
+use function array_filter;
+use function json_encode;
+
 /**
- * DeleteMessageForSelf
  *
  * @see DeleteMessageForSelfTest
  */
@@ -21,20 +23,15 @@ final readonly class DeleteMessageForSelf
     public function __construct(
         private RequestFactoryInterface $requestFactory,
         private StreamFactoryInterface $streamFactory,
-    ) {
-    }
+    ) {}
 
     public function __invoke(
         UriInterface $pdsUri,
         ?string $convoId = null,
         ?string $messageId = null,
-    ): RequestInterface
-    {
+    ): RequestInterface {
         $request = $this->requestFactory
-            ->createRequest(
-                'POST',
-                $pdsUri->withPath('xrpc/chat.bsky.convo.deleteMessageForSelf')
-            );
+            ->createRequest('POST', $pdsUri->withPath('xrpc/chat.bsky.convo.deleteMessageForSelf'));
 
         $headers = [
             'Accept' => 'application/json',
@@ -45,15 +42,11 @@ final readonly class DeleteMessageForSelf
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = \json_encode(\array_filter([
+        $jsonBody = json_encode(array_filter([
             'convoId' => $convoId,
             'messageId' => $messageId,
         ]), JSON_THROW_ON_ERROR);
 
-        return $request->withBody(
-            $this->streamFactory->createStream(
-                $jsonBody
-            )
-        );
+        return $request->withBody($this->streamFactory->createStream($jsonBody));
     }
 }
