c6d5d0de525551837ca9ebb0b86d5c7eef6dc4ac
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 15:06:12 -0600

    Update GetLikes.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Feed/GetLikes.php
+++ src/App/Bsky/Feed/GetLikes.php
@@ -8,6 +8,9 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
+use function array_filter;
+use function http_build_query;
+
 /**
  * Get like records which reference a subject (by AT-URI and CID).
  *
@@ -17,8 +20,7 @@ final readonly class GetLikes
 {
     public function __construct(
         private RequestFactoryInterface $requestFactory,
-    ) {
-    }
+    ) {}
 
     public function __invoke(
         UriInterface $pdsUri,
@@ -26,18 +28,17 @@ final readonly class GetLikes
         ?string $cid = null,
         ?int $limit = null,
         ?string $cursor = null,
-    ): RequestInterface
-    {
+    ): RequestInterface {
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
                 $pdsUri->withPath('xrpc/app.bsky.feed.getLikes')
-                    ->withQuery(\http_build_query(\array_filter([
-                    'uri' => $uri,
-                    'cid' => $cid,
-                    'limit' => $limit,
-                    'cursor' => $cursor,
-                ])))
+                    ->withQuery(http_build_query(array_filter([
+                        'uri' => $uri,
+                        'cid' => $cid,
+                        'limit' => $limit,
+                        'cursor' => $cursor,
+                    ])))
             );
 
         $headers = [
