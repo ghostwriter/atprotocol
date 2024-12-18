1930017ac047d707c5af377c96ca78cfa25211c0
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 14:42:47 -0600

    Update LeaveConvo.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Chat/Bsky/Convo/LeaveConvo.php
+++ src/Chat/Bsky/Convo/LeaveConvo.php
@@ -8,9 +8,11 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
-use RuntimeException;
+
+use const JSON_THROW_ON_ERROR;
 
 /**
+ * LeaveConvo
  *
  * @see LeaveConvoTest
  */
@@ -22,10 +24,16 @@ final readonly class LeaveConvo
     ) {
     }
 
-    public function __invoke(UriInterface $pdsUri, ?string $convoId = null): RequestInterface
+    public function __invoke(
+        UriInterface $pdsUri,
+        ?string $convoId = null,
+    ): RequestInterface
     {
         $request = $this->requestFactory
-            ->createRequest('POST', $pdsUri->withPath('xrpc/chat.bsky.convo.leaveConvo'));
+            ->createRequest(
+                'POST',
+                $pdsUri->withPath('xrpc/chat.bsky.convo.leaveConvo')
+            );
 
         $headers = [
             'Accept' => 'application/json',
@@ -38,12 +46,12 @@ final readonly class LeaveConvo
 
         $jsonBody = \json_encode(\array_filter([
             'convoId' => $convoId,
-        ]));
+        ]), JSON_THROW_ON_ERROR);
 
-        if ($jsonBody === false) {
-            throw new RuntimeException('Failed to encode JSON');
-        }
-
-        return $request->withBody($this->streamFactory->createStream($jsonBody));
+        return $request->withBody(
+            $this->streamFactory->createStream(
+                $jsonBody
+            )
+        );
     }
 }
