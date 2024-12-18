f7b111d384fd08406363bc9a565398a8c80e571b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 14:42:33 -0600

    Update GetList.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Graph/GetList.php
+++ src/App/Bsky/Graph/GetList.php
@@ -25,16 +25,17 @@ final readonly class GetList
         ?string $list = null,
         ?int $limit = null,
         ?string $cursor = null,
-    ): RequestInterface {
+    ): RequestInterface
+    {
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
                 $pdsUri->withPath('xrpc/app.bsky.graph.getList')
                     ->withQuery(\http_build_query(\array_filter([
-                        'list' => $list,
-                        'limit' => $limit,
-                        'cursor' => $cursor,
-                    ])))
+                    'list' => $list,
+                    'limit' => $limit,
+                    'cursor' => $cursor,
+                ])))
             );
 
         $headers = [
