d8c3bf0f362e1744e7bc405b40d2789de4e53d92
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 14:43:20 -0600

    Update DeleteTemplate.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Tools/Ozone/Communication/DeleteTemplate.php
+++ src/Tools/Ozone/Communication/DeleteTemplate.php
@@ -8,7 +8,8 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
-use RuntimeException;
+
+use const JSON_THROW_ON_ERROR;
 
 /**
  * Delete a communication template.
@@ -23,10 +24,16 @@ final readonly class DeleteTemplate
     ) {
     }
 
-    public function __invoke(UriInterface $pdsUri, ?string $id = null): RequestInterface
+    public function __invoke(
+        UriInterface $pdsUri,
+        ?string $id = null,
+    ): RequestInterface
     {
         $request = $this->requestFactory
-            ->createRequest('POST', $pdsUri->withPath('xrpc/tools.ozone.communication.deleteTemplate'));
+            ->createRequest(
+                'POST',
+                $pdsUri->withPath('xrpc/tools.ozone.communication.deleteTemplate')
+            );
 
         $headers = [
             'Accept' => 'application/json',
@@ -39,12 +46,12 @@ final readonly class DeleteTemplate
 
         $jsonBody = \json_encode(\array_filter([
             'id' => $id,
-        ]));
+        ]), JSON_THROW_ON_ERROR);
 
-        if ($jsonBody === false) {
-            throw new RuntimeException('Failed to encode JSON');
-        }
-
-        return $request->withBody($this->streamFactory->createStream($jsonBody));
+        return $request->withBody(
+            $this->streamFactory->createStream(
+                $jsonBody
+            )
+        );
     }
 }
