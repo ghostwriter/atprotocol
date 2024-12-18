7a5867987df174102e446432cb9f714b963aae55
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 15:06:33 -0600

    Update UpdateSubjectStatus.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Admin/UpdateSubjectStatus.php
+++ src/Com/Atproto/Admin/UpdateSubjectStatus.php
@@ -11,6 +11,9 @@ use Psr\Http\Message\UriInterface;
 
 use const JSON_THROW_ON_ERROR;
 
+use function array_filter;
+use function json_encode;
+
 /**
  * Update the service-specific admin status of a subject (account, record, or blob).
  *
@@ -21,21 +24,16 @@ final readonly class UpdateSubjectStatus
     public function __construct(
         private RequestFactoryInterface $requestFactory,
         private StreamFactoryInterface $streamFactory,
-    ) {
-    }
+    ) {}
 
     public function __invoke(
         UriInterface $pdsUri,
         ?string $subject = null,
         ?string $takedown = null,
         ?string $deactivated = null,
-    ): RequestInterface
-    {
+    ): RequestInterface {
         $request = $this->requestFactory
-            ->createRequest(
-                'POST',
-                $pdsUri->withPath('xrpc/com.atproto.admin.updateSubjectStatus')
-            );
+            ->createRequest('POST', $pdsUri->withPath('xrpc/com.atproto.admin.updateSubjectStatus'));
 
         $headers = [
             'Accept' => 'application/json',
@@ -46,16 +44,12 @@ final readonly class UpdateSubjectStatus
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = \json_encode(\array_filter([
+        $jsonBody = json_encode(array_filter([
             'subject' => $subject,
             'takedown' => $takedown,
             'deactivated' => $deactivated,
         ]), JSON_THROW_ON_ERROR);
 
-        return $request->withBody(
-            $this->streamFactory->createStream(
-                $jsonBody
-            )
-        );
+        return $request->withBody($this->streamFactory->createStream($jsonBody));
     }
 }
