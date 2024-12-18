4ad5f67cd6e3c8c83ae56bb235c6433116c633c3
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 14:42:51 -0600

    Update GetSubjectStatus.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Admin/GetSubjectStatus.php
+++ src/Com/Atproto/Admin/GetSubjectStatus.php
@@ -25,16 +25,17 @@ final readonly class GetSubjectStatus
         ?string $did = null,
         ?string $uri = null,
         ?string $blob = null,
-    ): RequestInterface {
+    ): RequestInterface
+    {
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
                 $pdsUri->withPath('xrpc/com.atproto.admin.getSubjectStatus')
                     ->withQuery(\http_build_query(\array_filter([
-                        'did' => $did,
-                        'uri' => $uri,
-                        'blob' => $blob,
-                    ])))
+                    'did' => $did,
+                    'uri' => $uri,
+                    'blob' => $blob,
+                ])))
             );
 
         $headers = [
