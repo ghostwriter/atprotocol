ae30ec7c3310e80def09b5ba2f8e8dac652462e5
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 14:43:30 -0600

    Update ListMembers.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Tools/Ozone/Team/ListMembers.php
+++ src/Tools/Ozone/Team/ListMembers.php
@@ -20,16 +20,20 @@ final readonly class ListMembers
     ) {
     }
 
-    public function __invoke(UriInterface $pdsUri, ?int $limit = null, ?string $cursor = null): RequestInterface
+    public function __invoke(
+        UriInterface $pdsUri,
+        ?int $limit = null,
+        ?string $cursor = null,
+    ): RequestInterface
     {
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
                 $pdsUri->withPath('xrpc/tools.ozone.team.listMembers')
                     ->withQuery(\http_build_query(\array_filter([
-                        'limit' => $limit,
-                        'cursor' => $cursor,
-                    ])))
+                    'limit' => $limit,
+                    'cursor' => $cursor,
+                ])))
             );
 
         $headers = [
