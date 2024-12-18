df8ffad56c5fa1472c31e3b41783211d3af97ab4
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 14:42:23 -0600

    Update SearchActors.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Actor/SearchActors.php
+++ src/App/Bsky/Actor/SearchActors.php
@@ -25,16 +25,17 @@ final readonly class SearchActors
         ?string $q = null,
         ?int $limit = null,
         ?string $cursor = null,
-    ): RequestInterface {
+    ): RequestInterface
+    {
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
                 $pdsUri->withPath('xrpc/app.bsky.actor.searchActors')
                     ->withQuery(\http_build_query(\array_filter([
-                        'q' => $q,
-                        'limit' => $limit,
-                        'cursor' => $cursor,
-                    ])))
+                    'q' => $q,
+                    'limit' => $limit,
+                    'cursor' => $cursor,
+                ])))
             );
 
         $headers = [
