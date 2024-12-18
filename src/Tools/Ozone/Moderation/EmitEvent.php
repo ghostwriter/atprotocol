c7f81b56115f1b18c40e99a0d7b81f0b0816195c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 14:43:21 -0600

    Update EmitEvent.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Tools/Ozone/Moderation/EmitEvent.php
+++ src/Tools/Ozone/Moderation/EmitEvent.php
@@ -8,7 +8,8 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
-use RuntimeException;
+
+use const JSON_THROW_ON_ERROR;
 
 /**
  * Take a moderation action on an actor.
@@ -29,9 +30,13 @@ final readonly class EmitEvent
         ?string $subject = null,
         ?string $createdBy = null,
         ?array $subjectBlobCids = null,
-    ): RequestInterface {
+    ): RequestInterface
+    {
         $request = $this->requestFactory
-            ->createRequest('POST', $pdsUri->withPath('xrpc/tools.ozone.moderation.emitEvent'));
+            ->createRequest(
+                'POST',
+                $pdsUri->withPath('xrpc/tools.ozone.moderation.emitEvent')
+            );
 
         $headers = [
             'Accept' => 'application/json',
@@ -47,12 +52,12 @@ final readonly class EmitEvent
             'subject' => $subject,
             'subjectBlobCids' => $subjectBlobCids,
             'createdBy' => $createdBy,
-        ]));
+        ]), JSON_THROW_ON_ERROR);
 
-        if ($jsonBody === false) {
-            throw new RuntimeException('Failed to encode JSON');
-        }
-
-        return $request->withBody($this->streamFactory->createStream($jsonBody));
+        return $request->withBody(
+            $this->streamFactory->createStream(
+                $jsonBody
+            )
+        );
     }
 }
