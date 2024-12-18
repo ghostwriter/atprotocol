aebc30f5ceb5c0a11532aa80fb7d4947dea33dcf
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 14:42:23 -0600

    Update PutPreferences.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Actor/PutPreferences.php
+++ src/App/Bsky/Actor/PutPreferences.php
@@ -8,7 +8,8 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
-use RuntimeException;
+
+use const JSON_THROW_ON_ERROR;
 
 /**
  * Set the private preferences attached to the account.
@@ -23,10 +24,16 @@ final readonly class PutPreferences
     ) {
     }
 
-    public function __invoke(UriInterface $pdsUri, ?string $preferences = null): RequestInterface
+    public function __invoke(
+        UriInterface $pdsUri,
+        ?string $preferences = null,
+    ): RequestInterface
     {
         $request = $this->requestFactory
-            ->createRequest('POST', $pdsUri->withPath('xrpc/app.bsky.actor.putPreferences'));
+            ->createRequest(
+                'POST',
+                $pdsUri->withPath('xrpc/app.bsky.actor.putPreferences')
+            );
 
         $headers = [
             'Accept' => 'application/json',
@@ -39,12 +46,12 @@ final readonly class PutPreferences
 
         $jsonBody = \json_encode(\array_filter([
             'preferences' => $preferences,
-        ]));
+        ]), JSON_THROW_ON_ERROR);
 
-        if ($jsonBody === false) {
-            throw new RuntimeException('Failed to encode JSON');
-        }
-
-        return $request->withBody($this->streamFactory->createStream($jsonBody));
+        return $request->withBody(
+            $this->streamFactory->createStream(
+                $jsonBody
+            )
+        );
     }
 }
