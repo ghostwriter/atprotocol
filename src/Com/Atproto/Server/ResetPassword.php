48f6367592b302c123eac83e6cc77b48cadf536b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 15:06:42 -0600

    Update ResetPassword.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Server/ResetPassword.php
+++ src/Com/Atproto/Server/ResetPassword.php
@@ -11,6 +11,9 @@ use Psr\Http\Message\UriInterface;
 
 use const JSON_THROW_ON_ERROR;
 
+use function array_filter;
+use function json_encode;
+
 /**
  * Reset a user account password using a token.
  *
@@ -21,20 +24,15 @@ final readonly class ResetPassword
     public function __construct(
         private RequestFactoryInterface $requestFactory,
         private StreamFactoryInterface $streamFactory,
-    ) {
-    }
+    ) {}
 
     public function __invoke(
         UriInterface $pdsUri,
         ?string $token = null,
         ?string $password = null,
-    ): RequestInterface
-    {
+    ): RequestInterface {
         $request = $this->requestFactory
-            ->createRequest(
-                'POST',
-                $pdsUri->withPath('xrpc/com.atproto.server.resetPassword')
-            );
+            ->createRequest('POST', $pdsUri->withPath('xrpc/com.atproto.server.resetPassword'));
 
         $headers = [
             'Accept' => 'application/json',
@@ -45,15 +43,11 @@ final readonly class ResetPassword
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = \json_encode(\array_filter([
+        $jsonBody = json_encode(array_filter([
             'token' => $token,
             'password' => $password,
         ]), JSON_THROW_ON_ERROR);
 
-        return $request->withBody(
-            $this->streamFactory->createStream(
-                $jsonBody
-            )
-        );
+        return $request->withBody($this->streamFactory->createStream($jsonBody));
     }
 }
