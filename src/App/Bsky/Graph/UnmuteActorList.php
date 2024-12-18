b874abbf5a94e165968c0a368453abdb9f6a690e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 15:06:20 -0600

    Update UnmuteActorList.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Graph/UnmuteActorList.php
+++ src/App/Bsky/Graph/UnmuteActorList.php
@@ -11,6 +11,9 @@ use Psr\Http\Message\UriInterface;
 
 use const JSON_THROW_ON_ERROR;
 
+use function array_filter;
+use function json_encode;
+
 /**
  * Unmutes the specified list of accounts. Requires auth.
  *
@@ -21,19 +24,12 @@ final readonly class UnmuteActorList
     public function __construct(
         private RequestFactoryInterface $requestFactory,
         private StreamFactoryInterface $streamFactory,
-    ) {
-    }
+    ) {}
 
-    public function __invoke(
-        UriInterface $pdsUri,
-        ?string $list = null,
-    ): RequestInterface
+    public function __invoke(UriInterface $pdsUri, ?string $list = null): RequestInterface
     {
         $request = $this->requestFactory
-            ->createRequest(
-                'POST',
-                $pdsUri->withPath('xrpc/app.bsky.graph.unmuteActorList')
-            );
+            ->createRequest('POST', $pdsUri->withPath('xrpc/app.bsky.graph.unmuteActorList'));
 
         $headers = [
             'Accept' => 'application/json',
@@ -44,14 +40,10 @@ final readonly class UnmuteActorList
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = \json_encode(\array_filter([
+        $jsonBody = json_encode(array_filter([
             'list' => $list,
         ]), JSON_THROW_ON_ERROR);
 
-        return $request->withBody(
-            $this->streamFactory->createStream(
-                $jsonBody
-            )
-        );
+        return $request->withBody($this->streamFactory->createStream($jsonBody));
     }
 }
