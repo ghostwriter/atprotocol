88a19e2083c2df87c4e9eac5a0327e26b0d71128
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 15:06:06 -0600

    Update GetPreferences.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Actor/GetPreferences.php
+++ src/App/Bsky/Actor/GetPreferences.php
@@ -8,6 +8,9 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
+use function array_filter;
+use function http_build_query;
+
 /**
  * Get private preferences attached to the current account. Expected use is synchronization between multiple devices, and import/export during account migration. Requires auth.
  *
@@ -17,18 +20,15 @@ final readonly class GetPreferences
 {
     public function __construct(
         private RequestFactoryInterface $requestFactory,
-    ) {
-    }
+    ) {}
 
-    public function __invoke(
-        UriInterface $pdsUri,
-    ): RequestInterface
+    public function __invoke(UriInterface $pdsUri): RequestInterface
     {
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
                 $pdsUri->withPath('xrpc/app.bsky.actor.getPreferences')
-                    ->withQuery(\http_build_query(\array_filter([])))
+                    ->withQuery(http_build_query(array_filter([])))
             );
 
         $headers = [
