557c104bd0248a6e078d05f987295b615c999dfc
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 14:42:29 -0600

    Update GetSuggestedFeeds.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Feed/GetSuggestedFeeds.php
+++ src/App/Bsky/Feed/GetSuggestedFeeds.php
@@ -20,16 +20,20 @@ final readonly class GetSuggestedFeeds
     ) {
     }
 
-    public function __invoke(UriInterface $pdsUri, ?int $limit = null, ?string $cursor = null): RequestInterface
+    public function __invoke(
+        UriInterface $pdsUri,
+        ?int $limit = null,
+        ?string $cursor = null,
+    ): RequestInterface
     {
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
                 $pdsUri->withPath('xrpc/app.bsky.feed.getSuggestedFeeds')
                     ->withQuery(\http_build_query(\array_filter([
-                        'limit' => $limit,
-                        'cursor' => $cursor,
-                    ])))
+                    'limit' => $limit,
+                    'cursor' => $cursor,
+                ])))
             );
 
         $headers = [
