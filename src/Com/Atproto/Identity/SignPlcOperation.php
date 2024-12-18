9262ff9ffcf59037752ad5309bd4836ed647bdd5
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 14:42:58 -0600

    Update SignPlcOperation.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Identity/SignPlcOperation.php
+++ src/Com/Atproto/Identity/SignPlcOperation.php
@@ -8,7 +8,8 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
-use RuntimeException;
+
+use const JSON_THROW_ON_ERROR;
 
 /**
  * Signs a PLC operation to update some value(s) in the requesting DID's document.
@@ -30,9 +31,13 @@ final readonly class SignPlcOperation
         ?array $alsoKnownAs = null,
         ?string $verificationMethods = null,
         ?string $services = null,
-    ): RequestInterface {
+    ): RequestInterface
+    {
         $request = $this->requestFactory
-            ->createRequest('POST', $pdsUri->withPath('xrpc/com.atproto.identity.signPlcOperation'));
+            ->createRequest(
+                'POST',
+                $pdsUri->withPath('xrpc/com.atproto.identity.signPlcOperation')
+            );
 
         $headers = [
             'Accept' => 'application/json',
@@ -49,12 +54,12 @@ final readonly class SignPlcOperation
             'alsoKnownAs' => $alsoKnownAs,
             'verificationMethods' => $verificationMethods,
             'services' => $services,
-        ]));
+        ]), JSON_THROW_ON_ERROR);
 
-        if ($jsonBody === false) {
-            throw new RuntimeException('Failed to encode JSON');
-        }
-
-        return $request->withBody($this->streamFactory->createStream($jsonBody));
+        return $request->withBody(
+            $this->streamFactory->createStream(
+                $jsonBody
+            )
+        );
     }
 }
