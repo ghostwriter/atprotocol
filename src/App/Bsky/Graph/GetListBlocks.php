3ad928c650ba3957def953401f7f0af8eb56d49b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 14:42:34 -0600

    Update GetListBlocks.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Graph/GetListBlocks.php
+++ src/App/Bsky/Graph/GetListBlocks.php
@@ -20,16 +20,20 @@ final readonly class GetListBlocks
     ) {
     }
 
-    public function __invoke(UriInterface $pdsUri, ?int $limit = null, ?string $cursor = null): RequestInterface
+    public function __invoke(
+        UriInterface $pdsUri,
+        ?int $limit = null,
+        ?string $cursor = null,
+    ): RequestInterface
     {
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
                 $pdsUri->withPath('xrpc/app.bsky.graph.getListBlocks')
                     ->withQuery(\http_build_query(\array_filter([
-                        'limit' => $limit,
-                        'cursor' => $cursor,
-                    ])))
+                    'limit' => $limit,
+                    'cursor' => $cursor,
+                ])))
             );
 
         $headers = [
