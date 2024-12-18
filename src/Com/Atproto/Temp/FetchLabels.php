89994246b6af965ad74505c592dec533e14a2248
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 15:06:49 -0600

    Update FetchLabels.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Temp/FetchLabels.php
+++ src/Com/Atproto/Temp/FetchLabels.php
@@ -8,6 +8,9 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
+use function array_filter;
+use function http_build_query;
+
 /**
  * DEPRECATED: use queryLabels or subscribeLabels instead -- Fetch all labels from a labeler created after a certain date.
  *
@@ -17,23 +20,18 @@ final readonly class FetchLabels
 {
     public function __construct(
         private RequestFactoryInterface $requestFactory,
-    ) {
-    }
+    ) {}
 
-    public function __invoke(
-        UriInterface $pdsUri,
-        ?int $since = null,
-        ?int $limit = null,
-    ): RequestInterface
+    public function __invoke(UriInterface $pdsUri, ?int $since = null, ?int $limit = null): RequestInterface
     {
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
                 $pdsUri->withPath('xrpc/com.atproto.temp.fetchLabels')
-                    ->withQuery(\http_build_query(\array_filter([
-                    'since' => $since,
-                    'limit' => $limit,
-                ])))
+                    ->withQuery(http_build_query(array_filter([
+                        'since' => $since,
+                        'limit' => $limit,
+                    ])))
             );
 
         $headers = [
