bc348f91cf721f62f5fb39e911047119dff3db45
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 15:06:08 -0600

    Update PutPreferences.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Actor/PutPreferences.php
+++ src/App/Bsky/Actor/PutPreferences.php
@@ -11,6 +11,9 @@ use Psr\Http\Message\UriInterface;
 
 use const JSON_THROW_ON_ERROR;
 
+use function array_filter;
+use function json_encode;
+
 /**
  * Set the private preferences attached to the account.
  *
@@ -21,19 +24,12 @@ final readonly class PutPreferences
     public function __construct(
         private RequestFactoryInterface $requestFactory,
         private StreamFactoryInterface $streamFactory,
-    ) {
-    }
+    ) {}
 
-    public function __invoke(
-        UriInterface $pdsUri,
-        ?string $preferences = null,
-    ): RequestInterface
+    public function __invoke(UriInterface $pdsUri, ?string $preferences = null): RequestInterface
     {
         $request = $this->requestFactory
-            ->createRequest(
-                'POST',
-                $pdsUri->withPath('xrpc/app.bsky.actor.putPreferences')
-            );
+            ->createRequest('POST', $pdsUri->withPath('xrpc/app.bsky.actor.putPreferences'));
 
         $headers = [
             'Accept' => 'application/json',
@@ -44,14 +40,10 @@ final readonly class PutPreferences
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = \json_encode(\array_filter([
+        $jsonBody = json_encode(array_filter([
             'preferences' => $preferences,
         ]), JSON_THROW_ON_ERROR);
 
-        return $request->withBody(
-            $this->streamFactory->createStream(
-                $jsonBody
-            )
-        );
+        return $request->withBody($this->streamFactory->createStream($jsonBody));
     }
 }
