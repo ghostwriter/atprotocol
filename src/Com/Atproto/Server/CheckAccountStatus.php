86d2787b7dbf2cc9ae9a582ca6780af75695fe4c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 15:06:38 -0600

    Update CheckAccountStatus.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Server/CheckAccountStatus.php
+++ src/Com/Atproto/Server/CheckAccountStatus.php
@@ -8,6 +8,9 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
+use function array_filter;
+use function http_build_query;
+
 /**
  * Returns the status of an account, especially as pertaining to import or recovery. Can be called many times over the course of an account migration. Requires auth and can only be called pertaining to oneself.
  *
@@ -17,18 +20,15 @@ final readonly class CheckAccountStatus
 {
     public function __construct(
         private RequestFactoryInterface $requestFactory,
-    ) {
-    }
+    ) {}
 
-    public function __invoke(
-        UriInterface $pdsUri,
-    ): RequestInterface
+    public function __invoke(UriInterface $pdsUri): RequestInterface
     {
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
                 $pdsUri->withPath('xrpc/com.atproto.server.checkAccountStatus')
-                    ->withQuery(\http_build_query(\array_filter([])))
+                    ->withQuery(http_build_query(array_filter([])))
             );
 
         $headers = [
