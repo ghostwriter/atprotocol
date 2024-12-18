62d24aaadc6757bc9abdcf0b5955615137b601d2
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 15:06:13 -0600

    Update GetListFeed.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Feed/GetListFeed.php
+++ src/App/Bsky/Feed/GetListFeed.php
@@ -8,6 +8,9 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
+use function array_filter;
+use function http_build_query;
+
 /**
  * Get a feed of recent posts from a list (posts and reposts from any actors on the list). Does not require auth.
  *
@@ -17,25 +20,23 @@ final readonly class GetListFeed
 {
     public function __construct(
         private RequestFactoryInterface $requestFactory,
-    ) {
-    }
+    ) {}
 
     public function __invoke(
         UriInterface $pdsUri,
         ?string $list = null,
         ?int $limit = null,
         ?string $cursor = null,
-    ): RequestInterface
-    {
+    ): RequestInterface {
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
                 $pdsUri->withPath('xrpc/app.bsky.feed.getListFeed')
-                    ->withQuery(\http_build_query(\array_filter([
-                    'list' => $list,
-                    'limit' => $limit,
-                    'cursor' => $cursor,
-                ])))
+                    ->withQuery(http_build_query(array_filter([
+                        'list' => $list,
+                        'limit' => $limit,
+                        'cursor' => $cursor,
+                    ])))
             );
 
         $headers = [
