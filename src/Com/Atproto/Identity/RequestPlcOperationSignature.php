3692beb4b20543ea24d8d685b69283b13a2c20e9
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 14:42:58 -0600

    Update RequestPlcOperationSignature.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Identity/RequestPlcOperationSignature.php
+++ src/Com/Atproto/Identity/RequestPlcOperationSignature.php
@@ -8,7 +8,8 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
-use RuntimeException;
+
+use const JSON_THROW_ON_ERROR;
 
 /**
  * Request an email with a code to in order to request a signed PLC operation. Requires Auth.
@@ -23,10 +24,15 @@ final readonly class RequestPlcOperationSignature
     ) {
     }
 
-    public function __invoke(UriInterface $pdsUri): RequestInterface
+    public function __invoke(
+        UriInterface $pdsUri,
+    ): RequestInterface
     {
         $request = $this->requestFactory
-            ->createRequest('POST', $pdsUri->withPath('xrpc/com.atproto.identity.requestPlcOperationSignature'));
+            ->createRequest(
+                'POST',
+                $pdsUri->withPath('xrpc/com.atproto.identity.requestPlcOperationSignature')
+            );
 
         $headers = [
             'Accept' => 'application/json',
@@ -37,12 +43,14 @@ final readonly class RequestPlcOperationSignature
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = \json_encode(\array_filter([]));
+        $jsonBody = \json_encode(\array_filter([
+            
+        ]), JSON_THROW_ON_ERROR);
 
-        if ($jsonBody === false) {
-            throw new RuntimeException('Failed to encode JSON');
-        }
-
-        return $request->withBody($this->streamFactory->createStream($jsonBody));
+        return $request->withBody(
+            $this->streamFactory->createStream(
+                $jsonBody
+            )
+        );
     }
 }
