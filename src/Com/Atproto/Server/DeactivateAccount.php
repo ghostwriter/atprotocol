db9e854c483b23291541dcd73ede1967dfd13a43
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 14:43:08 -0600

    Update DeactivateAccount.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Server/DeactivateAccount.php
+++ src/Com/Atproto/Server/DeactivateAccount.php
@@ -8,7 +8,8 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
-use RuntimeException;
+
+use const JSON_THROW_ON_ERROR;
 
 /**
  * Deactivates a currently active account. Stops serving of repo, and future writes to repo until reactivated. Used to finalize account migration with the old host after the account has been activated on the new host.
@@ -23,10 +24,16 @@ final readonly class DeactivateAccount
     ) {
     }
 
-    public function __invoke(UriInterface $pdsUri, ?string $deleteAfter = null): RequestInterface
+    public function __invoke(
+        UriInterface $pdsUri,
+        ?string $deleteAfter = null,
+    ): RequestInterface
     {
         $request = $this->requestFactory
-            ->createRequest('POST', $pdsUri->withPath('xrpc/com.atproto.server.deactivateAccount'));
+            ->createRequest(
+                'POST',
+                $pdsUri->withPath('xrpc/com.atproto.server.deactivateAccount')
+            );
 
         $headers = [
             'Accept' => 'application/json',
@@ -39,12 +46,12 @@ final readonly class DeactivateAccount
 
         $jsonBody = \json_encode(\array_filter([
             'deleteAfter' => $deleteAfter,
-        ]));
+        ]), JSON_THROW_ON_ERROR);
 
-        if ($jsonBody === false) {
-            throw new RuntimeException('Failed to encode JSON');
-        }
-
-        return $request->withBody($this->streamFactory->createStream($jsonBody));
+        return $request->withBody(
+            $this->streamFactory->createStream(
+                $jsonBody
+            )
+        );
     }
 }
