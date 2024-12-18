c0a48a6261ccc853387ed5d3035a64785762c8be
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 14:43:28 -0600

    Update FindCorrelation.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Tools/Ozone/Signature/FindCorrelation.php
+++ src/Tools/Ozone/Signature/FindCorrelation.php
@@ -20,15 +20,18 @@ final readonly class FindCorrelation
     ) {
     }
 
-    public function __invoke(UriInterface $pdsUri, ?array $dids = null): RequestInterface
+    public function __invoke(
+        UriInterface $pdsUri,
+        ?array $dids = null,
+    ): RequestInterface
     {
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
                 $pdsUri->withPath('xrpc/tools.ozone.signature.findCorrelation')
                     ->withQuery(\http_build_query(\array_filter([
-                        'dids' => $dids,
-                    ])))
+                    'dids' => $dids,
+                ])))
             );
 
         $headers = [
