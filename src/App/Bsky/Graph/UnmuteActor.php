5938d776d1d6dcab327b08f80e91fde1621e3044
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 14:42:39 -0600

    Update UnmuteActor.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Graph/UnmuteActor.php
+++ src/App/Bsky/Graph/UnmuteActor.php
@@ -8,7 +8,8 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
-use RuntimeException;
+
+use const JSON_THROW_ON_ERROR;
 
 /**
  * Unmutes the specified account. Requires auth.
@@ -23,10 +24,16 @@ final readonly class UnmuteActor
     ) {
     }
 
-    public function __invoke(UriInterface $pdsUri, ?string $actor = null): RequestInterface
+    public function __invoke(
+        UriInterface $pdsUri,
+        ?string $actor = null,
+    ): RequestInterface
     {
         $request = $this->requestFactory
-            ->createRequest('POST', $pdsUri->withPath('xrpc/app.bsky.graph.unmuteActor'));
+            ->createRequest(
+                'POST',
+                $pdsUri->withPath('xrpc/app.bsky.graph.unmuteActor')
+            );
 
         $headers = [
             'Accept' => 'application/json',
@@ -39,12 +46,12 @@ final readonly class UnmuteActor
 
         $jsonBody = \json_encode(\array_filter([
             'actor' => $actor,
-        ]));
+        ]), JSON_THROW_ON_ERROR);
 
-        if ($jsonBody === false) {
-            throw new RuntimeException('Failed to encode JSON');
-        }
-
-        return $request->withBody($this->streamFactory->createStream($jsonBody));
+        return $request->withBody(
+            $this->streamFactory->createStream(
+                $jsonBody
+            )
+        );
     }
 }
