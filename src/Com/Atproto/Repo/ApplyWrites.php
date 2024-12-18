7cef5ffc5c97b766882ee47217e5616c774c742f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 14:43:01 -0600

    Update ApplyWrites.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Repo/ApplyWrites.php
+++ src/Com/Atproto/Repo/ApplyWrites.php
@@ -8,7 +8,8 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
-use RuntimeException;
+
+use const JSON_THROW_ON_ERROR;
 
 /**
  * Apply a batch transaction of repository creates, updates, and deletes. Requires auth, implemented by PDS.
@@ -29,9 +30,13 @@ final readonly class ApplyWrites
         ?array $writes = null,
         ?bool $validate = null,
         ?string $swapCommit = null,
-    ): RequestInterface {
+    ): RequestInterface
+    {
         $request = $this->requestFactory
-            ->createRequest('POST', $pdsUri->withPath('xrpc/com.atproto.repo.applyWrites'));
+            ->createRequest(
+                'POST',
+                $pdsUri->withPath('xrpc/com.atproto.repo.applyWrites')
+            );
 
         $headers = [
             'Accept' => 'application/json',
@@ -47,12 +52,12 @@ final readonly class ApplyWrites
             'validate' => $validate,
             'writes' => $writes,
             'swapCommit' => $swapCommit,
-        ]));
+        ]), JSON_THROW_ON_ERROR);
 
-        if ($jsonBody === false) {
-            throw new RuntimeException('Failed to encode JSON');
-        }
-
-        return $request->withBody($this->streamFactory->createStream($jsonBody));
+        return $request->withBody(
+            $this->streamFactory->createStream(
+                $jsonBody
+            )
+        );
     }
 }
