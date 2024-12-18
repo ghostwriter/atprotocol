461fcfe28c22040f86e57f1ff100d193adb127f1
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 15:06:14 -0600

    Update GetSuggestedFeeds.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Feed/GetSuggestedFeeds.php
+++ src/App/Bsky/Feed/GetSuggestedFeeds.php
@@ -8,6 +8,9 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
+use function array_filter;
+use function http_build_query;
+
 /**
  * Get a list of suggested feeds (feed generators) for the requesting account.
  *
@@ -17,23 +20,18 @@ final readonly class GetSuggestedFeeds
 {
     public function __construct(
         private RequestFactoryInterface $requestFactory,
-    ) {
-    }
+    ) {}
 
-    public function __invoke(
-        UriInterface $pdsUri,
-        ?int $limit = null,
-        ?string $cursor = null,
-    ): RequestInterface
+    public function __invoke(UriInterface $pdsUri, ?int $limit = null, ?string $cursor = null): RequestInterface
     {
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
                 $pdsUri->withPath('xrpc/app.bsky.feed.getSuggestedFeeds')
-                    ->withQuery(\http_build_query(\array_filter([
-                    'limit' => $limit,
-                    'cursor' => $cursor,
-                ])))
+                    ->withQuery(http_build_query(array_filter([
+                        'limit' => $limit,
+                        'cursor' => $cursor,
+                    ])))
             );
 
         $headers = [
