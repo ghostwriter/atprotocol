905a81f86bd87833327ff5f5a5dc2c572d0fa580
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 15:06:35 -0600

    Update CreateRecord.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Repo/CreateRecord.php
+++ src/Com/Atproto/Repo/CreateRecord.php
@@ -11,6 +11,9 @@ use Psr\Http\Message\UriInterface;
 
 use const JSON_THROW_ON_ERROR;
 
+use function array_filter;
+use function json_encode;
+
 /**
  * Create a single new repository record. Requires auth, implemented by PDS.
  *
@@ -21,8 +24,7 @@ final readonly class CreateRecord
     public function __construct(
         private RequestFactoryInterface $requestFactory,
         private StreamFactoryInterface $streamFactory,
-    ) {
-    }
+    ) {}
 
     public function __invoke(
         UriInterface $pdsUri,
@@ -32,13 +34,9 @@ final readonly class CreateRecord
         ?string $rkey = null,
         ?bool $validate = null,
         ?string $swapCommit = null,
-    ): RequestInterface
-    {
+    ): RequestInterface {
         $request = $this->requestFactory
-            ->createRequest(
-                'POST',
-                $pdsUri->withPath('xrpc/com.atproto.repo.createRecord')
-            );
+            ->createRequest('POST', $pdsUri->withPath('xrpc/com.atproto.repo.createRecord'));
 
         $headers = [
             'Accept' => 'application/json',
@@ -49,7 +47,7 @@ final readonly class CreateRecord
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = \json_encode(\array_filter([
+        $jsonBody = json_encode(array_filter([
             'repo' => $repo,
             'collection' => $collection,
             'rkey' => $rkey,
@@ -58,10 +56,6 @@ final readonly class CreateRecord
             'swapCommit' => $swapCommit,
         ]), JSON_THROW_ON_ERROR);
 
-        return $request->withBody(
-            $this->streamFactory->createStream(
-                $jsonBody
-            )
-        );
+        return $request->withBody($this->streamFactory->createStream($jsonBody));
     }
 }
