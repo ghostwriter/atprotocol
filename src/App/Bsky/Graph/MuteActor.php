cb855b881106a393f606e90cf68f5573051491b3
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 15:06:19 -0600

    Update MuteActor.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Graph/MuteActor.php
+++ src/App/Bsky/Graph/MuteActor.php
@@ -11,6 +11,9 @@ use Psr\Http\Message\UriInterface;
 
 use const JSON_THROW_ON_ERROR;
 
+use function array_filter;
+use function json_encode;
+
 /**
  * Creates a mute relationship for the specified account. Mutes are private in Bluesky. Requires auth.
  *
@@ -21,19 +24,12 @@ final readonly class MuteActor
     public function __construct(
         private RequestFactoryInterface $requestFactory,
         private StreamFactoryInterface $streamFactory,
-    ) {
-    }
+    ) {}
 
-    public function __invoke(
-        UriInterface $pdsUri,
-        ?string $actor = null,
-    ): RequestInterface
+    public function __invoke(UriInterface $pdsUri, ?string $actor = null): RequestInterface
     {
         $request = $this->requestFactory
-            ->createRequest(
-                'POST',
-                $pdsUri->withPath('xrpc/app.bsky.graph.muteActor')
-            );
+            ->createRequest('POST', $pdsUri->withPath('xrpc/app.bsky.graph.muteActor'));
 
         $headers = [
             'Accept' => 'application/json',
@@ -44,14 +40,10 @@ final readonly class MuteActor
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = \json_encode(\array_filter([
+        $jsonBody = json_encode(array_filter([
             'actor' => $actor,
         ]), JSON_THROW_ON_ERROR);
 
-        return $request->withBody(
-            $this->streamFactory->createStream(
-                $jsonBody
-            )
-        );
+        return $request->withBody($this->streamFactory->createStream($jsonBody));
     }
 }
