382b1d31e990b7a310b669f05ed4da4bf651e605
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 15:06:37 -0600

    Update ListRecords.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Repo/ListRecords.php
+++ src/Com/Atproto/Repo/ListRecords.php
@@ -8,6 +8,9 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
+use function array_filter;
+use function http_build_query;
+
 /**
  * List a range of records in a repository, matching a specific collection. Does not require auth.
  *
@@ -17,8 +20,7 @@ final readonly class ListRecords
 {
     public function __construct(
         private RequestFactoryInterface $requestFactory,
-    ) {
-    }
+    ) {}
 
     public function __invoke(
         UriInterface $pdsUri,
@@ -27,19 +29,18 @@ final readonly class ListRecords
         ?int $limit = null,
         ?string $cursor = null,
         ?bool $reverse = null,
-    ): RequestInterface
-    {
+    ): RequestInterface {
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
                 $pdsUri->withPath('xrpc/com.atproto.repo.listRecords')
-                    ->withQuery(\http_build_query(\array_filter([
-                    'repo' => $repo,
-                    'collection' => $collection,
-                    'limit' => $limit,
-                    'cursor' => $cursor,
-                    'reverse' => $reverse,
-                ])))
+                    ->withQuery(http_build_query(array_filter([
+                        'repo' => $repo,
+                        'collection' => $collection,
+                        'limit' => $limit,
+                        'cursor' => $cursor,
+                        'reverse' => $reverse,
+                    ])))
             );
 
         $headers = [
