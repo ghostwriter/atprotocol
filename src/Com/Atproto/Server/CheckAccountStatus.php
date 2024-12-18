f1b2a04a703bc2f56d1626b491b05109b14d5ed5
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 14:43:06 -0600

    Update CheckAccountStatus.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Server/CheckAccountStatus.php
+++ src/Com/Atproto/Server/CheckAccountStatus.php
@@ -20,7 +20,9 @@ final readonly class CheckAccountStatus
     ) {
     }
 
-    public function __invoke(UriInterface $pdsUri): RequestInterface
+    public function __invoke(
+        UriInterface $pdsUri,
+    ): RequestInterface
     {
         $request = $this->requestFactory
             ->createRequest(
