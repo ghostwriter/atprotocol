2e916ff70e3a640a7537161fd48d4254d529e2da
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 14:42:45 -0600

    Update DeleteAccount.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Chat/Bsky/Actor/DeleteAccount.php
+++ src/Chat/Bsky/Actor/DeleteAccount.php
@@ -8,9 +8,11 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
-use RuntimeException;
+
+use const JSON_THROW_ON_ERROR;
 
 /**
+ * DeleteAccount
  *
  * @see DeleteAccountTest
  */
@@ -22,10 +24,15 @@ final readonly class DeleteAccount
     ) {
     }
 
-    public function __invoke(UriInterface $pdsUri): RequestInterface
+    public function __invoke(
+        UriInterface $pdsUri,
+    ): RequestInterface
     {
         $request = $this->requestFactory
-            ->createRequest('POST', $pdsUri->withPath('xrpc/chat.bsky.actor.deleteAccount'));
+            ->createRequest(
+                'POST',
+                $pdsUri->withPath('xrpc/chat.bsky.actor.deleteAccount')
+            );
 
         $headers = [
             'Accept' => 'application/json',
@@ -36,12 +43,14 @@ final readonly class DeleteAccount
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = \json_encode(\array_filter([]));
+        $jsonBody = \json_encode(\array_filter([
+            
+        ]), JSON_THROW_ON_ERROR);
 
-        if ($jsonBody === false) {
-            throw new RuntimeException('Failed to encode JSON');
-        }
-
-        return $request->withBody($this->streamFactory->createStream($jsonBody));
+        return $request->withBody(
+            $this->streamFactory->createStream(
+                $jsonBody
+            )
+        );
     }
 }
