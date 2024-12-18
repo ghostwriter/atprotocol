836e9d51aee1b36901b288085c2a838a4e1e0558
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 15:06:54 -0600

    Update AddValues.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Tools/Ozone/Set/AddValues.php
+++ src/Tools/Ozone/Set/AddValues.php
@@ -11,6 +11,9 @@ use Psr\Http\Message\UriInterface;
 
 use const JSON_THROW_ON_ERROR;
 
+use function array_filter;
+use function json_encode;
+
 /**
  * Add values to a specific set. Attempting to add values to a set that does not exist will result in an error.
  *
@@ -21,20 +24,12 @@ final readonly class AddValues
     public function __construct(
         private RequestFactoryInterface $requestFactory,
         private StreamFactoryInterface $streamFactory,
-    ) {
-    }
+    ) {}
 
-    public function __invoke(
-        UriInterface $pdsUri,
-        ?string $name = null,
-        ?array $values = null,
-    ): RequestInterface
+    public function __invoke(UriInterface $pdsUri, ?string $name = null, ?array $values = null): RequestInterface
     {
         $request = $this->requestFactory
-            ->createRequest(
-                'POST',
-                $pdsUri->withPath('xrpc/tools.ozone.set.addValues')
-            );
+            ->createRequest('POST', $pdsUri->withPath('xrpc/tools.ozone.set.addValues'));
 
         $headers = [
             'Accept' => 'application/json',
@@ -45,15 +40,11 @@ final readonly class AddValues
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = \json_encode(\array_filter([
+        $jsonBody = json_encode(array_filter([
             'name' => $name,
             'values' => $values,
         ]), JSON_THROW_ON_ERROR);
 
-        return $request->withBody(
-            $this->streamFactory->createStream(
-                $jsonBody
-            )
-        );
+        return $request->withBody($this->streamFactory->createStream($jsonBody));
     }
 }
