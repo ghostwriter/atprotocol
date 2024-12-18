335b63f0f46738b66aa2f71a956fc18efa435f2c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 15:06:43 -0600

    Update RevokeAppPassword.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Server/RevokeAppPassword.php
+++ src/Com/Atproto/Server/RevokeAppPassword.php
@@ -11,6 +11,9 @@ use Psr\Http\Message\UriInterface;
 
 use const JSON_THROW_ON_ERROR;
 
+use function array_filter;
+use function json_encode;
+
 /**
  * Revoke an App Password by name.
  *
@@ -21,19 +24,12 @@ final readonly class RevokeAppPassword
     public function __construct(
         private RequestFactoryInterface $requestFactory,
         private StreamFactoryInterface $streamFactory,
-    ) {
-    }
+    ) {}
 
-    public function __invoke(
-        UriInterface $pdsUri,
-        ?string $name = null,
-    ): RequestInterface
+    public function __invoke(UriInterface $pdsUri, ?string $name = null): RequestInterface
     {
         $request = $this->requestFactory
-            ->createRequest(
-                'POST',
-                $pdsUri->withPath('xrpc/com.atproto.server.revokeAppPassword')
-            );
+            ->createRequest('POST', $pdsUri->withPath('xrpc/com.atproto.server.revokeAppPassword'));
 
         $headers = [
             'Accept' => 'application/json',
@@ -44,14 +40,10 @@ final readonly class RevokeAppPassword
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = \json_encode(\array_filter([
+        $jsonBody = json_encode(array_filter([
             'name' => $name,
         ]), JSON_THROW_ON_ERROR);
 
-        return $request->withBody(
-            $this->streamFactory->createStream(
-                $jsonBody
-            )
-        );
+        return $request->withBody($this->streamFactory->createStream($jsonBody));
     }
 }
