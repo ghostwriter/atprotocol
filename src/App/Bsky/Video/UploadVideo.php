b3c7e762f17101078804794d666cb2d355e19dd1
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 15:06:26 -0600

    Update UploadVideo.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/App/Bsky/Video/UploadVideo.php
+++ src/App/Bsky/Video/UploadVideo.php
@@ -11,6 +11,9 @@ use Psr\Http\Message\UriInterface;
 
 use const JSON_THROW_ON_ERROR;
 
+use function array_filter;
+use function json_encode;
+
 /**
  * Upload a video to be processed then stored on the PDS.
  *
@@ -21,18 +24,12 @@ final readonly class UploadVideo
     public function __construct(
         private RequestFactoryInterface $requestFactory,
         private StreamFactoryInterface $streamFactory,
-    ) {
-    }
+    ) {}
 
-    public function __invoke(
-        UriInterface $pdsUri,
-    ): RequestInterface
+    public function __invoke(UriInterface $pdsUri): RequestInterface
     {
         $request = $this->requestFactory
-            ->createRequest(
-                'POST',
-                $pdsUri->withPath('xrpc/app.bsky.video.uploadVideo')
-            );
+            ->createRequest('POST', $pdsUri->withPath('xrpc/app.bsky.video.uploadVideo'));
 
         $headers = [
             'Accept' => 'video/mp4',
@@ -43,14 +40,8 @@ final readonly class UploadVideo
             $request = $request->withHeader($name, $value);
         }
 
-        $jsonBody = \json_encode(\array_filter([
-            
-        ]), JSON_THROW_ON_ERROR);
+        $jsonBody = json_encode(array_filter([]), JSON_THROW_ON_ERROR);
 
-        return $request->withBody(
-            $this->streamFactory->createStream(
-                $jsonBody
-            )
-        );
+        return $request->withBody($this->streamFactory->createStream($jsonBody));
     }
 }
