cb7fad61105d1bd320715a504014442a22f2d419
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 14:43:07 -0600

    Update CreateAccount.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Server/CreateAccount.php
+++ src/Com/Atproto/Server/CreateAccount.php
@@ -8,7 +8,8 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\StreamFactoryInterface;
 use Psr\Http\Message\UriInterface;
-use RuntimeException;
+
+use const JSON_THROW_ON_ERROR;
 
 /**
  * Create an account. Implemented by PDS.
@@ -34,9 +35,13 @@ final readonly class CreateAccount
         ?string $password = null,
         ?string $recoveryKey = null,
         ?string $plcOp = null,
-    ): RequestInterface {
+    ): RequestInterface
+    {
         $request = $this->requestFactory
-            ->createRequest('POST', $pdsUri->withPath('xrpc/com.atproto.server.createAccount'));
+            ->createRequest(
+                'POST',
+                $pdsUri->withPath('xrpc/com.atproto.server.createAccount')
+            );
 
         $headers = [
             'Accept' => 'application/json',
@@ -57,12 +62,12 @@ final readonly class CreateAccount
             'password' => $password,
             'recoveryKey' => $recoveryKey,
             'plcOp' => $plcOp,
-        ]));
+        ]), JSON_THROW_ON_ERROR);
 
-        if ($jsonBody === false) {
-            throw new RuntimeException('Failed to encode JSON');
-        }
-
-        return $request->withBody($this->streamFactory->createStream($jsonBody));
+        return $request->withBody(
+            $this->streamFactory->createStream(
+                $jsonBody
+            )
+        );
     }
 }
