61080c53038a05c7a2a786b42fa4c22914c5e239
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 15:06:44 -0600

    Update GetRecord.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Com/Atproto/Sync/GetRecord.php
+++ src/Com/Atproto/Sync/GetRecord.php
@@ -8,6 +8,9 @@ use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\UriInterface;
 
+use function array_filter;
+use function http_build_query;
+
 /**
  * Get data blocks needed to prove the existence or non-existence of record in the current version of repo. Does not require auth.
  *
@@ -17,25 +20,23 @@ final readonly class GetRecord
 {
     public function __construct(
         private RequestFactoryInterface $requestFactory,
-    ) {
-    }
+    ) {}
 
     public function __invoke(
         UriInterface $pdsUri,
         ?string $did = null,
         ?string $collection = null,
         ?string $rkey = null,
-    ): RequestInterface
-    {
+    ): RequestInterface {
         $request = $this->requestFactory
             ->createRequest(
                 'GET',
                 $pdsUri->withPath('xrpc/com.atproto.sync.getRecord')
-                    ->withQuery(\http_build_query(\array_filter([
-                    'did' => $did,
-                    'collection' => $collection,
-                    'rkey' => $rkey,
-                ])))
+                    ->withQuery(http_build_query(array_filter([
+                        'did' => $did,
+                        'collection' => $collection,
+                        'rkey' => $rkey,
+                    ])))
             );
 
         $headers = [
