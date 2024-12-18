0ec8d28aa43317e1a29c9bef99e08bb22b9923b5
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-12-18 14:43:31 -0600

    Update Bluesky.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Value/Bluesky.php
+++ src/Value/Bluesky.php
@@ -8,11 +8,26 @@ use Ghostwriter\AtProtocol\AtProtocol;
 use InvalidArgumentException;
 use Psr\Http\Message\UriInterface;
 
+use function is_string;
+
 final readonly class Bluesky
 {
     public function __construct(
         private PersonalDataServer $personalDataServer
-    ) {
+    ) {}
+
+    public static function new(PersonalDataServer|string|UriInterface $personalDataServer): self
+    {
+        return new self(
+            match (true) {
+                is_string($personalDataServer) => PersonalDataServer::new($personalDataServer),
+                $personalDataServer instanceof UriInterface => PersonalDataServer::new(
+                    $personalDataServer->__toString()
+                ),
+                $personalDataServer instanceof PersonalDataServer => $personalDataServer,
+                default => throw new InvalidArgumentException('Invalid personal data server'),
+            },
+        );
     }
 
     public function atProtocol(): AtProtocol
@@ -28,19 +43,5 @@ final readonly class Bluesky
     public function resumeSession(SessionData $sessionData): SessionData
     {
         return $sessionData;
-    }
-
-    public static function new(PersonalDataServer|string|UriInterface $personalDataServer): self
-    {
-        return new self(
-            match (true) {
-                \is_string($personalDataServer) => PersonalDataServer::new($personalDataServer),
-                $personalDataServer instanceof UriInterface => PersonalDataServer::new(
-                    $personalDataServer->__toString()
-                ),
-                $personalDataServer instanceof PersonalDataServer => $personalDataServer,
-                default => throw new InvalidArgumentException('Invalid personal data server'),
-            },
-        );
     }
 }
